390a5f846638f20e420f7598ee5f45c68c1d7184
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-13 23:55:46 -0400

    Update ManagementPC-OSDCloudInitialConfiguration.ps1

--- Management PC/ManagementPC-OSDCloudInitialConfiguration.ps1
+++ Management PC/ManagementPC-OSDCloudInitialConfiguration.ps1
@@ -94,25 +94,25 @@ Write-Host "Accessing OSD Cloud\User Profile Backup Restore GitHub Repo from OSD
 Write-Host
 Write-Host 
 Write-Host "Processing and Downloading: User Profile Backup and Restpre..." -ForegroundColor Cyan
-$UPBRUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/User%20Profile%20Backup%20and%20Restore.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/User%20Profile%20Backup%20and%20Restore.exe' -DestinationDirectory "C:\downloads"
-$UPBROrgFile = "C:\downloads\User%20Profile%20Backup%20and%20Restore.exe"
-$UPBRNewName = "C:\downloads\UPBR.exe"
+$UPBRUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe'
+Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe' -DestinationDirectory "C:\downloads"
+$UPBROrgFile = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
+$UPBRNewName = "C:\OSDCloud\GitHub\downloads\UPBR.exe"
 Rename-Item -Path $UPBROrgFile -NewName $UPBRNewName
 Copy-Item -Path $UPBRNewName -Destination "$env:username\Desktop"
 
 Write-Host "Accessing OSD Cloud\OS Kits GitHub Repo from OSDCloudCline to download Windows SDK, ADK and PE ADK Add-On files..." -ForegroundColor Cyan
 Write-Host "Processing and Downloading: Windows 11 22H2 SDK Setup File..." -ForegroundColor Cyan
 $Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe' -DestinationDirectory "C:\downloads"
+Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe' -DestinationDirectory "C:\OSDCloud\GitHub\downloads"
 
 Write-Host "Processing and Downloading: Windows 11 22H2 ADK Setup File..." -ForegroundColor Cyan
 $Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe' -DestinationDirectory "C:\downloads"
+Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe' -DestinationDirectory "C:\OSDCloud\GitHub\downloads"
 
 Write-Host "Processing and Downloading: Windows 11 22H2 PE ADK Add-On Setup File..." -ForegroundColor Cyan
 $Win11_22H2PEADKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe' -DestinationDirectory "C:\downloads"
+Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe' -DestinationDirectory "C:\OSDCloud\GitHub\downloads"
 
 Write-Host "Completed All Downloads..." -ForegroundColor Green
 
