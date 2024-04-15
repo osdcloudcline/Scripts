9c8d076156c6173d21fc3db0291e6aa84ad102b2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-15 06:28:37 -0400

    Create WinGet-Browsers.ps1

---
+++ WinGet /WinGet-Browsers.ps1
@@ -0,0 +1,10 @@
+$app32 = "Google Chrome for Enterprise"
+$app33 = "Mozilla Firefox"
+
+Write-Host "Beginning Application Installation on $env:computername..." -ForegroundColor Green
+
+Write-Host "Installing: $app32 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing: $app33 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Mozilla.Firefox --exact --accept-source-agreements  --accept-source-agreements --force
