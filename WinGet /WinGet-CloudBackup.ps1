f0bf98146d478e2b61b68fa202c953e71b1f729b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-15 07:39:20 -0400

    Update WinGet-CloudBackup.ps1

--- WinGet /WinGet-CloudBackup.ps1
+++ WinGet /WinGet-CloudBackup.ps1
@@ -21,5 +21,15 @@ winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept
 Write-Host "Installing: $app44 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Acquiring and Downloading $app5 from GitHub Repo..." -ForegroundColor Green
+Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
 
+$UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
+$UPBRFilePath = "C:\downloads\UserProfileBackupRestore.exe"
+$UPBRFilePathNewName = "C:\downloads\UPBR.exe"
+$UPBRDir = "C:\downloads"
+Save-WebFile -SourceUrl $UPBR -DestinationDirectory $UPBRDir
+Rename-Item -Path $UPBRFilePath  -NewName $UPBRFilePathNewName
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
+$Shortcut.TargetPath = "C:\downloads\UPBR.exe"
+$Shortcut.Save()
