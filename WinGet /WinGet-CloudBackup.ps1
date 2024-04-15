76f5bce1d2d4a5b6791b6c0ea68a831a955790be
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-15 07:39:55 -0400

    Update WinGet-CloudBackup.ps1

--- WinGet /WinGet-CloudBackup.ps1
+++ WinGet /WinGet-CloudBackup.ps1
@@ -9,16 +9,16 @@ Write-Host "Beginning Application Installation on $env:computername..." -Foregro
 # Cloud Backup software
 Write-Host "Installing Cloud Backup Applications on $env:computername..." -ForegroundColor Green
 
-Write-Host "Installing: $app41 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing: $app42 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing: $app43 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app3 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing: $app44 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app4 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
