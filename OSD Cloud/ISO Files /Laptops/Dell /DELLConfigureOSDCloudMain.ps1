ec4f9e47a2f4ba77ed3a3e0bc227c5330af0a937
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-17 00:34:54 -0400

    Update DELLConfigureOSDCloudMain.ps1

--- OSD Cloud/ISO Files /Laptops/Dell /DELLConfigureOSDCloudMain.ps1
+++ OSD Cloud/ISO Files /Laptops/Dell /DELLConfigureOSDCloudMain.ps1
@@ -4,7 +4,7 @@
 Function Show-MainMenu{
     [CmdletBinding()]
     param(
-    [string]$Title = 'OSD Cloud - Dell Computers Main Menu',
+    [string]$Title = 'OSD Cloud - Dell Laptop Computers Main Menu',
     [string]$Question = 'What type of action do you want to perform?'
 )
 
