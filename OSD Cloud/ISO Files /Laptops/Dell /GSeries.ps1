04b9f9faf49639ee7e6be9e3acd91fc21e571132
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-20 00:46:18 -0400

    Update GSeries.ps1

--- OSD Cloud/ISO Files /Laptops/Dell /GSeries.ps1
+++ OSD Cloud/ISO Files /Laptops/Dell /GSeries.ps1
@@ -19,6 +19,11 @@ Dismount-WindowsImage -Path $mountdir -Save
 
 }
 
+Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck 
+Import-Module -Name OSD -Force
+
 #######################################################################################
 #### DELL G-SERIES NETWORK DRIVER EXTRACTION LOCATIONS ################################
 #######################################################################################
