7e23bb2230d55dc0827c61f6f0c832056ea47bd5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-21 00:35:59 -0400

    Create Programming.ps1

---
+++ WinGet /Install /Individual Scripts/Programming.ps1
@@ -0,0 +1,12 @@
+$app1 = "VS Code"
+$app2 = "Visual Studio 2022 Enterprise"
+
+Write-Host "Beginning Application Installation on $env:computername..." -ForegroundColor Green
+
+Write-Host "Installing Programming Applications on $env:computername..." -ForegroundColor Green
+
+Write-Host "Installing $app1 on $env:computername ..." -ForegroundColor Yellow
+winget install --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing $app2 on $env:computername ..." -ForegroundColor Yellow
+winget install --id Microsoft.VisualStudio.2022.Enterprise --exact --accept-source-agreements  --accept-source-agreements --force
