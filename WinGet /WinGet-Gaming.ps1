d37bf4ea56647ddc2cd4b706d06ca8f827de8852
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-21 00:39:57 -0400

    Delete WinGet /WinGet-Gaming.ps1

--- WinGet /WinGet-Gaming.ps1
+++
@@ -1,8 +0,0 @@
-$app1 = "Steam"
-
-Write-Host "Beginning Application Installation on $env:computername..." -ForegroundColor Green
-
-Write-Host "Installing Gaming Applications on $env:computername..." -ForegroundColor Green
-
-Write-Host "Installing: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
