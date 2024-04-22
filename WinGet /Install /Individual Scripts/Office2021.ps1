790ec2f83672e4c1cd9658fcec89e5b5c0525bf6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-22 08:04:22 -0400

    Update Office2021.ps1

--- WinGet /Install /Individual Scripts/Office2021.ps1
+++ WinGet /Install /Individual Scripts/Office2021.ps1
@@ -5,6 +5,7 @@ $app1 = "Microsoft Office 2021 LTSC"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
 $OfficeGHdownloads = "C:\Office"
+mkdir $OfficeGHdownloads
 $Office2021FilesUrl = Invoke-WebRequest ("https://github.com/osdcloudcline/OSDCloud/raw/main/Software/Office2021/OSDCloud-Office2021.zip")
 Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
 Expand-Archive -Path "$OfficeGHdownloads\OSDCloud-Office2021.zip" -DestinationPath $OfficeGHdownloads
