6ee6b39b289c2d4d414ee975c891e32e88703477
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-23 00:25:59 -0400

    Update Office2021.ps1

--- WinGet /Install /Individual Scripts/Office2021.ps1
+++ WinGet /Install /Individual Scripts/Office2021.ps1
@@ -4,7 +4,7 @@ Import-Module -Name OSD -Force
 $app1 = "Microsoft Office 2021 LTSC"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
-$OfficeGHdownloads = "C:\Office"
+$OfficeGHdownloads = "C:\OSDCloud\GitHub\downloads\Office" 
 mkdir $OfficeGHdownloads
 $Office2021FilesUrl = "https://github.com/osdcloudcline/OSDCloud/raw/main/Software/Office2021/OSDCloud-Office2021.zip"
 Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
