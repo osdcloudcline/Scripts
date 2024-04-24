b528a021cab9a34a8adf26122064798fc9666c42
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-24 05:55:35 -0400

    Update OSDCloudMain.ps1

--- OSD Cloud/OSDCloudMain.ps1
+++ OSD Cloud/OSDCloudMain.ps1
@@ -7,3 +7,14 @@ Function Show-MainMenu{
     [string]$Title = 'OSD Cloud - Main Menu',
     [string]$Question = 'What type of action do you want to perform?'
 )
+
+
+Write-Host " 1. OSD Cloud - Initial Setup and Custimizations - Dell Laptops"
+Write-Host " 2. OSD Cloud - Initial Setup and Custimizations - Dell Desktops"
+Write-Host " 3. OSD Cloud - Initial Setup and Custimizations - Custom Built Desktops + ASUS AMD/Intel Motherboards"
+Write-Host " 4. OSD Cloud - Initial Setup and Custimizations - HP Laptops"
+Write-Host " 5. OSD Cloud - Initial Setup and Custimizations - ASUS Laptops"
+Write-Host " 6. OSD Cloud - Initial Setup and Custimizations - Acer Laptops"
+Write-Host " 7. OSD Cloud - Initial Setup and Custimizations - Lenovo Laptops"
+Write-Host " 8. OSD Cloud - Initial Setup and Custimizations - Virtual Machines"
+Write-Host " 9. OSD Cloud - Initial Setup and Custimizations - MS Surface Devices"
