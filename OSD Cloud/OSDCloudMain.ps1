d95afc7f4983dce596745b14c366a73e240439d9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-24 05:57:17 -0400

    Update OSDCloudMain.ps1

--- OSD Cloud/OSDCloudMain.ps1
+++ OSD Cloud/OSDCloudMain.ps1
@@ -18,3 +18,47 @@ Write-Host " 6. OSD Cloud - Initial Setup and Custimizations - Acer Laptops"
 Write-Host " 7. OSD Cloud - Initial Setup and Custimizations - Lenovo Laptops"
 Write-Host " 8. OSD Cloud - Initial Setup and Custimizations - Virtual Machines"
 Write-Host " 9. OSD Cloud - Initial Setup and Custimizations - MS Surface Devices"
+Write-Host " 10. Return to Main Menu"
+
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+
+  '1' { cls
+        $DellLaptopsOSDCloudConfigScript = Invoke-WebRequest "https://github.com/osdcloudcline/Scripts/raw/main/OSD%20Cloud/ISO%20Files%20/Laptops/Dell%20/DELLConfigureOSDCloudMain.ps1"
+        Invoke-Expression $($DellLaptopsOSDCloudConfigScript.Content)
+        }
+  '2' { cls
+        $DellDesktopsOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($DellDesktopsOSDCloudConfigScript.Content)
+        }
+  '3' { cls
+        $CustomPCOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($CustomPCOSDCloudConfigScript.Content)
+        }
+  '4' { cls
+        $HPLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($HPLaptopsOSDCloudConfigScript.Content)
+        }
+  '5' { cls
+        $ASUSLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($ASUSLaptopsOSDCloudConfigScript.Content)
+        }
+  '6' { cls
+        $AcerLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($AcerLaptopsOSDCloudConfigScript.Content)
+        }
+  '7' { cls
+        $LenovoLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($LenovoLaptopsOSDCloudConfigScript.Content)
+        }
+  '8' { cls
+         $ESXIVMOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($ESXIVMOSDCloudConfigScript.Content)
+        }
+  '9' { cls
+        $MSSurfaceOSDCloudConfigScript = Invoke-WebRequest ""
+        Invoke-Expression $($MSSurfaceOSDCloudConfigScript.Content)
+        }
