61f7e07b3453351a9979a29b0b9e323602568e8f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-24 06:07:18 -0400

    Update OSDCloudMain.ps1

--- OSD Cloud/OSDCloudMain.ps1
+++ OSD Cloud/OSDCloudMain.ps1
@@ -4,20 +4,20 @@
 Function Show-MainMenu{
     [CmdletBinding()]
     param(
-    [string]$Title = 'OSD Cloud - Main Menu',
+    [string]$Title = 'OSD Cloud Initial Setup and Custimizations - Main Menu',
     [string]$Question = 'What type of action do you want to perform?'
 )
 
 
-Write-Host " 1. OSD Cloud - Initial Setup and Custimizations - Dell Laptops"
-Write-Host " 2. OSD Cloud - Initial Setup and Custimizations - Dell Desktops"
-Write-Host " 3. OSD Cloud - Initial Setup and Custimizations - Custom Built Desktops + ASUS AMD/Intel Motherboards"
-Write-Host " 4. OSD Cloud - Initial Setup and Custimizations - HP Laptops"
-Write-Host " 5. OSD Cloud - Initial Setup and Custimizations - ASUS Laptops"
-Write-Host " 6. OSD Cloud - Initial Setup and Custimizations - Acer Laptops"
-Write-Host " 7. OSD Cloud - Initial Setup and Custimizations - Lenovo Laptops"
-Write-Host " 8. OSD Cloud - Initial Setup and Custimizations - Virtual Machines"
-Write-Host " 9. OSD Cloud - Initial Setup and Custimizations - MS Surface Devices"
+Write-Host " 1. Dell Laptops"
+Write-Host " 2. Dell Desktops"
+Write-Host " 3. Custom Built Desktops + ASUS AMD/Intel Motherboards"
+Write-Host " 4. HP Laptops"
+Write-Host " 5. ASUS Laptops"
+Write-Host " 6. Acer Laptops"
+Write-Host " 7. Lenovo Laptops"
+Write-Host " 8. Virtual Machines"
+Write-Host " 9. MS Surface Devices"
 Write-Host " 10. Return to Main Menu"
 
 do 
