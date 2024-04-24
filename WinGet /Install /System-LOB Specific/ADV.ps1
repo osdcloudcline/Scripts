30e857bf78344fe757f48741459361473829639e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-23 22:21:32 -0400

    Update ADV.ps1

--- WinGet /Install /System-LOB Specific/ADV.ps1
+++ WinGet /Install /System-LOB Specific/ADV.ps1
@@ -10,6 +10,8 @@ Write-Host
 
 pause
 
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-PreReqInstall.log"
+
 ####################################################################
 ### SYSTEM SOFTWARE VARIABLES AND INSTALL                      #####
 ####################################################################
@@ -67,45 +69,68 @@ $app25ver = "Version: 1.19.10302.0"
 $app26 = "Microsoft Windows Powershell"
 $app26ver = "Version: 7.4.1.0"	
 
+
+
 # WinGet Mandatory Software Install
 
-Write-Host "Configuring Pre-Requisities for Software Installation..." -ForegroundColor Green
+Write-Host "Processing Step 1 of 4: System Pre-Requisities, including PowerShell Package Management Modules..." -Foreground Color Yellow
+Write-Host
+Write-Host
+Write-Host
 Set-ExecutionPolicy unrestricted -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-Start-Transcript -Path "C:\Logs\Powershell\PreReqSoftwareInstall.log"
+
 Set-ExecutionPolicy -Scope LocalMachine unrestricted -Force -ErrorAction SilentlyContinue
 Set-ExecutionPolicy -Scope CurrentUser unrestricted -Force -ErrorAction SilentlyContinue
-
 
 $PSModule9 = "NuGet"
 $PSModule10 = "OneGet"
 $PSModule11 = "WinGet"
 $PSModule12 = "Program Management Module"
 
+
+Write-Host
+Write-Host 
 Write-Host "Installing: $PSModule9..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name NuGet -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule9..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name NuGet -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule10..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name PackageManagement -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule10..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name PackageManagement -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule11..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name WinGet -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSmodule11..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name WinGet -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name ProgramManagement -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name ProgramManagement -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Stop-Transcript
+
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-OSDPowerShellModules.log"
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host "Processing Step 2 of 4: OSD PowerShell Modules..." -Foreground Color Yellow
+Write-Host
 
 
 $PSModule1 = "OSD Module"
@@ -118,150 +143,189 @@ $PSModule7 = "OSD Software Module"
 $PSModule8 = "OSD Deploy Module"
 $PSModule13 = "Windows Update Module"
 
-Write-Host "Accessing www.powershellgallery.com package database to install Powershell Modules..." -ForegroundColor Cyan 
-		
+
+Write-Host
+Write-Host
+Write-Host "Accessing www.powershellgallery.com package database to install Powershell Modules..." -ForegroundColor Green
+Write-Host
+Write-Host
 Write-Host "Installing: $PSModule1..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSD -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule1..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSD -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-	
+Write-Host	
 Write-Host "Installing: $PSModule2..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDSUS -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
 Write-Host "Importing: $PSModule2..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDSUS -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule3..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDBuilder -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule3..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDBuilder -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule4..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDUpdate -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule4..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDUpdate -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule4..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 		
 Install-Module -Name OSDCatalog -Repository PSGallery -Force -AllowClobber 
 Start-Sleep -Seconds 5		
+Write-Host
 Write-Host "Importing: $PSModule4..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDCatalog -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-
+Write-Host
 Write-Host "Installing: $PSModule5..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDDrivers -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule5..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDDrivers -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule6 - 1 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name OSDCloudCLI -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 2 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan		
 Install-Module -Name OSDCloudGUI -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 3 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan		
 Install-Module -Name AutopilotOOBE -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 4 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan	
 Install-Module -Name OSDCloudAzure -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 5 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan		
 Install-Module -Name Azure -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 6 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan		
 Install-Module -Name AzureAD -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 7 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name Az.Accounts -Force -AllowClobber -SkipPublisherCheck
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 8 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name Az.KeyVault -Force -AllowClobber -SkipPublisherCheck
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 9 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name Az.Resources -Force -AllowClobber -SkipPublisherCheck
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 10 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name Az.Storage -Force -AllowClobber -SkipPublisherCheck
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Installing: $PSModule6 - 11 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name Microsoft.Graph.DeviceManagement -Force -AllowClobber -SkipPublisherCheck
-Start-Sleep -Seconds 5		
+Start-Sleep -Seconds 5	
+Write-Host
 Write-Host "Installing: $PSModule6 - 12 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Install-Module -Name PSCloudPC -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
-
+Write-Host
 Write-Host "Importing: $PSModule6 - 1 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name OSDCloudCLI -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 2 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name OSDCloudGUI -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 3 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name AutopilotOOBE -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 4 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name OSDCloudAzure -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 5 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name Azure -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 6 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name AzureAD -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 7 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name Az.Accounts -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 8 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name Az.KeyVault -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 9 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name Az.Resources -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 10 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name Az.Storage -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 11 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name Microsoft.Graph.DeviceManagement -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule6 - 12 of 12..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan
 Import-Module -Name PSCloudPC -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule7..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDSoftware -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule7..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDSoftware -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule8..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name OSDeploy -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
 Write-Host "Importing: $PSModule8..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name OSDeploy -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
-		
+Write-Host		
 Write-Host "Installing: $PSModule13..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Install-Module -Name PSWindowsUpdate -Repository PSGallery -Force -AllowClobber
 Start-Sleep -Seconds 5
+Write-Host
 Write-Host "Importing: $PSModule13..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Import-Module -Name PSWindowsUpdate -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 Start-Sleep -Seconds 5
+Write-Host
 Stop-Transcript 
 
 
-Start-Transcript -Path "C:\Logs\Powershell\ADVSoftwareInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-SoftwareInstall.log"
 
-Write-Host "Beginning System Software Installation on $env:computername..." -ForegroundColor Green
-
+Write-Host "Processing Step 3 of 4: Software Installation..." -Foreground Color Yellow  
+Write-Host
+Write-Host
+Write-Host "Processing: Mandatory System Software Pre-Requisites" -ForegroundColor Yellow
+Write-Host
+Write-Host
 Write-Host "Installing: $app1, $app1ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id  Microsoft.DotNet.Runtime.3_1 --exact --accept-source-agreements --accept-source-agreements --force
 
@@ -330,7 +394,7 @@ choco install dotnetfx -y
 		
 Write-Host "Installing: $app23, $app23ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 choco install openal -y
-		
+	
 Write-Host "Installing: $app24, $app24ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Microsoft.Edge --exact --accept-source-agreements --accept-source-agreements --force
 		
