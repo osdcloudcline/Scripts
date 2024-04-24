84335818ddb0947aacab06baf672e499092a9a07
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-24 16:43:28 -0400

    Update PCMainMenu.ps1

--- OSD Cloud/PCMainMenu.ps1
+++ OSD Cloud/PCMainMenu.ps1
@@ -1,5 +1,4 @@
-Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
-Import-Module -Name OSD -Force
+
 ##########################################################
 # Main Menu
 ##########################################################
@@ -42,11 +41,13 @@ do
         Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList "/scannow"
         }
   '5' { cls
+        Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+        Import-Module -Name OSD -Force
         $OSDCloudGHDownloads = "C:\OSDCloud\GitHub\downloads"
         $UPBR = Invoke-WebRequest ("https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe")
+        Save-WebFile -SourceUrl $UPBR -DestinationDirectory $OSDCloudGHdownloads
         $UPBROrgName = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
         $UPBRNewName = "C:\OSDCloud\GitHub\downloads\UPBR.exe"
-        Save-WebFile -SourceUrl $UPBR -DestinationDirectory $OSDCloudGHdownloads
         Rename-Item -Path $UPBROrgName -NewName $UPBRNewName
         Start-Process -FilePath $UPBRNewName
         
