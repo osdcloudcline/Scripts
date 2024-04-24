a64c881ce60619dac7c81c3c705e8dba35778f2c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-23 22:26:23 -0400

    Update VDI.ps1

--- WinGet /Install /System-LOB Specific/VDI.ps1
+++ WinGet /Install /System-LOB Specific/VDI.ps1
@@ -1,1 +1,10 @@
 
+
+
+
+
+Start-Transcript -Path "C:\Logs\Powershell\VDI\$env:computername-PreReqInstall.log"
+
+####################################################################
+### SYSTEM SOFTWARE VARIABLES AND INSTALL                      #####
+####################################################################
