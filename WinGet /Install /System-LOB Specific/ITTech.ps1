64555bcfddde89d93ae18eaf4cac84e5b57a4462
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-23 22:27:30 -0400

    Update ITTech.ps1

--- WinGet /Install /System-LOB Specific/ITTech.ps1
+++ WinGet /Install /System-LOB Specific/ITTech.ps1
@@ -1,1 +1,10 @@
 
+
+
+
+
+Start-Transcript -Path "C:\Logs\Powershell\ITTechs\$env:computername-PreReqInstall.log"
+
+####################################################################
+### SYSTEM SOFTWARE VARIABLES AND INSTALL                      #####
+####################################################################
