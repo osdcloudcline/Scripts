4d7f042d528df2f52836f11b1d5efc830e8106ea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-24 05:54:34 -0400

    Update OSDCloudMain.ps1

--- OSD Cloud/OSDCloudMain.ps1
+++ OSD Cloud/OSDCloudMain.ps1
@@ -1,1 +1,9 @@
-
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OSD Cloud - Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
