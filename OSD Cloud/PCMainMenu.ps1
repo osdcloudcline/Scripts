36d98c04eefa5c092b99ebc49901ab2894a92048
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-25 11:54:22 -0400

    Update PCMainMenu.ps1

--- OSD Cloud/PCMainMenu.ps1
+++ OSD Cloud/PCMainMenu.ps1
@@ -8,7 +8,31 @@ Function Show-MainMenu{
     [string]$Title = 'OSD Cloud -  Computers Main Menu',
     [string]$Question = 'What type of action do you want to perform?'
 )
+cls
 
+
+
+       $os = Get-CimInstance -ClassName Win32_OperatingSystem
+       $PCName = "$env:computername"
+       $OSVerison = $($os.Version)
+       $UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+       $FullOS = $($os.Caption)
+       $OSBuild = $($os.Version) + "." + $UBR
+       $DisplayVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name DisplayVersion).DisplayVersion
+       $EditionID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name EditionID).EditionID
+       $CompleteOSInfo = "$FullOS" + " $OSBuild" + " $DisplayVersion"
+       $EdgeEXE = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe' "(default)"
+       $EdgeBrowser = (Get-Item $EdgeEXE).VersionInfo.ProductVersion
+       Write-Host
+       Write-Host         "Running Computer Info:" -ForegroundColor Yellow 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Running Operating System: $CompleteOSInfo" -Verbose  
+       Write-Verbose "Operating System: $FullOS" -Verbose 
+       Write-Verbose "Build Number: $OSBuild" -Verbose 
+       Write-Verbose "Display Version: $DisplayVersion" -Verbose
+       Write-Verbose "OS Edition: $EditionID" -Verbose 
+       Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose 
+pause
 Clear-Host
 Write-Host "======= $Title ======"
 Write-Host " 1. OSD Cloud Configuration"
