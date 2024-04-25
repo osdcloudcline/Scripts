4e2d4d65b00f8683008e5198a4ec54111925d74e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-25 00:11:50 -0400

    Update SMConfigMain.ps1

--- Start Menu Configurations/SMConfigMain.ps1
+++ Start Menu Configurations/SMConfigMain.ps1
@@ -28,7 +28,7 @@ $selection = Read-Host 'Please choose an option'
        Invoke-Expression $($StandardSM.Content)
        }
  '2'{cls
-      $AdvancedSM = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/System-LOB%20Specific/ADV.ps1")
+      $AdvancedSM = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/Start%20Menu%20Configurations/AdvancedSM.ps1")
       Invoke-Expression $($AdvancedSM.Content)
      }
  '3'{cls
