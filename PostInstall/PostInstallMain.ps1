382aa41c786be1a4fea82347b830172cc0944cfa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-25 11:46:01 -0400

    Update PostInstallMain.ps1

--- PostInstall/PostInstallMain.ps1
+++ PostInstall/PostInstallMain.ps1
@@ -26,8 +26,7 @@ $selection = Read-Host 'Please choose an option'
        Invoke-Expression $($W11StartMenu.Content)
        }
  '2'{cls
-      $DebloatOS = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/OSDebloat/OSDebloatMain.ps1")
-      Invoke-Expression $($DebloatOS.Content)
+      
      }
  '3'{cls
       $ClientFeatures = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/System-LOB%20Specific/CORPORATE.ps1")
