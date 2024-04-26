a2819f7d1d7a98ef7c875f3d4bbe854b3cf532bd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-25 22:14:29 -0400

    Update Debloat24H2.ps1

--- PostInstall/OSDebloat/Windows 11 /24H2 /Debloat24H2.ps1
+++ PostInstall/OSDebloat/Windows 11 /24H2 /Debloat24H2.ps1
@@ -1,1 +1,17 @@
-
+       $PCName = "$env:computername"
+       $OSVerison = $($os.Version)
+       $UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+       $FullOS = $($os.Caption)
+       $OSBuild = $($os.Version) + "." + $UBR
+       $DisplayVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name DisplayVersion).DisplayVersion
+       $EditionID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name EditionID).EditionID
+       $CompleteOSInfo = "$FullOS" + " $OSBuild" + " $DisplayVersion"
+       $EdgeEXE = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe' "(default)"
+       $EdgeBrowser = (Get-Item $EdgeEXE).VersionInfo.ProductVersion
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Running Operating System: $CompleteOSInfo" -Verbose  
+       Write-Verbose "Operating System: $FullOS" -Verbose 
+       Write-Verbose "Build Number: $OSBuild" -Verbose 
+       Write-Verbose "Display Version: $DisplayVersion" -Verbose
+       Write-Verbose "OS Edition: $EditionID" -Verbose 
+       Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose
