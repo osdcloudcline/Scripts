6bdf0fc3a4f35beadd4e7b4036461c4807eabc3e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-27 01:25:48 -0400

    Update ADV.ps1

--- WinGet /Install /System-LOB Specific/ADV.ps1
+++ WinGet /Install /System-LOB Specific/ADV.ps1
@@ -655,10 +655,13 @@ Save-WebFile -SourceUrl $Office2021SetupEXEUrl -DestinationDirectory $OSDCloudGH
 Save-WebFile -SourceUrl $Office2021SetupCMDUrl -DestinationDirectory $OSDCloudGHOfficedownloads
 Save-WebFile -SourceUrl $Office2021SetupDLCMDUrl -DestinationDirectory $OSDCloudGHOfficedownloads
 
-Write-Host "Downloading $app57 from Microsoft Office CDN...." -ForegroundColor Cyan 
+Write-Host "Downloading $app57 - Download Source: Microsoft Office CDN...." -ForegroundColor Cyan 
 $Office2021SetupDLCMDUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/Software/Office2021/OfficeCDNDownload.cmd'
 Invoke-Expression $($Office2021SetupDLCMDUrl.Content)
 
-Write-Host "Installing $app57 from Microsoft Office CDN...." -ForegroundColor Cyan 
+Write-Host "Installing $app57 - Microsoft Office CDN...." -ForegroundColor Cyan 
 $Office2021SetupCMDUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/Software/Office2021/OfficeCDNSetup.cmd'
 Invoke-Expression $($Office2021SetupCMDUrl.Content)
+
+$Manufacturer = Invoke-WebRequest("")
+Invoke-Expression $($Manufacturer.Content)
