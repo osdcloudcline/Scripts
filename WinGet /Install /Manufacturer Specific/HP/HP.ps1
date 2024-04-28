e1e2578f0d229a9734164128568b812bed781ce5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-27 21:42:47 -0400

    Update HP.ps1

--- WinGet /Install /Manufacturer Specific/HP/HP.ps1
+++ WinGet /Install /Manufacturer Specific/HP/HP.ps1
@@ -162,3 +162,28 @@ Write-Host
 Write-Verbose "Installing: $VMModule1...Module No: 1 of 1" -Verbose
 Install-Module -Name WindowsImageBuilder -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name WindowsImageBuilder -Force
+
+$HPSoftware1 = "HP Smart"
+$HPSoftware2 = "HP Scan and Capture"
+$HPSoftware3 = "HP PC Hardware Diagnostics"
+$HPSoftware4 = "HP Cloud Recovery Tool"
+$HPSoftware5 = "HP Display Center"
+$HPSoftware6 = "OMEN Gaming Hub"
+
+$MfrSoftwareGroup1 = "HP"
+
+Write-Host "Installing $MfrSoftwareGroup1 Software..." -ForegroundColor Cyan
+Write-Host
+Write-Host
+Write-Verbose "Installing: $HPSoftware1...OEM Software No: 1 of 6" -Verbose
+winget install --id 9WZDNCRFHWLH --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Installing: $HPSoftware2...OEM Software No: 2 of 6" -Verbose
+winget install --id 9WZDNCRFHWL0 --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Installing: $HPSoftware3...OEM Software No: 3 of 6" -Verbose
+winget install --id 9P4PNDG7L782 --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Installing: $HPSoftware4...OEM Software No: 4 of 6" -Verbose
+winget install --id HP.HPCloudRecoveryTool --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Installing: $HPSoftware5...OEM Software No: 5 of 6" -Verbose
+winget install --id 9NT6FQ9KQF90 --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Installing: $HPSoftware6...OEM Software No: 6 of 6" -Verbose
+winget install --id 9NQDW009T0T5 --accept-source-agreements  --accept-source-agreements --force
