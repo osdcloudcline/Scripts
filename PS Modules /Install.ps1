2ea6bf2cb0b207f3afb966835a8b97daa8ec2699
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-28 12:43:21 -0400

    Update Install.ps1

--- PS Modules /Install.ps1
+++ PS Modules /Install.ps1
@@ -1,1 +1,103 @@
+############################
+### PS Module Group Names ##
+############################
 
+$OSDGroup1 = "OSD"
+$OSDGroup2 = "OSD: Configuration"
+$OSDGroup3 = "OSD: Cloud"
+$OSDGroup4 = "OSD: Autopilot, Drivers, Software and Updates"
+$OSDGroup5 = "OSD: Microsoft Graph"
+
+#################################
+## PS Modules to be installed ###
+#################################
+
+$PSModule9 = "NuGet"
+$PSModule10 = "OneGet"
+$PSModule11 = "WinGet"
+$PSModule12 = "Program Management Module"
+
+$OSDModule1 = "OSD"
+$OSDModule2 = "OSD Builder"
+$OSDModule3 = "OSD SUS"
+$OSDModule4 = "OSD CloudCLI"
+$OSDModule5 = "OSD CloudGUI"
+$OSDModule6 = "OSD CloudAzure"
+$OSDModule7 = "OSD Update"
+$OSDModule8 = "Autopilot OOBE"
+$OSDModule9 = "OSD Drivers"
+$OSDModule10 ="OSD Deploy"
+$OSDModule11 = "OSD Software"
+$OSDModule12 = "OSD Catalog"
+$OSDModule13 = "OSD Progress"
+
+$CloudModule1 = "Azure"
+$CloudModule2 = "Azure AD"
+$CloudModule3 = "Azure Accounts"
+$CloudModule4 = "Azure Storage"
+$CloudModule5 = "Azure Resources"
+$CloudModule6 = "Azure Key Vault"
+$CloudModule7 = "Azure Compute"
+$CloudModule8 = "Azure Automation"
+$CloudModule9 = "Azure Network"
+$CloudModule10 = "Azure Api Management"
+$CloudModule11 = "Azure Advisor"
+$CloudModule12 = "Azure Analysis Services"
+$CloudModule13 = "Azure Billing"
+$CloudModule14 = "Azure Application Insights"
+$CloudModule15 = "Azure CDN"
+$CloudModule16 = "Azure Batch"
+$CloudModule17 = "Azure Monitor"
+$CloudModule18 = "Windows 365 Cloud PC"
+
+Write-Host
+Write-Host
+Write-Host "Accessing www.powershellgallery.com package database to install Powershell Modules..." -ForegroundColor Green
+Write-Host
+Write-Host
+
+Write-Host "Installing $OSDGroup1 Modules..." -ForegroundColor Cyan
+Write-Host
+Write-Host
+Write-Verbose "Installing: $OSDModule1..." Verbose
+Install-Module -Name OSD -Repository PSGallery -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+
+Write-Host "Installing $OSDGroup2 Modules..." -ForegroundColor Cyan
+Write-Host
+Write-Host
+Write-Verbose "Installing: $OSDModule2..." -Verbose 
+Install-Module -Name OSDBuilder -Repository PSGallery -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+
+# Cloud PowerShell Modules
+
+Write-Host "Installing and Importing: $OSDGroup3 Modules..." -ForegroundColor Cyan
+Write-Host
+Write-Host
+Write-Verbose "Installing: $CloudModule1..." -Verbose	
+Install-Module -Name Azure -Repository PSGallery -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Installing: $CloudModule2..." -Verbose			
+Install-Module -Name AzureAD -Repository PSGallery -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Installing: $CloudModule3..." -Verbose	
+Install-Module -Name Az.Accounts -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Installing: $CloudModule4..." -Verbose
+Install-Module -Name Az.Storage -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Installing: $CloudModule5..." -Verbose
+Install-Module -Name Az.Resources -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Installing: $CloudModule6..." -Verbose
+Install-Module -Name Az.KeyVault -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 5
+Write-Host
