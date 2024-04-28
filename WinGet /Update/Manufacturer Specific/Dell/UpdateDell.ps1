7ea42b37f285050fff7e447dcc7c5dee60138cea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-28 09:28:45 -0400

    Update UpdateDell.ps1

--- WinGet /Update/Manufacturer Specific/Dell/UpdateDell.ps1
+++ WinGet /Update/Manufacturer Specific/Dell/UpdateDell.ps1
@@ -76,200 +76,200 @@ Write-Host
 Write-Host
 Write-Host "Updating $SYSGroup1 Modules..." -ForegroundColor Cyan
 Write-Verbose "Updating: $SYSModule1...Module No: 1 of 2" -Verbose
-Update-Module -Name PackageManagement -Force 
+Update-Module -Name PackageManagement -Force -Repository PSGallery
 Import-Module -Name PackageManagement -Force
 Write-Verbose "Updating: $SYSModule2...Module No: 2 of 2" -Verbose
-Update-Module -Name Choco -Force 
+Update-Module -Name Choco -Force -Repository PSGallery
 Import-Module -Name Choco -Force
 
 Write-Host "Updating $OSDGroup1 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $OSDModule1...Module No: 1 of 1" -Verbose
-Update-Module -Name OSD -Force 
+Update-Module -Name OSD -Force -Repository PSGallery
 Import-Module -Name OSD -Force
 
 Write-Host "Updating $OSDGroup2 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $OSDModule2...Module No: 1 of 2" -Verbose
-Update-Module -Name OSDBuilder -Force 
+Update-Module -Name OSDBuilder -Force -Repository PSGallery
 Import-Module -Name OSDBuilder -Force
 Write-Verbose "Updating: $OSDModule3...Module No: 2 of 2" -Verbose
-Update-Module -Name OSDSUS -Force 
+Update-Module -Name OSDSUS -Force -Repository PSGallery
 Import-Module -Name OSDSUS -Force
 
 Write-Host "Updating $OSDGroup3 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $OSDModule4...Module No: 1 of 20" -Verbose
-Update-Module -Name OSDCloudCLI -Force 
+Update-Module -Name OSDCloudCLI -Force -Repository PSGallery
 Import-Module -Name OSDCloudCLI -Force
 Write-Verbose "Updating: $OSDModule5...Module No: 2 of 20" -Verbose
-Update-Module -Name OSDCloudGUI -Force 
+Update-Module -Name OSDCloudGUI -Force -Repository PSGallery
 Import-Module -Name OSDCloudGUI -Force
 Write-Verbose "Updating: $OSDModule6...Module No: 3 of 20" -Verbose
-Update-Module -Name OSDCloudAzure -Force 
+Update-Module -Name OSDCloudAzure -Force -Repository PSGallery
 Import-Module -Name OSDCloudAzure -Force
 Write-Verbose "Updating: $CloudModule1...Module No: 4 of 20" -Verbose
-Update-Module -Name Azure -Force 
+Update-Module -Name Azure -Force -Repository PSGallery
 Import-Module -Name Azure -Force
 Write-Verbose "Updating: $CloudModule2...Module No: 5 of 20" -Verbose
-Update-Module -Name Az.Accounts -Force 
+Update-Module -Name Az.Accounts -Force -Repository PSGallery
 Import-Module -Name Az.Accounts -Force
 Write-Verbose "Updating: $CloudModule3...Module No: 6 of 20" -Verbose
-Update-Module -Name Azure.Storage -Force 
+Update-Module -Name Azure.Storage -Force -Repository PSGallery
 Import-Module -Name Azure.Storage -Force
 Write-Verbose "Updating: $CloudModule4...Module No: 7 of 20" -Verbose
-Update-Module -Name Az.Resources -Force 
+Update-Module -Name Az.Resources -Force -Repository PSGallery
 Import-Module -Name Az.Resources -Force
 Write-Verbose "Updating: $CloudModule5...Module No: 8 of 20" -Verbose
-Update-Module -Name Az.KeyVault -Force 
+Update-Module -Name Az.KeyVault -Force -Repository PSGallery
 Import-Module -Name Az.KeyVault -Force
 Write-Verbose "Updating: $CloudModule6...Module No: 9 of 20" -Verbose
-Update-Module -Name Az.Compute -Force 
+Update-Module -Name Az.Compute -Force -Repository PSGallery
 Import-Module -Name Az.Compute -Force
 Write-Verbose "Updating: $CloudModule7...Module No: 10 of 20" -Verbose
-Update-Module -Name Az.Automation -Force 
+Update-Module -Name Az.Automation -Force -Repository PSGallery
 Import-Module -Name Az.Automation -Force
 Write-Verbose "Updating: $CloudModule8...Module No: 11 of 20" -Verbose
-Update-Module -Name Az.Network -Force 
+Update-Module -Name Az.Network -Force -Repository PSGallery
 Import-Module -Name Az.Network -Force
 Write-Verbose "Updating: $CloudModule9...Module No: 12 of 20" -Verbose
-Update-Module -Name Az.ApiManagement -Force 
+Update-Module -Name Az.ApiManagement -Force -Repository PSGallery
 Import-Module -Name Az.ApiManagement -Force
 Write-Verbose "Updating: $CloudModule10...Module No: 13 of 20" -Verbose
-Update-Module -Name Az.Advisor -Force 
+Update-Module -Name Az.Advisor -Force -Repository PSGallery
 Import-Module -Name Az.Advisor -Force
 Write-Verbose "Updating: $CloudModule11...Module No: 14 of 20" -Verbose
-Update-Module -Name Az.AnalysisServices -Force 
+Update-Module -Name Az.AnalysisServices -Force -Repository PSGallery
 Import-Module -Name Az.AnalysisServices -Force
 Write-Verbose "Updating: $CloudModule12...Module No: 15 of 20" -Verbose
-Update-Module -Name Az.Billing -Force 
+Update-Module -Name Az.Billing -Force -Repository PSGallery
 Import-Module -Name Az.Billing -Force
 Write-Verbose "Updating: $CloudModule13...Module No: 16 of 20" -Verbose
-Update-Module -Name Az.ApplicationInsights -Force
+Update-Module -Name Az.ApplicationInsights -Force -Repository PSGallery
 Import-Module -Name Az.ApplicationInsights -Force
 Write-Verbose "Updating: $CloudModule14...Module No: 17 of 20" -Verbose
-Update-Module -Name Az.Cdn -Force
+Update-Module -Name Az.Cdn -Force -Repository PSGallery
 Import-Module -Name Az.Cdn -Force
 Write-Verbose "Updating: $CloudModule15...Module No: 18 of 20" -Verbose
-Update-Module -Name Az.Batch -Force
+Update-Module -Name Az.Batch -Force -Repository PSGallery
 Import-Module -Name Az.Batch -Force
 Write-Verbose "Updating: $CloudModule16...Module No: 19 of 20" -Verbose
-Update-Module -Name Az.Monitor -Force
+Update-Module -Name Az.Monitor -Force -Repository PSGallery
 Import-Module -Name Az.Monitor -Force
 Write-Verbose "Updating: $CloudModule17...Module No: 20 of 20" -Verbose
-Update-Module -Name PSCloudPC -Force
+Update-Module -Name PSCloudPC -Force -Repository PSGallery
 Import-Module -Name PSCloudPC -Force
 
 Write-Host "Updating $OSDGroup4 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $OSDModule7...Module No: 1 of 7" -Verbose
-Update-Module -Name OSDUpdate -Force -
+Update-Module -Name OSDUpdate -Force  -Repository PSGallery
 Import-Module -Name OSDUpdate -Force
 Write-Verbose "Updating: $OSDModule8...Module No: 2 of 7" -Verbose
-Update-Module -Name AutopilotOOBE -Force 
+Update-Module -Name AutopilotOOBE -Force -Repository PSGallery 
 Import-Module -Name AutopilotOOBE -Force
 Write-Verbose "Updating: $OSDModule9...Module No: 3 of 7" -Verbose
-Update-Module -Name OSDDrivers -Force 
+Update-Module -Name OSDDrivers -Force -Repository PSGallery
 Import-Module -Name OSDDrivers -Force
 Write-Verbose "Updating: $OSDModule10...Module No: 4 of 7" -Verbose
-Update-Module -Name OSDeploy -Force 
+Update-Module -Name OSDeploy -Force -Repository PSGallery
 Import-Module -Name OSDeploy -Force
 Write-Verbose "Updating: $OSDModule11...Module No: 5 of 7" -Verbose
-Update-Module -Name OSDSoftware -Force 
+Update-Module -Name OSDSoftware -Force -Repository PSGallery
 Import-Module -Name OSDSoftware -Force
 Write-Verbose "Updating: $OSDModule12...Module No: 6 of 7" -Verbose
-Update-Module -Name OSDCatalog -Force 
+Update-Module -Name OSDCatalog -Force -Repository PSGallery
 Import-Module -Name OSDCatalog -Force
 Write-Verbose "Updating: $OSDModule13...Module No: 7 of 7" -Verbose
-Update-Module -Name OSDProgress -Force 
+Update-Module -Name OSDProgress -Force -Repository PSGallery
 Import-Module -Name OSDProgress -Force
 
 Write-Host "Updating $OSDGroup5 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $MSGraphModule1...Module No: 1 of 9" -Verbose
-Update-Module -Name Microsoft.Graph -Force
+Update-Module -Name Microsoft.Graph -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph -Force
 Write-Verbose "Updating: $MSGraphModule2...Module No: 2 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Intune -Force
+Update-Module -Name Microsoft.Graph.Intune -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Intune -Force
 Write-Verbose "Updating: $MSGraphModule3...Module No: 3 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Groups -Force
+Update-Module -Name Microsoft.Graph.Groups -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Groups -Force
 Write-Verbose "Updating: $MSGraphModule4...Module No: 4 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Users -Force
+Update-Module -Name Microsoft.Graph.Users -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Users -Force
 Write-Verbose "Updating: $MSGraphModule5...Module No: 5 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Sites -Force
+Update-Module -Name Microsoft.Graph.Sites -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Sites -Force
 Write-Verbose "Updating: $MSGraphModule6...Module No: 6 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Mail -Force
+Update-Module -Name Microsoft.Graph.Mail -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Mail -Force
 Write-Verbose "Updating: $MSGraphModule7...Module No: 7 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Identity.DirectoryManagement -Force 
+Update-Module -Name Microsoft.Graph.Identity.DirectoryManagement -Force -Repository PSGallery 
 Import-Module -Name Microsoft.Graph.Identity.DirectoryManagement -Force
 Write-Verbose "Updating: $MSGraphModule8...Module No: 8 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.Teams -Force
+Update-Module -Name Microsoft.Graph.Teams -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.Teams -Force
 Write-Verbose "Updating: $MSGraphModule9...Module No: 9 of 9" -Verbose
-Update-Module -Name Microsoft.Graph.DeviceManagement -Force 
+Update-Module -Name Microsoft.Graph.DeviceManagement -Force -Repository PSGallery
 Import-Module -Name Microsoft.Graph.DeviceManagement -Force
 
 Write-Host "Updating $MfrGroup1 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $DellModule1...Module No: 1 of 14" -Verbose
-Update-Module -Name DellBIOSProvider -Force 
+Update-Module -Name DellBIOSProvider -Force -Repository PSGallery
 Import-Module -Name DellBIOSProvider -Force
 Write-Verbose "Updating: $DellModule2...Module No: 2 of 14" -Verbose
-Update-Module -Name GetBIOS -Force 
+Update-Module -Name GetBIOS -Force -Repository PSGallery
 Import-Module -Name GetBIOS -Force
 Write-Verbose "Updating: $DellModule3...Module No: 3 of 14" -Verbose
-Update-Module -Name Get-DellWarranty -Force 
+Update-Module -Name Get-DellWarranty -Force -Repository PSGallery
 Import-Module -Name Get-DellWarranty -Force
 Write-Verbose "Updating: $DellModule4...Module No: 4 of 14" -Verbose
-Update-Module -Name Dell.PowerStore -Force 
+Update-Module -Name Dell.PowerStore -Force -Repository PSGallery
 Import-Module -Name Dell.PowerStore -Force
 Write-Verbose "Updating: $DellModule5...Module No: 5 of 14" -Verbose
-Update-Module -Name DellOpenManage -Force 
+Update-Module -Name DellOpenManage -Force -Repository PSGallery
 Import-Module -Name DellOpenManage -Force
 Write-Verbose "Updating: $DellModule6...Module No: 6 of 14" -Verbose
-Update-Module -Name DellWarrantyServiceTag -Force 
+Update-Module -Name DellWarrantyServiceTag -Force -Repository PSGallery
 Import-Module -Name DellWarrantyServiceTag -Force
 Write-Verbose "Updating: $DellModule7...Module No: 7 of 14" -Verbose
-Update-Module -Name Dell.Unity -Force 
+Update-Module -Name Dell.Unity -Force -Repository PSGallery
 Import-Module -Name Dell.Unity -Force
 Write-Verbose "Updating: $DellModule8...Module No: 8 of 14" -Verbose
-Update-Module -Name Dell.PowerMax -Force 
+Update-Module -Name Dell.PowerMax -Force -Repository PSGallery
 Import-Module -Name Dell.PowerMax -Force
 Write-Verbose "Updating: $DellModule9...Module No: 9 of 14" -Verbose
-Update-Module -Name DellBios -Force 
+Update-Module -Name DellBios -Force -Repository PSGallery
 Import-Module -Name DellBios -Force
 Write-Verbose "Updating: $DellModule10...Module No: 10 of 14" -Verbose
-Update-Module -Name SetBIOS -Force 
+Update-Module -Name SetBIOS -Force -Repository PSGallery
 Import-Module -Name SetBIOS -Force
 Write-Verbose "Updating: $DellModule11...Module No: 11 of 14" -Verbose
-Update-Module -Name DellWarranty -Force 
+Update-Module -Name DellWarranty -Force -Repository PSGallery
 Import-Module -Name DellWarranty -Force
 Write-Verbose "Updating: $DellModule12...Module No: 12 of 14" -Verbose
-Update-Module -Name Get-DellSupportInfo -Force 
+Update-Module -Name Get-DellSupportInfo -Force -Repository PSGallery
 Import-Module -Name Get-DellSupportInfo -Force
 Write-Verbose "Updating: $DellModule13...Module No: 13 of 14" -Verbose
-Update-Module -Name Get-DriversPackFromDell -Force 
+Update-Module -Name Get-DriversPackFromDell -Force -Repository PSGallery
 Import-Module -Name Get-DriversPackFromDell -Force
 Write-Verbose "Updating: $DellModule14...Module No: 14 of 14" -Verbose
-Update-Module -Name Dell-BIOSDriver -Force 
+Update-Module -Name Dell-BIOSDriver -Force -Repository PSGallery
 Import-Module -Name Dell-BIOSDriver -Force
 
 Write-Host "Updating $MfrGroup2 Modules..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Updating: $VMModule1...Module No: 1 of 1" -Verbose
-Update-Module -Name WindowsImageBuilder -Force 
+Update-Module -Name WindowsImageBuilder -Force -Repository PSGallery
 Import-Module -Name WindowsImageBuilder -Force
 
 $DellSoftware1 = "Dell Command | Update for Windows Universal Application"
