2eab23812b3dd27d8580b14cfc89cd589b19e405
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-01 16:27:59 -0400

    Update Configure.ps1

--- AuditMode/Configure.ps1
+++ AuditMode/Configure.ps1
@@ -11,4 +11,7 @@ Write-Verbose "Step 2 - Merging Registry Entries..." -Verbose
 $Registry = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/REGFiles/Registry.ps1")
 Invoke-Expression $($Registry.Content)
 
+Write-Verbose "Step 3 - Mandatory OS Pre-Requirements..." -Verbose
 
+$OS = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM.ps1")
+Invoke-Expression $($OS.Content)
