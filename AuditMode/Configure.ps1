ee53bc5918d4d0cbf0de5ad27aec6a073dde9445
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-01 16:11:18 -0400

    Update Configure.ps1

--- AuditMode/Configure.ps1
+++ AuditMode/Configure.ps1
@@ -6,4 +6,6 @@ Write-Verbose "Step 1 - PowerShell Modules..." -Verbose
 $PSModules = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PS%20Modules%20/Install.ps1")
 Invoke-Expression $($PSModules.Content)
 
+Write-Verbose "Step 2 - Merging Registry Entries..." -Verbose
 
+$Registry 
