f4582edf4429fed43a6fdb6662818076ecd35c09
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-01 16:01:33 -0400

    Update Configure.ps1

--- AuditMode/Configure.ps1
+++ AuditMode/Configure.ps1
@@ -1,3 +1,8 @@
+Write-Host "Beginning Installation of Mandatory system software and PowerShell Modules..." -ForegroundColor Cyan
+
+
+Write-Verbose "Step 1 - PowerShell Modules..." -Verbose
+
 $PSModules = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PS%20Modules%20/Install.ps1")
 Invoke-Expression $($PSModules.Content)
 
