3d6d544d651201a9c0041cfccd839b7bfef94475
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-02 09:18:13 -0400

    Update ADV.ps1

--- WinGet /Install /Audit Mode Install/ADV.ps1
+++ WinGet /Install /Audit Mode Install/ADV.ps1
@@ -1,1 +1,53 @@
+Write-Host
+Write-Host
+Write-Host "Step 1 : Media Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
 
+
+
+
+Write-Host
+Write-Host
+Write-Host "Step 2 : Web Browsing Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+Write-Host
+Write-Host
+Write-Host "Step 3 : Sytem Utilities Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+
+Write-Host
+Write-Host
+Write-Host "Step 4 : Programming Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+Write-Host
+Write-Host
+Write-Host "Step 5 : Productivity Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+
+Write-Host
+Write-Host
+Write-Host "Step 6 : Remote Desktop / IT Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+Write-Host
+Write-Host
+Write-Host "Step 7 : Gaming Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
+
+
+Write-Host
+Write-Host
+Write-Host "Step 8 : Security Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
