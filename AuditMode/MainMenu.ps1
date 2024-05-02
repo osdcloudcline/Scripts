5202b4e429e759546251dcfe85a655da126d9a04
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-02 07:16:36 -0400

    Update MainMenu.ps1

--- AuditMode/MainMenu.ps1
+++ AuditMode/MainMenu.ps1
@@ -1,1 +1,12 @@
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OSD Cloud -  Audit Mode Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
+cls
+
 
