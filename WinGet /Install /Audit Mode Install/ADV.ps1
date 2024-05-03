de0e238a8b0853575537fb28f887deb58370be06
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-03 01:34:37 -0400

    Update ADV.ps1

--- WinGet /Install /Audit Mode Install/ADV.ps1
+++ WinGet /Install /Audit Mode Install/ADV.ps1
@@ -142,7 +142,9 @@ Write-Host
 
 $Gamingapp1 = "Steam"
 
-
+Write-Verbose "Installing Gaming Software 1 of 1..." -Verbose
+Write-Host "Installing $Gamingapp1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements --accept-source-agreements --force
 
 Write-Host
 Write-Host
