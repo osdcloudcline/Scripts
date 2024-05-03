41d2b3f0f1b033384621578f658773a28b4da860
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-03 01:15:32 -0400

    Update ADV.ps1

--- WinGet /Install /Audit Mode Install/ADV.ps1
+++ WinGet /Install /Audit Mode Install/ADV.ps1
@@ -4,51 +4,51 @@ Write-Host "Step 1 : Media Software" -ForegroundColor Cyan
 Write-Host
 Write-Host
 
-$app1 = "VLC Media Player"
-$app2 = "MediaInfo"
-$app3 = "MakeMKV"
-$app4 = "Handbrake"
-$app5 = "PowerISO"
-$app6 = "Audacity and FFMPEG"
-$app7 = "Logitech MyHarmony"
-$app8 = "MKVToolNix"
-$app9 = "Apple iTunes"
+$Mediaapp1 = "VLC Media Player"
+$Mediaapp2 = "MediaInfo"
+$Mediaapp3 = "MakeMKV"
+$Mediaapp4 = "Handbrake"
+$Mediaapp5 = "PowerISO"
+$Mediaapp6 = "Audacity and FFMPEG"
+$Mediaapp7 = "Logitech MyHarmony"
+$Mediaapp8 = "MKVToolNix"
+$Mediaapp9 = "Apple iTunes"
 
 Write-Verbose "Installing Media Software 1 of 9..." -Verbose
-Write-Verbose "Installing $app1 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp1 on $env:computername..." -Verbose
 winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 2 of 9..." -Verbose
-Write-Verbose "Installing $app2 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp2 on $env:computername..." -Verbose
 winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 3 of 9..." -Verbose
-Write-Verbose "Installing $app3 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp3 on $env:computername..." -Verbose
 winget install --id GuinpinSoft.MakeMKV --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 4 of 9..." -Verbose
-Write-Verbose "Installing $app4 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp4 on $env:computername..." -Verbose
 winget install --id HandBrake.HandBrake --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 5 of 9..." -Verbose
-Write-Verbose "Installing $app5 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp5 on $env:computername..." -Verbose
 winget install --id PowerSoftware.PowerISO --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 6 of 9..." -Verbose
-Write-Verbose "Installing $app6 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp6 on $env:computername..." -Verbose
 winget install --id Audacity.Audacity --exact --accept-source-agreements  --accept-source-agreements --force
 choco install audacity-ffmpeg -y
 
 Write-Verbose "Installing Media Software 7 of 9..." -Verbose
-Write-Verbose "Installing $app7 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp7 on $env:computername..." -Verbose
 winget install --id Logitech.MyHarmony --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 8 of 9..." -Verbose
-Write-Verbose "Installing $app8 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp8 on $env:computername..." -Verbose
 winget install --id MoritzBunkus.MKVToolNix --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Installing Media Software 9 of 9..." -Verbose
-Write-Verbose "Installing $app9 on $env:computername..." -Verbose
+Write-Verbose "Installing $Mediaapp9 on $env:computername..." -Verbose
 winget install --id Apple.iTunes --exact --accept-source-agreements  --accept-source-agreements --force
 
 
@@ -57,6 +57,17 @@ Write-Host
 Write-Host "Step 2 : Web Browsing Software" -ForegroundColor Cyan
 Write-Host
 Write-Host
+
+$Browsingapp1 = "Google Chrome for Enterprise"
+$Browsingapp2 = "Mozilla Firefox"
+
+Write-Verbose "Installing Web Browsing Software 1 of 2..." -Verbose
+Write-Verbose "Installing $Browsingapp1 on $env:computername..." -Verbose
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Verbose "Installing Web Browsing Software 2 of 2..." -Verbose
+Write-Verbose "Installing $Browsingapp2 on $env:computername..." -Verbose
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Host
 Write-Host
