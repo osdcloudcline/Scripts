7c04e52a7c6ea6a439976f6bb9ec4f21ae7fbb45
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-04 09:16:51 -0400

    Update SYSTEM.ps1

--- WinGet /Install /Individual Scripts/SYSTEM.ps1
+++ WinGet /Install /Individual Scripts/SYSTEM.ps1
@@ -1,133 +1,175 @@
-$app1 = ".NET Runtime 3.1"
-$app1ver = "Version: 3.1.32"
-$app2 = ".NET Runtime 5.0"
-$app2ver = "Version: 5.0.17"
-$app3 = ".NET Runtime 6.0"
-$app3ver = "Version: 6.0.26"
-$app4 = ".NET Runtime 7.0"
-$app4ver = "Version: 7.0.15"
-$app5 = ".NET Runtime 8.0"
-$app5ver = "Version: 8.0.1"
-$app6 = "Oracle JAVA Runtime Environment 8.0"
-$app6ver = "Version: 8.0.4010.10"
-$app7 = "MS VS C++ Redistributable 2005 x64"
-$app7ver = "Version: 8.0.61000"
-$app8 = "MS VS C++ Redistributable 2005 x86"
-$app8ver = "Version: 8.0.61001"
-$app9 = "MS VS C++ Redistributable 2008 x64"
-$app9ver = "Version: 9.0.30729.6161"
-$app10 = "MS VS C++ Redistributable 2008 x86"
-$app10ver = "Version: 9.0.30729.6161"
-$app11 = "MS VS C++ Redistributable 2010 x64"
-$app11ver = "Version: 10.0.40219"
-$app12 = "MS VS C++ Redistributable 2010 x86"
-$app12ver = "Version: 10.0.40219"
-$app13 = "MS VS C++ Redistributable 2012 x64"
-$app13ver = "Version: 11.0.61030.0"
-$app14 = "MS VS C++ Redistributable 2012 x86"
-$app14ver = "Version: 11.0.61030.0"
-$app15 = "MS VS C++ Redistributable 2013 x64"
-$app15ver = "Version: 12.040664.0"
-$app16 = "MS VS C++ Redistributable 2013 x86"
-$app16ver = "Version: 12.040664.0"
-$app17 = "MS VS C++ Redistributable 2015 UWP Desktop"
-$app17ver = "Version: 14.0.33321.0"
-$app18 = "MS VS C++ Redistributable 2015-2022 x64"
-$app18ver = "Version: 14.38.33135.0"
-$app19 = "MS VS C++ Redistributable 2015-2022 x86"
-$app19ver = "Version: 14.38.33130.0"
-$app20 = "MS VS C++ Redistributable 2017"
-$app20ver = "Version: 14.16.27033"
-$app21 = "MS XNA Redistributable Framework"
-$app21ver = "Version: 4.0.30901.0"
-$app22 = ".NET Framework"
-$app22ver = "Version: 4.8.1"
-$app23 = "OpenAL Framework"
-$app23ver = "Version: 1.1"
-$app24 = "Microsoft Edge"
-$app24ver = "Version: Latest"
-$app25 = "Microsoft Windows Terminal"
-$app25ver = "Version: 1.19.10302.0"
-$app26 = "Microsoft Windows Powershell"
-$app26ver = "Version: 7.4.1.0"	
+$NETapp1 = ".NET Runtime 3.1"
+$NETapp1ver = "Version: 3.1.32"
 
+$NETapp2 = ".NET Runtime 5.0"
+$NETapp2ver = "Version: 5.0.17"
 
-Write-Host "Installing: $app1, $app1ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+$NETapp3 = ".NET Runtime 6.0"
+$NETapp3ver = "Version: 6.0.26"
+
+$NETapp4 = ".NET Runtime 7.0"
+$NETapp4ver = "Version: 7.0.15"
+
+$NETapp5 = ".NET Runtime 8.0"
+$NETapp5ver = "Version: 8.0.1"
+
+$NETapp6 = ".NET Framework 4.8.1"
+$NETapp6ver = "Version: 4.8.1"
+
+$VCRedistributableapp1 = "MS VS C++ Redistributable 2005 x64"
+$VCRedistributableapp1ver = "Version: 8.0.61000"
+
+$VCRedistributableapp2 = "MS VS C++ Redistributable 2005 x86"
+$VCRedistributableapp2ver = "Version: 8.0.61001"
+
+$VCRedistributableapp3 = "MS VS C++ Redistributable 2008 x64"
+$VCRedistributableapp3ver = "Version: 9.0.30729.6161"
+
+$VCRedistributableapp4 = "MS VS C++ Redistributable 2008 x86"
+$VCRedistributableapp4ver = "Version: 9.0.30729.6161"
+
+$VCRedistributableapp5 = "MS VS C++ Redistributable 2010 x64"
+$VCRedistributableapp5ver = "Version: 10.0.40219"
+
+$VCRedistributableapp6 = "MS VS C++ Redistributable 2010 x86"
+$VCRedistributableapp6ver = "Version: 10.0.40219"
+
+$VCRedistributableapp7 = "MS VS C++ Redistributable 2012 x64"
+$VCRedistributableapp7ver = "Version: 11.0.61030.0"
+
+$VCRedistributableapp8 = "MS VS C++ Redistributable 2012 x86"
+$VCRedistributableapp8ver = "Version: 11.0.61030.0"
+
+$VCRedistributableapp9 = "MS VS C++ Redistributable 2013 x64"
+$VCRedistributableapp9ver = "Version: 12.040664.0"
+
+$VCRedistributableapp10 = "MS VS C++ Redistributable 2013 x86"
+$VCRedistributableapp10ver = "Version: 12.040664.0"
+
+$VCRedistributableapp11 = "MS VS C++ Redistributable 2015-2022 x64"
+$VCRedistributableapp11ver = "Version: 14.38.33135.0"
+
+$VCRedistributableapp12 = "MS VS C++ Redistributable 2015-2022 x86"
+$VCRedistributableapp12ver = "Version: 14.38.33130.0"
+
+$VCRedistributableapp13 = "MS VS C++ Redistributable 2017"
+$VCRedistributableapp13ver = "Version: 14.16.27033"
+
+$JAVAapp1 = "Oracle JAVA Runtime Environment 8.0"
+$JAVAapp1ver = "Version: 8.0.4010.10"
+
+$XNAapp1 = "MS XNA Framework"
+$XNAapp1ver = "Version: 4.0.30901.0"
+
+$OpenALapp1 = "OpenAL Framework"
+$OpenALapp1ver = "Version: 1.1"
+
+$OSapp1 = "Microsoft Edge"
+$OSapp1ver = "Version: Latest"
+
+$OSapp2 = "Microsoft Windows Terminal"
+$OSapp2ver = "Version: 1.19.10302.0"
+
+$OSapp3 = "Microsoft Windows Powershell"
+$OSapp3ver = "Version: 7.4.1.0"	
+
+$SYSGroup1 = "Microsoft .NET Framework"
+$SYSGroup2 = "Microsoft C++ Redistributables"
+$SYSGroup3 = "Oracle Java Runtime Environments"
+$SYSGroup4 = "Microsoft XNA Framework"
+$SYSGroup5 = "Creative Technology Open AL"
+$SYSGroup6 = "OS Software"
+
+Write-Host "Installing $SYSGroup1...." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $NETapp1, $NETapp1ver..." -Verbose
 winget install --id  Microsoft.DotNet.Runtime.3_1 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing: $app2, $app2ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $NETapp2, $NETapp2ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.5 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app3, $app3ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $NETapp3, $NETapp3ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.6 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app4, $app4ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $NETapp4, $NETapp4ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.7 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app5, $app5ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $NETapp5, $NETapp5ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.8 --accept-source-agreements --accept-source-agreements --force
-		
-Write-Host "Installing: $app6, $app6ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget install --id Oracle.JavaRuntimeEnvironment --exact --accept-source-agreements --accept-source-agreements --force
-		
-Write-Host "Installing: $app7, $app7ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+
+Write-Verbose "Installing $NETapp6, $NETapp6ver..." -Verbose 
+choco install dotnetfx -y
+
+
+Write-Host "Installing $SYSGroup2...." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $VCRedistributableapp1, $VCRedistributableapp1ver..." -Verbose
 winget install --id Microsoft.VCRedist.2005.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app8, $app8ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp2, $VCRedistributableapp2ver..." -Verbose
 winget install --id Microsoft.VCRedist.2005.x86 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app9, $app9ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp3, $VCRedistributableapp3ver..." -Verbose
 winget install --id Microsoft.VCRedist.2008.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app10, $app10ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp4, $VCRedistributableapp4ver..." -Verbose
 winget install --id Microsoft.VCRedist.2008.x86 --exact --accept-source-agreements --accept-source-agreements  --force
 		
-Write-Host "Installing: $app11, $app11ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp5, $VCRedistributableapp5ver..." -Verbose
 winget install --id Microsoft.VCRedist.2010.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app12, $app12ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp6, $VCRedistributableapp6ver..." -Verbose
 winget install --id Microsoft.VCRedist.2010.x86 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app13, $app13ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp7, $VCRedistributableapp7ver..." -Verbose
 winget install --id Microsoft.VCRedist.2012.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app14, $app14ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp8, $VCRedistributableapp8ver..." -Verbose
 winget install --id Microsoft.VCRedist.2012.x86 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app15, $app15ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp9, $VCRedistributableapp9ver..." -Verbose
 winget install --id Microsoft.VCRedist.2013.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app16, $app16ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp10, $VCRedistributableapp10ver..." -Verbose
 winget install --id Microsoft.VCRedist.2013.x86 --exact --accept-source-agreements --accept-source-agreements --force
-		
-Write-Host "Installing: $app17, $app17ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget install Microsoft.VCLibs.Desktop.14 --exact --accept-source-agreements --accept-source-agreements --force
-		
-Write-Host "Installing: $app18, $app18ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+				
+Write-Verbose "Installing $VCRedistributableapp11, $VCRedistributableapp12ver..." -Verbose
 winget install --id Microsoft.VCRedist.2015+.x64 --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app19, $app19ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $VCRedistributableapp12, $VCRedistributableapp12ver..." -Verbose
 winget install --id Microsoft.VCRedist.2015+.x86 --exact --accept-source-agreements --accept-source-agreements --force
-		
-Write-Host "Installing: $app20, $app20ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+
+Write-Verbose "Installing $VCRedistributableapp13, $VCRedistributableapp13ver..." -Verbose
 choco install vcredist2017 -y
+
+
+Write-Host "Installing $SYSGroup3...." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $JAVAapp1, $JAVAapp1ver..." -Verbose
+winget install --id Oracle.JavaRuntimeEnvironment --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app21, $app21ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+
+Write-Host "Installing $SYSGroup4...." -ForegroundColor Cyan
+Write-Host		
+Write-Verbose "Installing $XNAapp1, $XNAapp1ver..." -Verbose
 winget install --id Microsoft.XNARedist --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app22, $app22ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-choco install dotnetfx -y
-		
-Write-Host "Installing: $app23, $app23ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+
+Write-Host "Installing $SYSGroup5...." -ForegroundColor Cyan
+Write-Host		
+Write-Verbose "Installing $OpenALapp1, $OpenALapp1ver..." -Verbose
 choco install openal -y
-		
-Write-Host "Installing: $app24, $app24ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+
+
+Write-Host "Installing $SYSGroup6...." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $OSapp1, $OSapp1ver..." -Verbose
 winget install --id Microsoft.Edge --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app25, $app25ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $OSapp2, $OSapp2ver..." -Verbose
 winget install --id Microsoft.WindowsTerminal --exact --accept-source-agreements --accept-source-agreements --force
 		
-Write-Host "Installing: $app26, $app26ver on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Verbose "Installing $OSapp3, $OSapp3ver..." -Verbose
 winget install --id Microsoft.PowerShell --exact --accept-source-agreements --accept-source-agreements --force
 	
 Write-Host "Finished Installing System related software..." -ForegroundColor Yellow
