c6e6d72768c95c0f8dcfbf0b8ee4e5e390c53706
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-04 19:09:56 -0400

    Update ADV.ps1

--- WinGet /Install /Audit Mode Install/ADV.ps1
+++ WinGet /Install /Audit Mode Install/ADV.ps1
@@ -232,7 +232,7 @@ choco install cinebench -y
 Write-Host "Installing System Utilities Software 12 of 13..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp12..." -Verbose
-
+winget.exe install --id LianLi.LConnect3 --exact --accept-source-agreements  --accept-package-agreements --force
 
 Write-Host "Installing System Utilities Software 13 of 13..." -ForegroundColor Cyan
 Write-Host
