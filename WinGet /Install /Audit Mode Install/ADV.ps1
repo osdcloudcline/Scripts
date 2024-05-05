01e3cf3cfcbae2b608ab339c44cbd0e6c586ef1f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-05 10:51:48 -0400

    Update ADV.ps1

--- WinGet /Install /Audit Mode Install/ADV.ps1
+++ WinGet /Install /Audit Mode Install/ADV.ps1
@@ -144,40 +144,48 @@ $Utilitiesapp17 = "Elgato Stream Deck"
 $Utilitiesapp18 = "G.Skill WigiDash"
 $Utilitiesapp19 = "G.Skill Trident Z and Z5 Lighting Control Software"
 $Utilitiesapp20 = "Sapphire TriXX Video Card Software"
+$Utilitiesapp21 = "Display Driver Uninstaller"
+$Utilitiesapp22 = "Rufus"
+$Utilitiesapp23 = "Sonos"
+$Utilitiesapp24 = "SoundBlaster Command"
+$Utilitiesapp25 = "Cystal Disk Info"
+$Utilitiesapp26 = "Crystal DiskMark"
+$Utilitiesapp27 = "Logitech Unifying Software"
+$Utilitiesapp28 = "Logitech Options"
 
 Write-Host "Installing System Utilities Applications on $env:computername..." -ForegroundColor Green
 
-Write-Host "Installing System Utilities Software 1 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 1 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp1 on $env:computername..." -Verbose
 winget install --id FinalWire.AIDA64.Engineer --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 2 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 2 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp2..." -Verbose
 winget install --id 7zip.7zip --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 3 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 3 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp3..." -Verbose
 winget install --id Piriform.CCleaner --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 4 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 4 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp4..." -Verbose
 winget install --id IObit.DriverBooster --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 5 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 5 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp5..." -Verbose
 winget install --id XPDLS1XBTXVPP4 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 6 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 6 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp6..." -Verbose
 winget install --id Microsoft.WindowsAdminCenter --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing System Utilities Software 7 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 7 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Acquiring $Utilitiesapp7 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
@@ -191,7 +199,7 @@ Start-Process -FilePath $Win11_22H2SDK
 
 pause
 
-Write-Host "Installing System Utilities Software 8 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 8 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Acquiring $Utilitiesapp8 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
@@ -205,7 +213,7 @@ Start-Process -FilePath $Win11_22H2ADK
 
 pause
 
-Write-Host "Installing System Utilities Software 9 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 9 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Acquiring $Utilitiesapp7 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose
@@ -219,7 +227,7 @@ Start-Process -FilePath $Win11_22H2PEADK
 
 pause
 
-Write-Host "Installing System Utilities Software 10 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 10 of 28..." -ForegroundColor Cyan
 Write-Host
 $OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
 $StarwindV2VConverterURL = "https://www.starwindsoftware.com/tmplink/starwindconverter.exe"
@@ -231,27 +239,27 @@ Start-Process -FilePath $V2VConverter
 
 pause
 
-Write-Host "Installing System Utilities Software 11 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 11 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp11..." -Verbose
 choco install cinebench -y
 
-Write-Host "Installing System Utilities Software 12 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 12 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp12..." -Verbose
 winget install --id LianLi.LConnect3 --exact --accept-source-agreements  --accept-package-agreements --force
 
-Write-Host "Installing System Utilities Software 13 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 13 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp13..." -Verbose
 winget install --id Rem0o.FanControl --exact --accept-source-agreements  --accept-package-agreements --force
 
-Write-Host "Installing System Utilities Software 14 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 14 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp14..." -Verbose
 winget install --id Samsung.SamsungMagician --exact --accept-source-agreements  --accept-package-agreements --force
 
-Write-Host "Installing System Utilities Software 15 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 15 of 28..." -ForegroundColor Cyan
 Write-Host
 $3DMarkLocation = "C:\OSDCloud\downloads\3DMark\3dmark-setup.exe"
 Write-Verbose "Installing $Utilitiesapp15..." -Verbose
@@ -259,7 +267,7 @@ Start-Process -FilePath $3DMarkLocation
 
 pause
 
-Write-Host "Installing System Utilities Software 16 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 16 of 28..." -ForegroundColor Cyan
 Write-Host
 $PCMarkLocation = "C:\OSDCloud\downloads\PCMark10\INSTALL.exe"
 Write-Verbose "Installing $Utilitiesapp16..." -Verbose
@@ -267,12 +275,12 @@ Start-Process -FilePath $PCMarkLocation
 
 pause
 
-Write-Host "Installing System Utilities Software 17 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 17 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp17..." -Verbose
 winget install --id Elgato.StreamDeck --exact --accept-source-agreements  --accept-package-agreements --force
 
-Write-Host "Installing System Utilities Software 18 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 18 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp18..." -Verbose
 $GSkillWigiDashSoftwareURL = "https://www.gskill.com/gskill-device/wigidash/WigiDash_Manager_Setup_v10883138720.zip"
@@ -295,7 +303,7 @@ Write-Verbose "Expanding WigiDash AIDA64 Theme ZIP file..." -Verbose
 Expand-Archive -Path "$WigiDashAIDA64Themedownloads\gskill_wigidash_aida64_sensorpanel_theme_v1.4.zip" -Destination $OSDCloudGSkillWigiDashSWdownloads
 
 
-Write-Host "Installing System Utilities Software 19 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 19 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp19..." -Verbose
 $GSkillRAMLightingSoftwareURL = "https://www.gskill.com/gskill-device/memory/G.SKILL-Trident-Z-Lighting-Control-v1.00.35.zip"
@@ -311,7 +319,7 @@ Write-Host
 Write-Verbose "Expanding $Utilitiesapp19 ZIP file..." -Verbose
 Expand-Archive -Path "$OSDCloudGSkillRAMLightingSWdownloads\G.SKILL-Trident-Z-Lighting-Control-v1.00.35.zip" -Destination $OSDCloudGSkillRAMLightingSWdownloads
 
-Write-Host "Installing System Utilities Software 20 of 20..." -ForegroundColor Cyan
+Write-Host "Installing System Utilities Software 20 of 28..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Installing $Utilitiesapp20..." -Verbose
 $SapphireTRIXXURL = "https://cdn.sapphiretech.global/assets/software/TRIXX_9.7.0.zip"
@@ -322,6 +330,49 @@ Save-WebFile -SourceUrl $SapphireTRIXXURL -DestinationDirectory $OSDCloudSapphir
 Write-Host
 Write-Verbose "Expanding $Utilitiesapp20 ZIP file..." -Verbose
 Expand-Archive -Path "$OSDCloudSapphiredownloads\TRIXX_9.7.0.zip" -Destination $OSDCloudSapphiredownloads
+
+Write-Host "Installing System Utilities Software 21 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp21..." -Verbose
+winget install --id Wagnardsoft.DisplayDriverUninstaller --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 22 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp22..." -Verbose
+winget install --id Rufus.Rufus --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 23 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp23..." -Verbose
+winget install --id Sonos.Controller --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 24 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp24..." -Verbose
+winget install --id CreativeTechnology.SoundBlasterCommand --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 25 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp25..." -Verbose
+winget install --id CrystalDewWorld.CrystalDiskInfo --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 26 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp26..." -Verbose
+winget install --id CrystalDewWorld.CrystalDiskMark --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 27 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp27..." -Verbose
+winget install --id Logitech.UnifyingSoftware --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "Installing System Utilities Software 28 of 28..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Utilitiesapp28..." -Verbose
+winget install --id Logitech.Options --exact --accept-source-agreements  --accept-package-agreements --force
+
+Write-Host "All System Utility software successfully installed" -ForegroundColor Green
+
 Stop-Transcript
 
 $OSDProgrammingSoftwareLogs = "C:\OSDCloud\DeploymentLogs\ADV\$env:computername-OSDProgrammingSoftware.log"
@@ -337,8 +388,9 @@ Write-Host
 
 $Programmingapp1 = "VS Code"
 $Programmingapp2 = "Visual Studio 2022 Enterprise"
+$Devapp1 = "GitHub Desktop"
 
-Write-Host "Installing Programming Applications on $env:computername..." -ForegroundColor Green
+Write-Host "Installing Programming/Dev Applications on $env:computername..." -ForegroundColor Green
 
 Write-Host "Installing Programming Software 1 of 2..." -ForegroundColor Cyan
 Write-Host
@@ -350,7 +402,14 @@ Write-Host
 Write-Verbose "Installing $Programmingapp2..." -Verbose
 choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
 
+Write-Host "Installing Dev Software 1 of 1..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Devapp1..." -Verbose
+winget install --id GitHub.GitHubDesktop --exact --accept-source-agreements  --accept-source-agreements --force
+
 Stop-Transcript
+
+
 
 $OSDProductivitySoftwareLogs = "C:\OSDCloud\DeploymentLogs\ADV\$env:computername-OSDProductivitySoftware.log"
 Start-Transcript -Path $OSDProductivitySoftwareLogs
