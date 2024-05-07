f1aa4cbc4c9daacd51dab75938a1602b209761a5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-07 06:08:06 -0400

    Update CorporateSM.ps1

--- Start Menu Configurations/CorporateSM.ps1
+++ Start Menu Configurations/CorporateSM.ps1
@@ -1,2 +1,7 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
+
+$CORPLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\Corporate" -IsValid){
+Write-Host "This computer has been installed with the Advanced Configuration of software" -ForgroundColor Green
+Write-Host
+Write-Host "The Corporate Start Menu layout will not be acquired from OSDCloud Cline\Scripts\Start Menu Configurations and configured on $env:computername..." -ForegroundColor Cyan
