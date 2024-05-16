be3edd2c5570d8e5e67740ca5a360c0b75aad767
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:25:44 -0400

    Update StandardMedia.ps1

--- WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
@@ -2,3 +2,20 @@ $app1 = "VLC Media Player"
 $app2 = "MediaInfo"
 $app3 = "MKVToolNix"
 $app4 = "Apple iTunes"
+
+# Media-related software
+Write-Host "Installing Media Applications..." -ForegroundColor Green
+
+Write-Verbose "Processing: $app1..." -Verbose
+
+
+
+Write-Verbose "Processing: $app2..." -Verbose
+
+
+
+Write-Verbose "Processing: $app3..." -Verbose
+
+
+
+Write-Verbose "Processing: $app4..." -Verbose
