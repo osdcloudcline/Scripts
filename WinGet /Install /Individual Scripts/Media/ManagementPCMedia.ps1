064ee5c87d7c3c39545897c75c4a4c82448232fb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 01:00:34 -0400

    Update ManagementPCMedia.ps1

--- WinGet /Install /Individual Scripts/Media/ManagementPCMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/ManagementPCMedia.ps1
@@ -1,5 +1,9 @@
 Start-Transcript -Path "C:\Logs\Powershell\Management PC\$env:computername-ManagementPCMediaInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
 $app1 = "VLC Media Player"
 $app2 = "MediaInfo"
 
