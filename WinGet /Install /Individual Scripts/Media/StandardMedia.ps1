d75647943bd3d34627733e6d2baf6766010da2ea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:29:18 -0400

    Update StandardMedia.ps1

--- WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
@@ -15,15 +15,16 @@ winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-so
 
 Write-Verbose "Processing: $app2..." -Verbose
 Write-Host
-Write-Host "Installing $app1..." -ForegroundColor Cyan
+Write-Host "Installing $app2..." -ForegroundColor Cyan
 
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app3..." -Verbose
 Write-Host
-Write-Host "Installing $app1..." -ForegroundColor Cyan
+Write-Host "Installing $app3..." -ForegroundColor Cyan
 
 
 Write-Verbose "Processing: $app4..." -Verbose
 Write-Host
-Write-Host "Installing $app1..." -ForegroundColor Cyan
+Write-Host "Installing $app4..." -ForegroundColor Cyan
 
