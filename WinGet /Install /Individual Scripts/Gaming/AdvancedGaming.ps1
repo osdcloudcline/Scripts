9458e142f30b5e486fa99a33c99af7487c787379
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 08:06:42 -0400

    Update AdvancedGaming.ps1

--- WinGet /Install /Individual Scripts/Gaming/AdvancedGaming.ps1
+++ WinGet /Install /Individual Scripts/Gaming/AdvancedGaming.ps1
@@ -12,3 +12,7 @@ Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
