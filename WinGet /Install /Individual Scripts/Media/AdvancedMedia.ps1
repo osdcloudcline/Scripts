38f780da39535449cc00b4a49ac4a6330adda290
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:58:59 -0400

    Update AdvancedMedia.ps1

--- WinGet /Install /Individual Scripts/Media/AdvancedMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/AdvancedMedia.ps1
@@ -1,5 +1,9 @@
 Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMediaInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
 $app1 = "VLC Media Player"
 $app2 = "MediaInfo"
 $app3 = "MKVToolNix"
