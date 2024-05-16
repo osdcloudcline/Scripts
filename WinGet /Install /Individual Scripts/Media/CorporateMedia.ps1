f7037ff4ffcd0f3c516a1f6127e9e594566cc12e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:47:42 -0400

    Update CorporateMedia.ps1

--- WinGet /Install /Individual Scripts/Media/CorporateMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/CorporateMedia.ps1
@@ -12,3 +12,11 @@ Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
 
 winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Verbose "Processing: $app2..." -Verbose
+Write-Host
+Write-Host "Installing $app2..." -ForegroundColor Cyan
+
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
