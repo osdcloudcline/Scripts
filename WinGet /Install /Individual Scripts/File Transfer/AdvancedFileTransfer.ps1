221b219bc4f4889f647a0b628763aefea55d2a99
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 07:54:26 -0400

    Update AdvancedFileTransfer.ps1

--- WinGet /Install /Individual Scripts/File Transfer/AdvancedFileTransfer.ps1
+++ WinGet /Install /Individual Scripts/File Transfer/AdvancedFileTransfer.ps1
@@ -1,6 +1,10 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedBrowsersInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedFileTransferInstall.log"
 
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "FileZilla"
+$app2 = "PuTTy"
+$app3 = "WinSCP"
 
