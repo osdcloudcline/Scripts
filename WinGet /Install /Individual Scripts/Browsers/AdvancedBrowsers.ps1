b0bec012202e9c4d9e00c27d977fb947e5f7a8ef
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:54:33 -0400

    Create AdvancedBrowsers.ps1

---
+++ WinGet /Install /Individual Scripts/Browsers/AdvancedBrowsers.ps1
@@ -0,0 +1,10 @@
+
+
+
+
+
+Write-Host "Installing Web Browsing Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
