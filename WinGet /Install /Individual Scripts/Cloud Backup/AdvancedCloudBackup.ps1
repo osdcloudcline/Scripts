8e4cd131ebe9714353b8093f61646b3f883ab6fa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 07:35:57 -0400

    Update AdvancedCloudBackup.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/AdvancedCloudBackup.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/AdvancedCloudBackup.ps1
@@ -3,3 +3,44 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedCl
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "Google Drive"
+$app2 = "Nextcloud Desktop Client"
+$app3 = "DropBox"
+$app4 = "Microsoft OneDrive"
+$app5 = "User Profile Backup and Restore"
+
+Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
+Write-Host "Beginning Application Installation on $env:computername..." -ForegroundColor Green
+
+# Cloud Backup software
+Write-Host "Installing Cloud Backup Applications on $env:computername..." -ForegroundColor Green
+
+Write-Host "Installing: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing: $app3 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing: $app4 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
+
+$UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
+$UPBRFilePath = "C:\downloads\UserProfileBackupRestore.exe"
+$UPBRFilePathNewName = "C:\downloads\UPBR.exe"
+$UPBRDir = "C:\downloads"
+Save-WebFile -SourceUrl $UPBR -DestinationDirectory $UPBRDir
+Rename-Item -Path $UPBRFilePath  -NewName $UPBRFilePathNewName
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
+$Shortcut.TargetPath = "C:\downloads\UPBR.exe"
+$Shortcut.Save()
