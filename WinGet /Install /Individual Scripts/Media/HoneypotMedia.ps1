7c465ca67ad56090a5b2df7dad76987d3b9b055d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:49:09 -0400

    Create HoneypotMedia.ps1

---
+++ WinGet /Install /Individual Scripts/Media/HoneypotMedia.ps1
@@ -0,0 +1,22 @@
+Start-Transcript -Path "C:\Logs\Powershell\Honeypot\$env:computername-HoneypotMediaInstall.log"
+
+$app1 = "VLC Media Player"
+$app2 = "MediaInfo"
+
+
+# Media-related software
+Write-Host "Installing Media Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Verbose "Processing: $app2..." -Verbose
+Write-Host
+Write-Host "Installing $app2..." -ForegroundColor Cyan
+
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
