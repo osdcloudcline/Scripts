bf5c67e5ffaec454330b167b1a5b80207d7461e6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:55:33 -0400

    Update AdvancedBrowsers.ps1

--- WinGet /Install /Individual Scripts/Browsers/AdvancedBrowsers.ps1
+++ WinGet /Install /Individual Scripts/Browsers/AdvancedBrowsers.ps1
@@ -1,6 +1,5 @@
-
-
-
+$app1 = "Google Chrome for Enterprise"
+$app2 = "Mozilla Firefox"
 
 
 Write-Host "Installing Web Browsing Applications..." -ForegroundColor Green
@@ -8,3 +7,9 @@ Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+
+
+Write-Verbose "Processing: $app2..." -Verbose
+Write-Host
+Write-Host "Installing $app2..." -ForegroundColor Cyan
