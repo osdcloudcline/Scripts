64e786680df54e6ea98f31804e63c1bd0c8e00c5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-16 00:28:27 -0400

    Update StandardMedia.ps1

--- WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/StandardMedia.ps1
@@ -5,17 +5,25 @@ $app4 = "Apple iTunes"
 
 # Media-related software
 Write-Host "Installing Media Applications..." -ForegroundColor Green
-
+Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
 
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
 
 
 Write-Verbose "Processing: $app2..." -Verbose
-
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
 
 
 Write-Verbose "Processing: $app3..." -Verbose
-
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
 
 
 Write-Verbose "Processing: $app4..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+
