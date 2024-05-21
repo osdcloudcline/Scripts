cb3eb534109feb73253b0552cd8bf09e7eec49e2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-21 12:02:36 -0400

    Update AdvancedSM.ps1

--- Start Menu Configurations/AdvancedSM.ps1
+++ Start Menu Configurations/AdvancedSM.ps1
@@ -6,3 +6,13 @@ Write-Host "This computer has been installed with the Advanced Configuration of 
 Write-Host
 Write-Host "The Advanced Start Menu layout will not be acquired from OSDCloud Cline\Scripts\Start Menu Configurations and configured on $env:computername..." -ForegroundColor Cyan
 
+$StartMenudownload = "C:\OSDCloud\GitHub\downloads\StartMenu\Advanced"
+
+Write-Verbose "Acquiring ADVANCED Start2.bin for Start Menu Configuration..." -Verbose
+Save-WebFile -SourceUrl -DestinationDirectory $StartMenudownload
+
+Write-Host "Applying changes to Start Menu on $env:computername..." -ForegroundColor Cyan 
+$StartMenuBINFile = "C:\OSDCloud\GitHub\downloads\StartMenu\Advanced\Start2.bin"
+$SMDestination = "C:\Users\$env:username\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
+Write-Verbose "Copying Start Menu file to $SMDestination...." -Verbose
+Copy-Item -Path $StartMenuBINFile -Destination $SMDestination -Force 
