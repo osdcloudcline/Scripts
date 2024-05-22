231eaab2b8f31774c9c1649bee0e3b8f7a5ae4fe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-22 00:33:46 -0400

    Update AdminMainMenu.ps1

--- Administrative/AdminMainMenu.ps1
+++ Administrative/AdminMainMenu.ps1
@@ -1,1 +1,75 @@
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OSD Cloud -  Administrative Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
+cls
 
+
+
+       $os = Get-CimInstance -ClassName Win32_OperatingSystem
+       $PCName = "$env:computername"
+       $OSVerison = $($os.Version)
+       $UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+       $FullOS = $($os.Caption)
+       $OSBuild = $($os.Version) + "." + $UBR
+       $DisplayVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name DisplayVersion).DisplayVersion
+       $EditionID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name EditionID).EditionID
+       $CompleteOSInfo = "$FullOS" + " $OSBuild" + " $DisplayVersion"
+       $EdgeEXE = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe' "(default)"
+       $EdgeBrowser = (Get-Item $EdgeEXE).VersionInfo.ProductVersion
+       Write-Host
+       Write-Host         "Running Computer Info:" -ForegroundColor Green 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Running Operating System: $CompleteOSInfo" -Verbose  
+       Write-Verbose "Operating System: $FullOS" -Verbose 
+       Write-Verbose "Build Number: $OSBuild" -Verbose 
+       Write-Verbose "Display Version: $DisplayVersion" -Verbose
+       Write-Verbose "OS Edition: $EditionID" -Verbose 
+       Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose 
+pause
+Clear-Host
+Write-Host "======= $Title ======"
+Write-Host " 1. Integrate Updates into Offline/Mounted OS Image"
+Write-Host " 2. Integrate Drivers into Offline/Mounted OS Image"
+Write-Host " 3. Integrate Registry Entries into Offline/Mounted OS Image"
+Write-Host " 4. Start OSD Builder"
+Write-Host " 5. Return to Main Menu"
+Write-Host " 6. Exit PowerShell"
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+
+  '1' { cls
+        
+        }
+  '2' { cls
+        
+        }
+  '3' { cls
+       
+        }
+  '4' { cls
+        
+        }
+  '5' { cls
+        
+        }
+  '6' { cls
+        
+        exit
+        }
+
+  
+    }
+    }
+    until ($selection -eq '5'){}
+    }
+
+Show-MainMenu
