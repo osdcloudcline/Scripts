e733f5e1487c27ef7e09b45142c5ea1a3a35a6d3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-23 09:11:35 -0400

    Update Registry.ps1

--- Administrative/OS Integration/Registry.ps1
+++ Administrative/OS Integration/Registry.ps1
@@ -1,1 +1,99 @@
+########################################################## 
+# 
+# USBUpdate.ps1
+#
+# A PS Script to update Windows 10 install USB. 
+# 
+# You are free to edit & share this script as long as
+# source TenForums.com is mentioned.
+#
+# *** Twitter.com/TenForums *** Facebook.com/TenForums ***
+# 
+# Script by Kari 
+# - TenForums.com/members/kari.html
+# - Twitter.com/KariTheFinn
+# - YouTube.com/KariTheFinn
+#
+# 'Use-RunAs' function to check if script was launched
+# in normal user mode and elevating it if necessary by
+# Matt Painter (Microsoft TechNet Script Center)
+# https://gallery.technet.microsoft.com/scriptcenter/ 
+#
+##########################################################
 
+##########################################################
+# Checking if PS is running elevated. If not, elevating it
+##########################################################   
+
+function Use-RunAs 
+{    
+    # Check if script is running as Administrator and if not elevate it
+    # Use Check Switch to check if admin 
+     
+    param([Switch]$Check) 
+     
+    $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()` 
+        ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
+         
+    if ($Check) { return $IsAdmin }   
+      
+    if ($MyInvocation.ScriptName -ne "") 
+    {  
+        if (-not $IsAdmin)  
+          {  
+            try 
+            {  
+                $arg = "-file `"$($MyInvocation.ScriptName)`"" 
+                Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList $arg -ErrorAction 'stop'  
+            } 
+            catch 
+            { 
+                Write-Warning "Error - Failed to restart script elevated"  
+                break               
+            } 
+            exit 
+        }  
+    }  
+} 
+
+Use-RunAs 
+
+##########################################################
+# Show short instructions to user
+##########################################################   
+
+cls
+Write-Host                                                                       
+Write-Host ' This script will integrate registry files with Windows install media '
+Write-Host ' downloaded from OSDCloud Cline repo'
+Write-Host 
+Write-Host ' Please notice that the process will take quite some time, depending'
+Write-Host ' on amount and size of registry entry being applied to Windows image. '
+Write-Host
+Write-Host ' If you already have a bootable Windows 10 install media on USB '
+Write-Host ' flash drive, plug it in now.'
+Write-Host 
+Write-Host ' If you want to upgrade an ISO instead, mount (double click) a Windows'
+Write-Host ' ISO image and copy its content to a folder on local PC, for instance'
+Write-Host ' "D:\ISO_Files". Make sure the folder has no other content.'
+Write-Host 
+Write-Host ' When ISO files have been copied to a hard disk folder, or USB drive'
+Write-Host ' has been plugged in, press Enter to start.'
+Write-Host 
+Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' Notice that you cannot use this script to update an ESD based install' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' media like for instance ISO / USB made with Media Creation Tool.     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' You must first convert "install.esd" file to "install.wim". See      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' TenForums tutorial "Convert ESD to WIM":' -ForegroundColor DarkBlue -BackgroundColor White -NoNewline
+Write-Host ' http://w10g.eu/esd2wim      ' -ForegroundColor DarkCyan -BackgroundColor White
+Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host
+Write-Host ' ' -NoNewline
+pause
+ 
+##########################################################
+# Delete possible old log files from previous runs
+##########################################################
+
+if (Test-Path C:\OSDCloud\Logs\OSUpdates\WUSuccess.log) {Remove-Item C:\OSDCloud\Logs\OSRegistry\RegistrySuccess.log}
+if (Test-Path C:\OSDCloud\Logs\OSUpdates\WUFail.log) {Remove-Item C:\OSDCloud\Logs\OSRegistry\RegistryFail.log}
