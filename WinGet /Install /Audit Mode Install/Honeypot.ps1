050443bedd683dc8a2b0178701426b40ba9d7e9e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-26 11:40:17 -0400

    Update Honeypot.ps1

--- WinGet /Install /Audit Mode Install/Honeypot.ps1
+++ WinGet /Install /Audit Mode Install/Honeypot.ps1
@@ -1,4 +1,4 @@
-$OSDModuleLogs = "C:\OSDCloud\DeploymentLogs\ADV\$env:computername-OSModule.log"
+$OSDModuleLogs = "C:\OSDCloud\DeploymentLogs\Honeypot\$env:computername-OSModule.log"
 Start-Transcript -Path $OSDModuleLogs
 
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
@@ -6,4 +6,18 @@ Import-Module -Name OSD -Force
 
 Stop-Transcript
 
+$OSDSoftwareLogs = "C:\OSDCloud\DeploymentLogs\Honeypot\$env:computername-OSSoftware.log"
+Start-Transcript -Path $OSDSoftwareLogs
 
+$OSSoftware1 = "IPVanish"
+
+
+
+Write-Host "Acquiring Software..." -ForegroundColor Cyan 
+Write-Host
+Write-Verbose "Downloading $OSSoftware1..." -Verbose
+$VPN = "https://www.ipvanish.com/software/setup-prod-v2/ipvanish-setup.exe"
+$Destination = "C:\OSDCloud\GitHub\downloads\Software"
+Save-WebFile -SourceUrl $VPN -DestinationDirectory $Destination
+
+Stop-Transcript
