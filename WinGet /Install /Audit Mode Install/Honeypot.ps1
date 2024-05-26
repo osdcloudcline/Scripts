f1ee79a3efbfd942775ce0b406f4ec7b72e77995
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-25 23:18:52 -0400

    Update Honeypot.ps1

--- WinGet /Install /Audit Mode Install/Honeypot.ps1
+++ WinGet /Install /Audit Mode Install/Honeypot.ps1
@@ -1,1 +1,9 @@
+$OSDModuleLogs = "C:\OSDCloud\DeploymentLogs\ADV\$env:computername-OSModule.log"
+Start-Transcript -Path $OSDModuleLogs
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+Stop-Transcript
+
 
