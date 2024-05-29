fb3b7731902871d1b3c9e948fa0723044e861e3c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-29 17:51:12 -0400

    Update OS.ps1

--- OS Downloads/OS.ps1
+++ OS Downloads/OS.ps1
@@ -134,7 +134,8 @@ Write-Host " 2. Download and Create Windows 11 22H2 ISO"
 Write-Host " 3. Download and Create Windows 11 23H2 ISO"
 Write-Host " 4. Download and Create Windows 11 24H2 ISO"
 Write-Host " 5. Download and Create Windows 11 25H2 ISO"
-Write-Host " 6. Exit PowerShell"
+Write-Host " 6. Download and Create ALL Windows 11 ISO"
+Write-Host " 7. Exit PowerShell"
 do 
 {
   $selection = Read-Host 'Please choose an option'
@@ -162,12 +163,15 @@ do
         Invoke-Expression $($Win1125H2.Content)
         }
   '6' { cls
+        $ALLWin11 = Invoke-WEbRequest("https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/Windows%2011/ALLWin11.ps1")
+        Invoke-Expression $($ALLWin11.Content)
+  '7' { cls
         exit
     
         }
      }
     }
-    until ($selection -eq '6'){exit}
+    until ($selection -eq '7'){exit}
     }
 
 
