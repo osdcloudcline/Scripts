8245e3bb82186f2628bea34e7d6f34984ac99e37
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-29 17:48:25 -0400

    Update ALLWin11.ps1

--- OS Downloads/Windows 11/ALLWin11.ps1
+++ OS Downloads/Windows 11/ALLWin11.ps1
@@ -22,7 +22,11 @@ pause
 
 Write-Host
 Write-Host "Processing: Windows 11 24H2 Build 26100.712...." -ForegroundColor Cyan 
+Get-Win1124H2
+pause
 
+Write-Host
+Write-Host "Processing: Windows 11 25H2 Build ...." -ForegroundColor Cyan 
 
 Function Get-Win1121H2(){
 ################### ISO Download, Extraction and Creation Variables region ##########################
@@ -198,4 +202,65 @@ Write-Host
 Write-Host "Completed Processing: Windows 11 23H2 Build 22631.3668...." -ForegroundColor Green 
 }
 
+Function Get-Win1124H2(){
+
+################### ISO Download, Extraction and Creation Variables region ##########################
+
+$ZIPDestination = "$env:userprofile\Downloads\OS\Windows11\24H2"
+
+$Windows1124H2URL = "https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/Windows%2011/24H2/26100.712_amd64_en-us_multi_0c393b5f_convert_virtual.zip"
+
+$ExtractionLocation = $ZIPDestination
+
+$Windows11Downloader = "$ZIPDestination\uup_download_windows.cmd"
+
+########################################################################################
+
+Write-Host "Downloading Windows 11 24H2 ZIP File..." -ForegroundColor Cyan 
+
+Save-WebFile -SourceUrl $Windows1124H2URL -DestinationDirectory $ZIPDestination
+
+Write-Host "Expanding Windows 11 24H2 ZIP File..." -ForegroundColor Cyan 
+
+Expand-Archive -Path "$ZIPDestination\26100.712_amd64_en-us_multi_0c393b5f_convert_virtual.zip" -DestinationPath $ExtractionLocation
+Write-Host
+Write-Host "Downloading Windows 11 23H2 Build: 26100.712..." -ForegroundColor Cyan 
+
+Start-Process -FilePath $Windows11Downloader
+
+pause
+
+#######    Delete Items Variables region ########################
+
+$binfolder = "$ZIPDestination\bin"
+$Driversfolder = "$ZIPDestination\Drivers"
+$filesfolder = "$ZIPDestination\files"
+$UUPsfolder = "$ZIPDestination\UUPs"
+
+#####################################################
+
+Write-Host "Deleting Subfolders and Files Windows 11 24H2 Source Directory..." -ForegroundColor Cyan 
+
+rd -Path "$binfolder" -Recurse -Force
+rd -Path "$Driversfolder" -Recurse -Force
+rd -Path "$filesfolder" -Recurse -Force
+rd -Path "$UUPsfolder" -Recurse -Force
+
+Remove-Item -Path "$ZIPDestination\*.log" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.ini" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.cmd" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.txt" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.html" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.md" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.sh" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.zip" -Recurse -Force
+
+Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
+Write-Host
+Write-Host "Completed Processing: Windows 11 24H2 Build 26100.712...." -ForegroundColor Green 
+}
+
+Function Get-Win1125H2(){
+
+}
 Stop-Transcript
