43ebbd02f810920af4ee82960319b1f31d45fae8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-29 10:28:29 -0400

    Update Win10.ps1

--- OS Downloads/Windows 10/Win10.ps1
+++ OS Downloads/Windows 10/Win10.ps1
@@ -6,7 +6,7 @@ Get-Date
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
-# Download Zip file region
+################### ISO Download, Extraction and Creation Variables region ##########################
 
 $ZIPDestination = "$env:userprofile\Downloads\OS\Windows10\22H2"
 
@@ -15,6 +15,8 @@ $Windows10URL = "https://github.com/osdcloudcline/Scripts/raw/main/OS%20Download
 $ExtractLocation = $ZIPDestination
 
 $Windows10Downloader = "$ZIPDestination\uup_download_windows.cmd"
+
+########################################################################################
 
 Write-Host "Downloading Windows 10 22H2 ZIP File..." -ForegroundColor Cyan 
 
@@ -27,3 +29,33 @@ Expand-Archive -Path "$ZIPDestination\19045.4472_amd64_en-us_multi_39143328_conv
 Write-Host "Downloading Windows 10 22H2..." -ForegroundColor Cyan 
 
 Start-Process -FilePath $Windows10Downloader
+
+
+
+#######    Delete Items Variables region ########################
+
+$binfolder = "$ZIPDestination\bin"
+$Driversfolder = "$ZIPDestination\Drivers"
+$filesfolder = "$ZIPDestination\files"
+$UUPsfolder = "$ZIPDestination\UUPs"
+
+#####################################################
+
+Write-Host "Deleting Subfolders and Files Windows 10 22H2 Source Directory..." -ForegroundColor Cyan 
+
+rd -Path "$binfolder" -Recurse -Force
+rd -Path "$Driversfolder" -Recurse -Force
+rd -Path "$filesfolder" -Recurse -Force
+rd -Path "$UUPsfolder" -Recurse -Force
+
+Remove-Item -Path "$ZIPDestination\*.log" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.ini" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.cmd" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.txt" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.html" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.md" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.sh" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.zip" -Recurse -Force
+
+Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
+
