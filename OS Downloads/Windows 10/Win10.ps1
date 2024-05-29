b6ed97727f6c7d4fddd191a9e3fde7130c88c07b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-29 07:38:39 -0400

    Update Win10.ps1

--- OS Downloads/Windows 10/Win10.ps1
+++ OS Downloads/Windows 10/Win10.ps1
@@ -1,1 +1,13 @@
+$Win10Log = "C:\OSD Cloud\Logs\OS Downloads\Windows10.log"
+Start-Transcript $Win10Log
+
+Get-Date
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+# Download Zip file region
+
+$ZIPDestination = "$env:userprofile\Downloads\OS\Windows10"
+
 
