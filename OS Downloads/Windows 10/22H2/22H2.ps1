9cd0b7bb7e1b97d4962643b1b537a6cac2e8bbaf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-29 12:57:54 -0400

    Update 22H2.ps1

--- OS Downloads/Windows 10/22H2/22H2.ps1
+++ OS Downloads/Windows 10/22H2/22H2.ps1
@@ -25,7 +25,7 @@ Save-WebFile -SourceUrl $Windows10URL -DestinationDirectory $ZIPDestination
 Write-Host "Expanding Windows 10 22H2 ZIP File..." -ForegroundColor Cyan 
 
 Expand-Archive -Path "$ZIPDestination\19045.4472_amd64_en-us_multi_39143328_convert_virtual.zip" -DestinationPath $ExtractionLocation
-
+Write-Host
 Write-Host "Downloading Windows 10 22H2..." -ForegroundColor Cyan 
 
 Start-Process -FilePath $Windows10Downloader
@@ -60,3 +60,25 @@ Remove-Item -Path "$ZIPDestination\*.zip" -Recurse -Force
 Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
 
 Stop-Transcript
+
+$OSDBuilder = Read-Host -Prompt 'Do you want to use OSD Builder to further customize the ISO?'
+If(($OSDBuilder -eq "yes") -or ($OSDBuilder -eq "y") -or ($OSDBuilder -eq "YES") -or ($OSDBuilder -eq "Y")){
+$OSDBuilderCheck = Get-InstalledModule | Where-Object -Property Name -eq "OSDBuilder"
+If($OSDBuilderCheck -eq $true){
+$OSDBuilderPWD = Read-Host -Prompt 'Please specify a path for OSDBuilder working directory'
+Get-OSDBuilder -SetPath $OSDBuilderPWD
+Get-OSDBuilder -SetHome $OSDBuilderPWD
+Import-OSMedia -Update -ShowInfo
+}
+ElseIf($OSDBuilderCheck -eq $false){
+Install-Module -Name OSDBuilder -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSDBuilder -Force
+$OSDBuilderPWD = Read-Host -Prompt 'Please specify a path for OSDBuilder working directory'
+Get-OSDBuilder -SetPath $OSDBuilderPWD
+Get-OSDBuilder -SetHome $OSDBuilderPWD
+Import-OSMedia -Update -ShowInfo
+}
+ElseIf(($OSDBuilder -eq "no") -or ($OSDBuilder -eq "n") -or ($OSDBuilder -eq "NO") -or ($OSDBuilder -eq "N")){
+exit
+}
+
