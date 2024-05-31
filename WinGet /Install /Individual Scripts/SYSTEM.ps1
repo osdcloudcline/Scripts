5692e7c260cb6a7820447c753f7bdc34861d78c6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-05-31 00:52:42 -0400

    Update SYSTEM.ps1

--- WinGet /Install /Individual Scripts/SYSTEM.ps1
+++ WinGet /Install /Individual Scripts/SYSTEM.ps1
@@ -89,6 +89,10 @@ $SYSGroup6 = "OS Software"
 
 ################################################################
 
+# Package Manager region 
+
+Write-Host "Installing Chocolatey..." -ForegroundColor Cyan 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # NET Region
 
