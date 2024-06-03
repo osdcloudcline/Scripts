d3fc004d4222d76278439cb766a8bd9cc39c8785
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-03 09:18:01 -0400

    Update STD.ps1

--- WinGet /Install /Audit Mode Install/STD.ps1
+++ WinGet /Install /Audit Mode Install/STD.ps1
@@ -1,1 +1,18 @@
+$OSDModuleLogs = "C:\OSDCloud\DeploymentLogs\STD\$env:computername-OSModule.log"
+Start-Transcript -Path $OSDModuleLogs
 
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+Stop-Transcript
+
+$OSDMediaSoftwareLogs = "C:\OSDCloud\DeploymentLogs\ADV\$env:computername-OSDMediaSoftware.log"
+Start-Transcript -Path $OSDMediaSoftwareLogs
+
+# Media Software
+
+Write-Host
+Write-Host
+Write-Host "Step 1 : Media Software" -ForegroundColor Cyan
+Write-Host
+Write-Host
