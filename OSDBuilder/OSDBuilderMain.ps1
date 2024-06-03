59ff0f6596e854e62990cd67cab3942de6ce16f2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-03 00:25:41 -0400

    Update OSDBuilderMain.ps1

--- OSDBuilder/OSDBuilderMain.ps1
+++ OSDBuilder/OSDBuilderMain.ps1
@@ -67,6 +67,7 @@ do
         }
         ElseIf($OSDBuilderCheck -eq $true){
         Set-ExecutionPolicy Bypass -Force
+        $OSDBuilderPath = Read-Host -Prompt 'Please specify the working directory for OSDBuilder'
         Write-Verbose "Setting OSDBuilder working directory..." -Verbose
         Get-OSDBuilder -SetPath $OSDBuilderPath
         Get-OSDBuilder -CreatePath
