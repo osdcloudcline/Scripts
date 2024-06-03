06a761e2bb6cc710be450bd59b69aae9ce25325c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-03 09:19:57 -0400

    Update STD.ps1

--- WinGet /Install /Audit Mode Install/STD.ps1
+++ WinGet /Install /Audit Mode Install/STD.ps1
@@ -19,3 +19,18 @@ Write-Host
 
 $Mediaapp1 = "VLC Media Player"
 $Mediaapp2 = "MediaInfo"
+
+Write-Host "ATTN: $env:username - 53 pieces of software will be installed. 3 will be downloaded. Please be patient" -ForegroundColor Red
+
+Write-Host "Installing Media Applications on $env:computername..." -ForegroundColor Green
+
+Write-Host "Installing Media Software 1 of 10..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Mediaapp1..." -Verbose
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Host "Installing Media Software 2 of 10..." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $Mediaapp2..." -Verbose
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
