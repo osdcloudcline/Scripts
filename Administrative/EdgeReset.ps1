376b4a81da7e9930c135fab934edfb254675241b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-09 22:54:17 -0400

    Update EdgeReset.ps1

--- Administrative/EdgeReset.ps1
+++ Administrative/EdgeReset.ps1
@@ -4,13 +4,21 @@ Start-Transcript -Path $EdgeResetLog
 
 Get-Date
 
-Write-Verbose "Renaming Edge User Data File to reset MS Edge Browser..." -Verbose
+Write-Verbose "Killing MS Edge Browser Running Tasks..." -Verbose
+
+Get-Process -Name "*Edge*" | Stop-Process
+
+Write-Verbose "Renaming Edge User Data Folder to reset MS Edge Browser..." -Verbose
 
 Rename-Item -Path "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data" -NewName "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data0" -Force 
 
 $EdgeEXE = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 
 Start-Process -FilePath $EdgeEXE
+
+Write-Verbose "Deleting Edge User Data Folder..." -Verbose
+
+rd "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data0" -Force
 
 Write-Verbose "Edge Browser Reset is complete. Any corruption is fixed" -Verbose
 
