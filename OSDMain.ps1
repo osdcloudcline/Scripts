5d3e590753ec2a25a598ef5304d33da25b241286
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-12 00:36:58 -0400

    Update OSDMain.ps1

--- OSDMain.ps1
+++ OSDMain.ps1
@@ -1,1 +1,42 @@
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OSD Cloud -  Computers Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
+cls
+
+
+
+       $os = Get-CimInstance -ClassName Win32_OperatingSystem
+       $PCName = "$env:computername"
+       $OSVerison = $($os.Version)
+       $UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+       $FullOS = $($os.Caption)
+       $OSBuild = $($os.Version) + "." + $UBR
+       $DisplayVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name DisplayVersion).DisplayVersion
+       $EditionID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name EditionID).EditionID
+       $CompleteOSInfo = "$FullOS" + " $OSBuild" + " $DisplayVersion"
+       $EdgeEXE = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe' "(default)"
+       $EdgeBrowser = (Get-Item $EdgeEXE).VersionInfo.ProductVersion
+       Write-Host
+       Write-Host         "Running Computer Info:" -ForegroundColor Green 
+       Write-Verbose "System Hostname: $PCName" -Verbose
+       Write-Verbose "Running Operating System: $CompleteOSInfo" -Verbose  
+       Write-Verbose "Operating System: $FullOS" -Verbose 
+       Write-Verbose "Build Number: $OSBuild" -Verbose 
+       Write-Verbose "Display Version: $DisplayVersion" -Verbose
+       Write-Verbose "OS Edition: $EditionID" -Verbose 
+       Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose 
+pause
+
+
+Write-Host 
+Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
+Write-Host 
+Clear-Host
+
 
