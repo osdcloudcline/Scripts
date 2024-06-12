bacb8c893899c4308e107b51ab217d0a85f8de40
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-12 09:33:08 -0400

    Update OSDMain.ps1

--- OSDMain.ps1
+++ OSDMain.ps1
@@ -34,7 +34,8 @@ cls
 Write-Host 
 Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
 Write-Host
-Write-Host "Today is:" Get-Date
+$Date = Get-Date
+Write-Host "Today is:" + "$Date"
 Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
 Write-Host
 Write-Host 
