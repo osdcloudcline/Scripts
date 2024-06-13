f15fbd798d127590f8e93585b555c7c86ff5b85c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-12 22:22:21 -0400

    Update OSDMain.ps1

--- OSDMain.ps1
+++ OSDMain.ps1
@@ -25,7 +25,13 @@ cls
        $IP1 = (Get-NetIpaddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet"}).IPAddress
        $IP2 = (Get-NetIpaddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -eq "Ethernet 2"}).IPAddress
        $DNS = (Get-CimInstance -ClassName Win32_networkAdapterConfiguration | Where-Object { $_.IPEnabled })
-       
+
+       $Baseboard1 = (Get-CimInstance -ClassName Win32_Baseboard).Manufacturer
+       $Baseboard2 = (Get-CimInstance -ClassName Win32_Baseboard).Product
+	    $PCManufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+	    $BIOS1 = (Get-CimInstance -ClassName Win32_BIOS).Manufacturer
+       $BIOS2 = (Get-CimInstance -ClassName Win32_BIOS).SMBIOSBIOSVersion
+ 
        Write-Host
        Write-Host         "Running Computer Info:" -ForegroundColor Green 
        Write-Verbose "System Hostname: $PCName" -Verbose
@@ -36,13 +42,21 @@ cls
        Write-Verbose "OS Edition: $EditionID" -Verbose 
        Write-Verbose "MS Edge Chromium Version: $EdgeBrowser" -Verbose 
 
-Write-Host 
+      Write-Host 
       Write-Host
       Write-Host         "Network Connection Info:" -ForegroundColor Green 
       Write-Verbose "System Hostname: $PCName" -Verbose
       Write-Verbose "System IP Address 1: $IP1" -Verbose
       Write-Verbose "System IP Address 2: $IP2" -Verbose
       Write-Verbose "System Gateway/DNS Server: $DNS" -Verbose
+
+      Write-Host 
+      Write-Host
+      Write-Host         "System Hardware Info:" -ForegroundColor Green 
+      Write-Verbose "System Hostname: $PCName" -Verbose
+      Write-Verbose "Motherboard: $Baseboard1 $Baseboard2" -Verbose
+      Write-Verbose "Manufacturer: $PCManufacturer" -Verbose
+      Write-Verbose "System BIOS: $BIOS1 $BIOS2" -Verbose
       
 Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
 Write-Host
