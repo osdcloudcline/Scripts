9c9396d2151c723fc17292d9775f70ce83310dfd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-14 07:05:11 -0400

    Update LocalPCMain.ps1

--- Local Computer/LocalPCMain.ps1
+++ Local Computer/LocalPCMain.ps1
@@ -1,1 +1,11 @@
+#################################################################
+# Main Menu
+#################################################################
 
+Function Show-MainMenu{
+   [CmdletBinding()]
+   param(
+   [string]$Title = 'Local PC Administration - Main Menu',
+   [string]$Question = 'What type of action do you need to do?'
+   )
+cls
