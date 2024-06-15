2dda9e51748d419fbe9bcd076c7bfbc2705f3fca
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-15 01:36:56 -0400

    Update Configure.ps1

--- AuditMode/Configure.ps1
+++ AuditMode/Configure.ps1
@@ -15,3 +15,8 @@ Write-Verbose "Step 3 - Mandatory OS Pre-Requirements..." -Verbose
 
 $OS = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM.ps1")
 Invoke-Expression $($OS.Content)
+
+Write-Verbose "Step 4 - Software Installation..." -Verbose
+
+$WinGetSoftware = Invoke-WebRequest("")
+Invoke-Expression $($WingetSoftware.Content)
