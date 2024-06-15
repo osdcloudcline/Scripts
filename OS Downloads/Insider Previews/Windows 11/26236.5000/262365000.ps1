8642c5315e374678889dfb60de29fd5c34bd4aaf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-15 13:52:01 -0400

    Create 262365000.ps1

---
+++ OS Downloads/Insider Previews/Windows 11/26236.5000/262365000.ps1
@@ -0,0 +1,64 @@
+
+$Build = "26236.5000"
+$Win10Log = "C:\OSD Cloud\Logs\OS Downloads\WindowsInsiderPreview-$Build.log"
+Start-Transcript $Win10Log
+
+Get-Date
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+################### ISO Download, Extraction and Creation Variables region ##########################
+
+$ZIPDestination = "$env:userprofile\Downloads\OS\InsiderPreviewBuilds\26235-5000"
+
+$WindowsIP262365000URL = "https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/Insider%20Previews/Windows%2011/26236.5000/26236.5000_amd64_en-us_multi_d55ca98b_convert_virtual.zip"
+
+$ExtractionLocation = $ZIPDestination
+
+$WindowsIP262365000Downloader = "$ZIPDestination\uup_download_windows.cmd"
+
+########################################################################################
+
+Write-Host "Downloading Windows Insider Preview 26236.5000 ZIP File..." -ForegroundColor Cyan 
+
+Save-WebFile -SourceUrl $Windows10URL -DestinationDirectory $ZIPDestination
+
+Write-Host "Expanding Windows Insider Preview 26236.5000 ZIP File..." -ForegroundColor Cyan 
+
+Expand-Archive -Path "$ZIPDestination\26235.5000/26236.5000_amd64_en-us_multi_d55ca98b_convert_virtual.zip" -DestinationPath $ExtractionLocation
+Write-Host
+Write-Host "Downloading Windows Insider Preview 26236.5000..." -ForegroundColor Cyan 
+
+Start-Process -FilePath $WindowsIP262365000Downloader
+
+pause
+
+#######    Delete Items Variables region ########################
+
+$binfolder = "$ZIPDestination\bin"
+$Driversfolder = "$ZIPDestination\Drivers"
+$filesfolder = "$ZIPDestination\files"
+$UUPsfolder = "$ZIPDestination\UUPs"
+
+#####################################################
+
+Write-Host "Deleting Subfolders and Files Windows Insider Preview 26236.5000 Source Directory..." -ForegroundColor Cyan 
+
+rd -Path "$binfolder" -Recurse -Force
+rd -Path "$Driversfolder" -Recurse -Force
+rd -Path "$filesfolder" -Recurse -Force
+rd -Path "$UUPsfolder" -Recurse -Force
+
+Remove-Item -Path "$ZIPDestination\*.log" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.ini" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.cmd" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.txt" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.html" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.md" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.sh" -Recurse -Force
+Remove-Item -Path "$ZIPDestination\*.zip" -Recurse -Force
+
+Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
+
+Stop-Transcript
