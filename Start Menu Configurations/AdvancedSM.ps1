6890e4ec3a5298ce4d9cafbd0fcf8d928c4e3f37
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-16 11:45:36 -0400

    Update AdvancedSM.ps1

--- Start Menu Configurations/AdvancedSM.ps1
+++ Start Menu Configurations/AdvancedSM.ps1
@@ -4,7 +4,7 @@ Import-Module -Name OSD -Force
 $ADVLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\ADV" -IsValid){
 Write-Host "This computer has been installed with the Advanced Configuration of software" -ForgroundColor Green
 Write-Host
-Write-Host "The Advanced Start Menu layout will not be acquired from OSDCloud Cline\Scripts\Start Menu Configurations and configured on $env:computername..." -ForegroundColor Cyan
+Write-Host "The Advanced Start Menu layout will now be downloaded from OSDCloud Cline\Scripts\Start Menu Configurations to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
 
 $StartMenudownload = "C:\OSDCloud\GitHub\downloads\StartMenu\Advanced"
 
