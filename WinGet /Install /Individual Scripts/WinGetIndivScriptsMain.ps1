18641b8cfd7dd03b2de7a15a7b586961bdf9d350
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 01:05:24 -0400

    Update WinGetIndivScriptsMain.ps1

--- WinGet /Install /Individual Scripts/WinGetIndivScriptsMain.ps1
+++ WinGet /Install /Individual Scripts/WinGetIndivScriptsMain.ps1
@@ -70,5 +70,59 @@ $selection = Read-Host 'Please choose an option'
  until ($selection -eq '13'){exit}
 }
 
-Function Show-BrowsersMainMenu(){}
+Function Show-BrowsersMainMenu(){
+
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'Web Browsers Installation Scripts - Software Installation Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
+
+Clear-Host
+Write-Host "======= $Title ======"
+Write-Host " 1. Install Advanced Web Browsing Configuration"
+Write-Host " 2. Install Corporate Web Browsing Configuration"
+Write-Host " 3. Install Honeypot Web Browsing Configuration"
+Write-Host " 4. Install IT Tech Web Browsing Configuration"
+Write-Host " 5. Install Management PC Web Browsing Configuration"
+Write-Host " 6. Install Standard Web Browsing Configuration"
+Write-Host " 7. Install VDI Web Browsing Configuration"
+Write-Host " 8. Return to Winget Individual Software Main Menu"
+Write-Host " 9. Return to OSD Git-Hub Main Menu"
+Write-Host " 10. Exit PowerShell"
+
+do
+{
+$selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+  '1' {cls
+      
+       }
+ '2'{cls
+      
+     }
+ '3'{cls
+       
+    }
+ '4'{cls
+       
+    }
+ '5'{cls
+      
+    }
+ '6'{cls
+       
+    }
+ '7'{cls
+       
+    }
+ '8'{cls
+       
+    }
+'10'{exit}
+ }
+ }
+ until ($selection -eq '10'){exit}
+}
 Show-MainMenu 
