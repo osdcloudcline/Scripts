8aba5c781163f2bb67e4ef090b0ff7ce29677dd4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:29:59 -0400

    Update ITTechSM.ps1

--- Start Menu Configurations/IT Tech/ITTechSM.ps1
+++ Start Menu Configurations/IT Tech/ITTechSM.ps1
@@ -1,2 +1,8 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
+
+$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\IT Tech"
+
+Write-Host "This computer has been installed with the IT TECH Configuration of software" -ForgroundColor Green
+Write-Host
+Write-Host "The IT Tech Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
