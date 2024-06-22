ffdd4b112be5caf6846d127ba161baaac28f6ad3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 00:11:55 -0400

    Update AdvancedProgramming.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/AdvancedProgramming.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/AdvancedProgramming.ps1
@@ -8,4 +8,23 @@ $app1 = "VS Code"
 $app2 = "AI Cursor Editor"
 $app3 = "Microsoft Visual Studio Enterprise 2022"
 
+Write-Host "Installing Programming Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
 
+
+
+Write-Host
+Write-Verbose "Processing: $app2..." -Verbose
+Write-Host
+Write-Host "Installing $app2..." -ForegroundColor Cyan
+
+
+
+
+Write-Host
+Write-Verbose "Processing: $app3..." -Verbose
+Write-Host
+Write-Host "Installing $app3..." -ForegroundColor Cyan
