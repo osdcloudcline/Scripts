3e56be9d2c20f6adc52f4e0c0248f4808441c6b8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:31:07 -0400

    Update ManagementPCSM.ps1

--- Start Menu Configurations/Management PC/ManagementPCSM.ps1
+++ Start Menu Configurations/Management PC/ManagementPCSM.ps1
@@ -1,2 +1,8 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
+
+$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Management PC"
+
+Write-Host "This computer has been installed with the Honeypot Configuration of software" -ForgroundColor Green
+Write-Host
+Write-Host "The MANAGEMENT PC Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
