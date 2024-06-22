5a2bf293ede63ee3a5df1dc4e3d8f156f000d4a4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:25:28 -0400

    Update CorporateSM.ps1

--- Start Menu Configurations/Corporate/CorporateSM.ps1
+++ Start Menu Configurations/Corporate/CorporateSM.ps1
@@ -2,6 +2,7 @@ Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
 $CORPLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\Corporate" -IsValid){
+$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Corporate"
 Write-Host "This computer has been installed with the Corporate Configuration of software" -ForgroundColor Green
 Write-Host
-Write-Host "The Corporate Start Menu layout will now be downloaded from OSDCloud Cline\Scripts\Start Menu Configurations to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
+Write-Host "The Corporate Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
