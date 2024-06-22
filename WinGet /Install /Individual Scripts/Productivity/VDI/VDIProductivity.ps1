8e46858d7010335af124201bdf872cf7c38ba3ba
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 14:07:05 -0400

    Create VDIProductivity.ps1

---
+++ WinGet /Install /Individual Scripts/Productivity/VDI/VDIProductivity.ps1
@@ -0,0 +1,11 @@
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+$app1 = "Microsoft Office 2021 LTSC"
+
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+$OfficeGHdownloads = "C:\OSDCloud\GitHub\downloads\Office" 
+mkdir $OfficeGHdownloads
+$Office2021FilesUrl = "https://github.com/osdcloudcline/OSDCloud/raw/main/Software/Office2021/OSDCloud-Office2021.zip"
+Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
+Expand-Archive -Path "$OfficeGHdownloads\OSDCloud-Office2021.zip" -DestinationPath $OfficeGHdownloads
