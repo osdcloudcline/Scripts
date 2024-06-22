57f3e5dc0c5722371d1d4048464d1833b02426df
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 14:16:45 -0400

    Create Corporate.ps1

---
+++ WinGet /Install /System-LOB Specific/Corporate/Corporate.ps1
@@ -0,0 +1,5 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\$env:computername-PreReqInstall.log"
+
+####################################################################
+### SYSTEM SOFTWARE VARIABLES AND INSTALL                      #####
+####################################################################
