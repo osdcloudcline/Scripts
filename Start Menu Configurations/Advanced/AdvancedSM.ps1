73c754e780e481cd9d405846d31c2253a105b6c8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:26:45 -0400

    Update AdvancedSM.ps1

--- Start Menu Configurations/Advanced/AdvancedSM.ps1
+++ Start Menu Configurations/Advanced/AdvancedSM.ps1
@@ -2,9 +2,11 @@ Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
 $ADVLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\ADV" -IsValid){
+$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Advanced"
+
 Write-Host "This computer has been installed with the Advanced Configuration of software" -ForgroundColor Green
 Write-Host
-Write-Host "The Advanced Start Menu layout will now be downloaded from OSDCloud Cline\Scripts\Start Menu Configurations to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
+Write-Host "The Advanced Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
 
 $StartMenudownload = "C:\OSDCloud\GitHub\downloads\StartMenu\Advanced"
 
@@ -16,3 +18,5 @@ $StartMenuBINFile = "C:\OSDCloud\GitHub\downloads\StartMenu\Advanced\Start2.bin"
 $SMDestination = "C:\Users\$env:username\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
 Write-Verbose "Copying Start Menu file to $SMDestination...." -Verbose
 Copy-Item -Path $StartMenuBINFile -Destination $SMDestination -Force 
+
+}
