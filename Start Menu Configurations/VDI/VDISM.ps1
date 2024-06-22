10ddd89da999bad92a01c536275dac26bcffd31e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:33:45 -0400

    Update VDISM.ps1

--- Start Menu Configurations/VDI/VDISM.ps1
+++ Start Menu Configurations/VDI/VDISM.ps1
@@ -1,2 +1,10 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
+
+
+
+$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\VDI"
+
+Write-Host "This computer has been installed with the VDI Remote Desktop Configuration of software" -ForgroundColor Green
+Write-Host
+Write-Host "The VDI Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
