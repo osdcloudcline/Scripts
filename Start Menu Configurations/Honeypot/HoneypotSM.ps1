20c9e680627509ec3a00add37b971ea9ab8913e8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-22 08:22:11 -0400

    Update HoneypotSM.ps1

--- Start Menu Configurations/Honeypot/HoneypotSM.ps1
+++ Start Menu Configurations/Honeypot/HoneypotSM.ps1
@@ -1,2 +1,7 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
+
+$HoneypotLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\Corporate" -IsValid){
+Write-Host "This computer has been installed with the Honeypot Configuration of software" -ForgroundColor Green
+Write-Host
+Write-Host "The Honeypot Start Menu layout will now be downloaded from OSDCloud Cline\Scripts\Honeypot\Start Menu Configurations to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
