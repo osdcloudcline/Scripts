00aeaed6a9a71b482df2bdb072b67a1a5cb4482e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-24 13:46:53 -0400

    Update ConfigureServers.ps1

--- AuditMode/ConfigureServers.ps1
+++ AuditMode/ConfigureServers.ps1
@@ -1,1 +1,9 @@
+Write-Host "Beginning System Configuration..." -ForegroundColor Cyan
 
+
+Write-Verbose "Step 1 - PowerShell Modules..." -Verbose
+
+$PSModules = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PS%20Modules%20/Install.ps1")
+Invoke-Expression $($PSModules.Content)
+
+Write-Verbose "Step 2 - Merging Registry Entries..." -Verbose
