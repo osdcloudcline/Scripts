e6f2706789cb951f226b3ea1fef430f41dbe8e56
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-25 15:01:05 -0400

    Update Intel.ps1

--- WinGet /Install /Manufacturer Specific/Intel/Intel.ps1
+++ WinGet /Install /Manufacturer Specific/Intel/Intel.ps1
@@ -9,13 +9,13 @@ $MfrSoftwareGroup1 = "Intel"
 Write-Host "Installing $MfrSoftwareGroup1 Software..." -ForegroundColor Cyan
 Write-Host
 Write-Host
-Write-Verbose "Installing: $IntelSoftware1...OEM Software No: 1 of 5" -Verbose
+Write-Verbose "Installing: $IntelSoftware1..." -Verbose
 choco.exe install intel-chipset-device-software -y
-Write-Verbose "Installing: $IntelSoftware2...OEM Software No: 2 of 5" -Verbose
+Write-Verbose "Installing: $IntelSoftware2..." -Verbose
 choco.exe install intel-driver-update-utility -y
-Write-Verbose "Installing: $IntelSoftware3...OEM Software No: 3 of 5" -Verbose
+Write-Verbose "Installing: $IntelSoftware3..." -Verbose
 choco.exe install intel-dsa -y
-Write-Verbose "Installing: $IntelSoftware4...OEM Software No: 4 of 5" -Verbose
+Write-Verbose "Installing: $IntelSoftware4..." -Verbose
 choco.exe install intel-killer-performance-suite -y
-Write-Verbose "Installing: $IntelSoftware4...OEM Software No: 4 of 5" -Verbose
+Write-Verbose "Installing: $IntelSoftware4..." -Verbose
 choco.exe install intel-processor-identification-utility -y
