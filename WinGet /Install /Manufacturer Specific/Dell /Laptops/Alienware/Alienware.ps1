1170b6ed1a9871782152d787db6e38b984c9e483
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-26 00:23:10 -0400

    Update Alienware.ps1

--- WinGet /Install /Manufacturer Specific/Dell /Laptops/Alienware/Alienware.ps1
+++ WinGet /Install /Manufacturer Specific/Dell /Laptops/Alienware/Alienware.ps1
@@ -2,3 +2,4 @@ Write-Host 'Welcome to the Dell Alienware Software Download Script' -ForegroundC
 Write-Host '                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host 'This script downloads the following:                  ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '    Alienware Control Center Installation Manager     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '    Dell Digital Delivery Manager                     ' -ForegroundColor DarkBlue -BackgroundColor White
