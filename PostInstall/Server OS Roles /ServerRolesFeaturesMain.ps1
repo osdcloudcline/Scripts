ddabca2d4626cb41c204cea141167338fad8f605
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-27 08:59:40 -0400

    Update ServerRolesFeaturesMain.ps1

--- PostInstall/Server OS Roles /ServerRolesFeaturesMain.ps1
+++ PostInstall/Server OS Roles /ServerRolesFeaturesMain.ps1
@@ -113,25 +113,26 @@ Write-Host " 2. Active Directory Domain Services"
 Write-Host " 3. Active Directory Federation Services"
 Write-Host " 4. Active Directory LDS Services"
 Write-Host " 5. Active Directory Rights Management Services"
-Write-Host " 6. Device Health Attestation"
-Write-Host " 7. DHCP Server"
-Write-Host " 8. DNS Server"
-Write-Host " 9. Fax Server"
-Write-Host " 10. File and Storage Services"
-Write-Host " 11. Host Guardian Services"
-Write-Host " 12. Hyper-V"
-Write-Host " 13. Network Controller"
-Write-Host " 14. Network Policy and Access Services"
-Write-Host " 15. Print and Document Services"
-Write-Host " 16. Remote Access Server"
-Write-Host " 17. Remote Desktop Services"
-Write-Host " 18. Volume Activation Services"
-Write-Host " 19. IIS Web Server"
-Write-Host " 20. Windows Deployment Server Services"
-Write-Host " 21. Windows Server Update Server Services"
-Write-Host " 22. Install OS Pre-Reqs, System Software and Pre-Reqs for SQL/Exchange/MECM Server"
-Write-Host " 23. Return to Main Menu"
-Write-Host " 24. Exit PowerShell"
+Write-Host " 6. Application Server"
+Write-Host " 7. Device Health Attestation"
+Write-Host " 8. DHCP Server"
+Write-Host " 9. DNS Server"
+Write-Host " 10. Fax Server"
+Write-Host " 11. File and Storage Services"
+Write-Host " 12. Host Guardian Services"
+Write-Host " 13. Hyper-V"
+Write-Host " 14. Network Controller"
+Write-Host " 15. Network Policy and Access Services"
+Write-Host " 16. Print and Document Services"
+Write-Host " 17. Remote Access Server"
+Write-Host " 18. Remote Desktop Services"
+Write-Host " 19. Volume Activation Services"
+Write-Host " 20. IIS Web Server"
+Write-Host " 21. Windows Deployment Server Services"
+Write-Host " 22. Windows Server Update Server Services"
+Write-Host " 23. Install OS Pre-Reqs, System Software and Pre-Reqs for SQL/Exchange/MECM Server"
+Write-Host " 24. Return to Main Menu"
+Write-Host " 25. Exit PowerShell"
 do 
 {
   $selection = Read-Host 'Please choose an option'
@@ -139,79 +140,143 @@ do
   {
 
   '1' { cls
-        $ADCSLog = "C:\Logs\Roles\ADCS.log'
+        $ADCSLog = "C:\Logs\Server Roles\ADCS.log'
         $ADCS = "Active Directory Certificate Services"
         Start-Transcript $ADCSLog
         Write-Verbose "Installing $ADCS..." -Verbose
         Get-WindowsFeature -Name AD-Certificate | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Cert-Authority | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Enroll-Web-Pol | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Enroll-Web-Svc | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Web-Enrollment | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Device-Enrollment | Install-WindowsFeature
+        Get-WindowsFeature -Name ADCS-Online-Cert | Install-WindowsFeature
         Stop-Transcript
         Show-ServerRolesMenu
         }
   '2' { cls
-        
+        $ADDSLog = "C:\Logs\Server Roles\ADDS.log'
+        $ADCS = "Active Directory Domain Services"
+        Start-Transcript $ADDSLog
+        Write-Verbose "Installing $ADDS..." -Verbose
+        Get-WindowsFeature -Name AD-Domain-Services | Install-WindowsFeature
+        Write-Verbose "Installing and Importing $ADDS Deployment PowerShell Module..." -Verbose
+        Install-Module -Name ADDSDeployment -Force -AllowClobber -SkipPublisherCheck
+        Import-Module -Name ADDSDeployment -Force
+        Stop-Transcript
+        Show-ServerRolesMenu
         }
   '3' { cls
-        
+        $ADFSLog = "C:\Logs\Server Roles\ADFS.log'
+        $ADFS = "Active Directory Federation Services"
+        Start-Transcript $ADFSLog
+        Write-Verbose "Installing $ADFS..." -Verbose
+        Get-WindowsFeature -Name ADFS-Federation | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
         }
-  '4' {
+  '4' {cls
+        $ADLDSLog = "C:\Logs\Server Roles\ADLDS.log'
+        $ADLDS = "Active Directory Lightweight Directory Services"
+        Start-Transcript $ADLDSLog
+        Write-Verbose "Installing $ADLDS..." -Verbose
+        Get-WindowsFeature -Name ADLDS | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
       }
   '5' { cls
-        
+        $ADRMSLog = "C:\Logs\Server Roles\ADRMS.log'
+        $ADRMS = "Active Directory Rights Management Server Services"
+        Start-Transcript $ADRMSLog
+        Write-Verbose "Installing $ADRMS..." -Verbose
+        Get-WindowsFeature -Name ADRMS | Install-WindowsFeature
+        Get-WindowsFeature -Name ADRMS-Server | Install-WindowsFeature
+        Get-WindowsFeature -Name ADRMS-Identity | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
         }
-  '6' { cls
+   '6' { cls
         
         }
   '7' { cls
-        
+        $DHALog = "C:\Logs\Server Roles\DHA.log'
+        $DHA = "Device Health Attestation"
+        Start-Transcript $DHALog
+        Write-Verbose "Installing $DHA..." -Verbose
+        Get-WindowsFeature -Name DeviceHealthAttestationService | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
         }
-  '8' {
+  '8' { cls
+        $DHCPLog = "C:\Logs\Server Roles\DHCP.log'
+        $DHCP = "DHCP Server"
+        Start-Transcript $DHCPLog
+        Write-Verbose "Installing $DHCP..." -Verbose
+        Get-WindowsFeature -Name DHCP | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
+        }
+  '9' {cls
+        $DNSLog = "C:\Logs\Server Roles\DNS.log'
+        $DNS = "DNS Server"
+        Start-Transcript $DNSLog
+        Write-Verbose "Installing $DNS..." -Verbose
+        Get-WindowsFeature -Name DNS | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
       }
-  '9' { cls
-        
-        }
   '10' { cls
-        
+        $FaxLog = "C:\Logs\Server Roles\FaxServer.log'
+        $FaxServer = "Fax Server"
+        Start-Transcript $FaxLog
+        Write-Verbose "Installing $FaxServer..." -Verbose
+        Get-WindowsFeature -Name Fax | Install-WindowsFeature
+        Stop-Transcript
+        Show-ServerRolesMenu
         }
   '11' { cls
         
         }
-  '12' {
-      }
-  '13' { cls
+  '12' { cls
         
         }
+  '13' {
+      }
   '14' { cls
         
         }
   '15' { cls
         
         }
-  '16' {
-      }
-  '17' { cls
+  '16' { cls
         
         }
+  '17' {
+      }
   '18' { cls
         
         }
   '19' { cls
         
         }
-  '20' {
-      }
-  '21' { cls
+  '20' { cls
         
         }
+  '21' {
+      }
   '22' { cls
         
         }
   '23' { cls
         
         }
-  '24' {exit}
+  '24' { cls
+        
+        }
+  '25' {exit}
     }
     }
-    until ($selection -eq '24'){exit}
+    until ($selection -eq '25'){exit}
     }
 
 
