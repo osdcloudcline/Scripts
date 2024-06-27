d6e582d249d4f922e6dfd201b351a674e2427b22
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-26 21:19:15 -0400

    Update Optiplex.ps1

--- WinGet /Install /Manufacturer Specific/Dell /Desktops/Optiplex Micro Form Factor/Optiplex.ps1
+++ WinGet /Install /Manufacturer Specific/Dell /Desktops/Optiplex Micro Form Factor/Optiplex.ps1
@@ -1,1 +1,3 @@
-
+Write-Host 'Welcome to the Dell Optiplex Software Download Script ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host 'This script downloads the following:                   ' -ForegroundColor DarkBlue -BackgroundColor White
