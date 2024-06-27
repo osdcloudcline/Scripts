f18a16b45dbf06a62a6edd47886f5d3baa0f07f8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-26 21:23:02 -0400

    Update XPS.ps1

--- WinGet /Install /Manufacturer Specific/Dell /Laptops/XPS/XPS.ps1
+++ WinGet /Install /Manufacturer Specific/Dell /Laptops/XPS/XPS.ps1
@@ -1,1 +1,3 @@
-
+Write-Host 'Welcome to the Dell XPS Software Download Script       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host 'This script downloads the following:                   ' -ForegroundColor DarkBlue -BackgroundColor White
