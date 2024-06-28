fe2969544242d296c90e03fa4389514be499f8bd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-28 08:48:03 -0400

    Update MPGZ790EdgeTiMAXWiFi7Gaming.ps1

--- WinGet /Install /Manufacturer Specific/MSI/Motherboards/Intel/LGA 1700/Z790 Chipsets/MPG Z790 Edge Ti MAX WiFi 7 Gaming /MPGZ790EdgeTiMAXWiFi7Gaming.ps1
+++ WinGet /Install /Manufacturer Specific/MSI/Motherboards/Intel/LGA 1700/Z790 Chipsets/MPG Z790 Edge Ti MAX WiFi 7 Gaming /MPGZ790EdgeTiMAXWiFi7Gaming.ps1
@@ -1,1 +1,9 @@
-
+Write-Host '                                                                                                           ' -BackgroundColor White 
+Write-Host '  The following MSI INtel LGA 1700 MPG Z-790 Edge Ti WiFi 7 Gaming motherboard drivers will be downloaded: ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Onboard and dedicated Audio Drivers                                                                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Onboard and dedicated Video Drivers                                                                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Onboard Network Card Drivers                                                                           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Storage Drivers                                                                                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - System and Chipset Drivers                                                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Printer Drivers                                                                                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                ' -BackgroundColor White 
