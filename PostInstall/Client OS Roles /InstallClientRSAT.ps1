9080a2c20eb098e90d778e2b23946f4f7c9867bf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-27 22:21:00 -0400

    Update InstallClientRSAT.ps1

--- PostInstall/Client OS Roles /InstallClientRSAT.ps1
+++ PostInstall/Client OS Roles /InstallClientRSAT.ps1
@@ -1,1 +1,26 @@
+$RSAT = "Windows Client OS RSAT Tools"
+Write-Verbose "Installing $RSAT..." -Verbose
+Write-Host
 
+Write-Host '  The following Windows Client OS RSAT Tools will be installed: ' 
+Write-Host '  - RSAT: Active Directory Domain Services and LDS Tools        '
+Write-Host '  - RSAT: PowerShell Module for Azure Stack HCI                 '
+Write-Host '  - RSAT: Active Directory Certificate Server Services          '
+Write-Host '  - RSAT: DHCP Server Tools                                     '
+Write-Host '  - RSAT: DNS Server Tools                                      '
+Write-Host '  - RSAT: Failover Clustering Tools                             '
+Write-Host '  - RSAT: File Services Tools                                   '
+Write-Host '  - RSAT: Group Policy Management Console                       '
+Write-Host '  - RSAT: IP Address Management Client (IPAM)                   '
+Write-Host '  - RSAT: Data Center Bridging LLDP Tools                       '
+Write-Host '  - RSAT: Network Controller Management Tools                   '
+Write-Host '  - RSAT: Network Load Balancing Tools                          '
+Write-Host '  - RSAT: Remote Access Management Tools                        '
+Write-Host '  - RSAT: Remote Desktop Services Tools                         '
+Write-Host '  - RSAT: Server Manager                                        '
+Write-Host '  - RSAT: Storage Migration Service Management Tools            '
+
+Write-Host '  - RSAT: Storage Replica PowerShell Module                     '
+Write-Host '  - RSAT: System Insights PowerShell Module                     '
+Write-Host '  - RSAT: Volume Activation Tools                               '
+Write-Host '  - RSAT: Windows Server Update Services (WSUS) Tools           '
