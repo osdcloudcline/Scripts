42392457a44c06fc9867b553ea21c263c3ce9088
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-27 22:23:52 -0400

    Update InstallClientRSAT.ps1

--- PostInstall/Client OS Roles /InstallClientRSAT.ps1
+++ PostInstall/Client OS Roles /InstallClientRSAT.ps1
@@ -1,27 +1,31 @@
+
+
+Write-Host '                                                                ' -BackgroundColor White 
+Write-Host '  The following Windows Client OS RSAT Tools will be installed: ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Active Directory Domain Services and LDS Tools        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: PowerShell Module for Azure Stack HCI                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Active Directory Certificate Server Services          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: DHCP Server Tools                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: DNS Server Tools                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Failover Clustering Tools                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: File Services Tools                                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Group Policy Management Console                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: IP Address Management Client (IPAM)                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Data Center Bridging LLDP Tools                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Network Controller Management Tools                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Network Load Balancing Tools                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Remote Access Management Tools                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Remote Desktop Services Tools                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Server Manager                                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Storage Migration Service Management Tools            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Storage Replica PowerShell Module                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: System Insights PowerShell Module                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Volume Activation Tools                               ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - RSAT: Windows Server Update Services (WSUS) Tools           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                ' -BackgroundColor White 
+
+pause
+
 $RSAT = "Windows Client OS RSAT Tools"
 Write-Verbose "Installing $RSAT..." -Verbose
 Write-Host
-
-Write-Host '                                                                ' -BackgroundColor White 
-Write-Host '  The following Windows Client OS RSAT Tools will be installed: ' 
-Write-Host '  - RSAT: Active Directory Domain Services and LDS Tools        '
-Write-Host '  - RSAT: PowerShell Module for Azure Stack HCI                 '
-Write-Host '  - RSAT: Active Directory Certificate Server Services          '
-Write-Host '  - RSAT: DHCP Server Tools                                     '
-Write-Host '  - RSAT: DNS Server Tools                                      '
-Write-Host '  - RSAT: Failover Clustering Tools                             '
-Write-Host '  - RSAT: File Services Tools                                   '
-Write-Host '  - RSAT: Group Policy Management Console                       '
-Write-Host '  - RSAT: IP Address Management Client (IPAM)                   '
-Write-Host '  - RSAT: Data Center Bridging LLDP Tools                       '
-Write-Host '  - RSAT: Network Controller Management Tools                   '
-Write-Host '  - RSAT: Network Load Balancing Tools                          '
-Write-Host '  - RSAT: Remote Access Management Tools                        '
-Write-Host '  - RSAT: Remote Desktop Services Tools                         '
-Write-Host '  - RSAT: Server Manager                                        '
-Write-Host '  - RSAT: Storage Migration Service Management Tools            '
-
-Write-Host '  - RSAT: Storage Replica PowerShell Module                     '
-Write-Host '  - RSAT: System Insights PowerShell Module                     '
-Write-Host '  - RSAT: Volume Activation Tools                               '
-Write-Host '  - RSAT: Windows Server Update Services (WSUS) Tools           '
