e19f22b631f5a67c359fd3c348b55f868dd4eeb1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-28 21:51:02 -0400

    Update MPGZ790EdgeTiMAXWiFi7Gaming.ps1

--- WinGet /Install /Manufacturer Specific/MSI/Motherboards/Intel/LGA 1700/Z790 Chipsets/MPG Z790 Edge Ti MAX WiFi 7 Gaming /MPGZ790EdgeTiMAXWiFi7Gaming.ps1
+++ WinGet /Install /Manufacturer Specific/MSI/Motherboards/Intel/LGA 1700/Z790 Chipsets/MPG Z790 Edge Ti MAX WiFi 7 Gaming /MPGZ790EdgeTiMAXWiFi7Gaming.ps1
@@ -23,6 +23,39 @@ $OnboardAudio2URL = ""
 
 ### Onboard Video
 
+$OnboardVideo1URL = ""
+$OnboardVideo2URL = ""
+$OnboardVideo3URL = ""
+$OnboardVideo4URL = ""
+$OnboardVideo5URL = ""
+$OnboardVideo6URL = ""
+$OnboardVideo7URL = ""
+$OnboardVideo8URL = ""
+$OnboardVideo9URL = ""
+$OnboardVideo10URL = ""
+$OnboardVideo11URL = ""
+$OnboardVideo12URL = ""
+$OnboardVideo13URL = ""
+$OnboardVideo14URL = ""
+$OnboardVideo15URL = ""
+$OnboardVideo16URL = ""
+$OnboardVideo17URL = ""
+$OnboardVideo18URL = ""
+$OnboardVideo19URL = ""
+$OnboardVideo20URL = ""
+$OnboardVideo21URL = ""
+$OnboardVideo22URL = ""
+$OnboardVideo23URL = ""
+$OnboardVideo24URL = ""
+$OnboardVideo25URL = ""
+$OnboardVideo26URL = ""
+$OnboardVideo27URL = ""
+$OnboardVideo28URL = ""
+$OnboardVideo29URL = ""
+$OnboardVideo30URL = ""
+$OnboardVideo31URL = ""
+
+
 ### Onboard Network Card
 $OnboardNIC1URL = ""
 $OnboardNIC2URL = ""
@@ -30,15 +63,54 @@ $OnboardNIC3URL = ""
 
 ### Storage Drivers
 
+$Storage1URL = ""
 
 
 ### System and Chipset Drivers
 
+$SystemChipset1URL = ""
 
 ### Printer Drivers
 
 
 ### Dedicated Video card
+
+$AMDRadeon1URL = ""
+$AMDRadeon2URL = ""
+$AMDRadeon3URL = ""
+$AMDRadeon4URL = ""
+$AMDRadeon5URL = ""
+$AMDRadeon6URL = ""
+$AMDRadeon7URL = ""
+$AMDRadeon8URL = ""
+$AMDRadeon9URL = ""
+$AMDRadeon10URL = ""
+$AMDRadeon11URL = ""
+$AMDRadeon12URL = ""
+$AMDRadeon13URL = ""
+$AMDRadeon14URL = ""
+$AMDRadeon15URL = ""
+$AMDRadeon16URL = ""
+$AMDRadeon17URL = ""
+$AMDRadeon18URL = ""
+$AMDRadeon19URL = ""
+$AMDRadeon20URL = ""
+$AMDRadeon21URL = ""
+$AMDRadeon22URL = ""
+$AMDRadeon23URL = ""
+$AMDRadeon24URL = ""
+$AMDRadeon25URL = ""
+$AMDRadeon26URL = ""
+$AMDRadeon27URL = ""
+$AMDRadeon28URL = ""
+$AMDRadeon29URL = ""
+$AMDRadeon30URL = ""
+$AMDRadeon31URL = ""
+$AMDRadeon32URL = ""
+$AMDRadeon33URL = ""
+$AMDRadeon34URL = ""
+$AMDRadeon35URL = ""
+$AMDRadeon36URL = ""
 
 
 ### Dedicated Audio card
