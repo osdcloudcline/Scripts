8f3fbb0d7e566961834ff383944307b8bda74c15
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 12:13:02 -0400

    Update AdvancedUtilities.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/AdvancedUtilities.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/AdvancedUtilities.ps1
@@ -1,1 +1,3 @@
-
+Write-Host '  - Utilities: DDU, Driver Booster 11,  CCleaner                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: Smart Defrag, V2V Converter                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: Windows 11 SDK, ADK, WinPE Add-On                 ' -ForegroundColor DarkBlue -BackgroundColor White
