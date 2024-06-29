ebc6f28a17fddc49733c88d1ae9c14be8ec0e841
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 11:23:30 -0400

    Update Advanced.ps1

--- WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
+++ WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
@@ -1,1 +1,27 @@
-TEST
+Write-Host "              ADVANCED SOFTWARE CONFIGURATION                    " -ForegroundColor DarkBlue -BackgroundColor White 
+Write-Host "  The following software will be installed on $env:computername: " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "                                                                 " -BackgroundColor White 
+Write-Host "  - Web Browsers: Google Chrome and Mozilla Firefox              " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Cloud Backup: One Drive, DropBox and NextCloud Client        " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - File Transfer: Filezilla, PuTTY and WinSCP                   " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Gaming: Steam Client                                         " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Media: VLC Player, MediaInfo, MKVToolnix, iTunes,            " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Media: MakeMKV, Handbrake, PowerISO, Audacity, FFMPEG        " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Media: Logitech MyHarmony, GIMP, Kodi,                       " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Media: HD Video Converter Factory Pro, DVDFab                " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Productivity: Microsoft Office 2021 LTSC                     " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Productivity: Adobe Master Collection                        " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Programming: VS Code, AI Code Editor,                        " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Programming: Microsoft Visual Studio Enterprise 2022         " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - VDI/Remote Connections: Microsoft Remote Desktop,            " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - VDI/Remote Connections: VMWare Horizon Client,               " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - VDI/Remote Connections: AnyDesk,                             " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - VDI/Remote Connections: Microsoft 365 VDI                    " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Virtualization: VMWare Workstation Pro                       " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Utilities: DDU, Driver Booster 11,  CCleaner                 " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Utilities: Smart Defrag, V2V Converter                       " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Utilities: Windows 11 SDK, ADK, WinPE Add-On                 " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "  - Security: DOWNLOAD ONLY - MalwareBytes, Norton360            " -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "                                                                 " -BackgroundColor White 
+
+pause
