bf0c0ab959e8dbcb95c0ed8e5c433f014edbf973
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 16:10:19 -0400

    Create ManagementPCMedia.ps1

---
+++ WinGet /Install /Individual Scripts/Media/Management PC/ManagementPCMedia.ps1
@@ -0,0 +1,26 @@
+Start-Transcript -Path "C:\Logs\Powershell\Management PC\$env:computername-ManagementPCMediaInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VLC Media Player"
+$app2 = "MediaInfo"
+
+
+# Media-related software
+Write-Host "Installing Media Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Write-Verbose "Processing: $app2..." -Verbose
+Write-Host
+Write-Host "Installing $app2..." -ForegroundColor Cyan
+
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
