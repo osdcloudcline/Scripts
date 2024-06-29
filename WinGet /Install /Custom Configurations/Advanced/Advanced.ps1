5adb96760e340094a616a7b8edbeb9480300f2a1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 12:04:17 -0400

    Update Advanced.ps1

--- WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
+++ WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
@@ -53,7 +53,7 @@ Invoke-Expression $($Programming.Content)
 $VDI = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/VDI-Remote%20Desktop/Advanced/AdvancedVDIRDC.ps1")
 Invoke-Expression $($VDI.Content)
 
-$Virtualization = Invoke-WebRequest ("")
+$Virtualization = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Virtualization/Advanced/AdvancedVirtualization.ps1")
 Invoke-Expression $($Virtualization.Content)
 
 $Utilities = Invoke-WebRequest("")
