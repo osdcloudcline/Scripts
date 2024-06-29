7b1d9c8c99a2c522353e4ad32d264604968c0671
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 15:58:07 -0400

    Update StandardFileTransfer.ps1

--- WinGet /Install /Individual Scripts/File Transfer/Standard/StandardFileTransfer.ps1
+++ WinGet /Install /Individual Scripts/File Transfer/Standard/StandardFileTransfer.ps1
@@ -1,1 +1,19 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\$env:computername-StandardFileTransferInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "FileZilla"
+
+
+
+Write-Host "Installing File Transfer Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+choco install filezilla  -y
+
+Stop-Transcript
