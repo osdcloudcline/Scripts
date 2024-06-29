f233fa29ef6c57e3abb668b91fc5fbb545f8cf65
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 16:01:14 -0400

    Create AdvancedGaming.ps1

---
+++ WinGet /Install /Individual Scripts/Gaming/Advanced/AdvancedGaming.ps1
@@ -0,0 +1,18 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedGamingInstall.log"
+
+$Date = Get-Date
+
+$app1 = "Steam"
+
+
+Write-Host "Installed on: $Date"
+
+Write-Host "Installing Gaming Applications..." -ForegroundColor Green
+Write-Host
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+
+winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
