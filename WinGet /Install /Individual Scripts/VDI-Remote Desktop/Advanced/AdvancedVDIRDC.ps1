60fb1755d57c52060feb7caa261f0cc4102d6704
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-29 12:03:40 -0400

    Update AdvancedVDIRDC.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AdvancedVDIRDC.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AdvancedVDIRDC.ps1
@@ -1,8 +1,7 @@
 $app1 = "Microsoft Remote Desktop Client"
 $app2 = "VMWare Horizon Remote Desktop Connection Client"
 $app3 = "AnyDesk"
-$app4 = "VMWare Workstation Professional"
-$app5 = "Windows 365 Azure Virtual Desktop"
+$app4 = "Windows 365 Azure Virtual Desktop"
 
 # Virtualization related software
 Write-Host "Installing Virtualization and Remote Desktop Applications on $env:computername..." -ForegroundColor Green
@@ -17,7 +16,4 @@ Write-Host "Installing: $app3 on $env:computername..."  -ErrorAction SilentlyCon
 winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Host "Installing: $app4 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
-
-Write-Host "Installing: $app5 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
 winget install --id 9N1F85V9T8BN --exact --accept-source-agreements  --accept-source-agreements --force
