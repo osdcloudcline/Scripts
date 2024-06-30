a85dc5626181ccc63855a0cc91883d9a036bbfdc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-30 09:32:54 -0400

    Update ManagementPC.ps1

--- WinGet /Install /Custom Configurations/Management PC/ManagementPC.ps1
+++ WinGet /Install /Custom Configurations/Management PC/ManagementPC.ps1
@@ -20,6 +20,9 @@ Invoke-Expression $($PS5Modules.Content)
 $PS7Modules = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1")
 Invoke-Expression $($PS7Modules.Content)
 
+$ClientRSAT = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/Client%20OS%20Roles%20/InstallClientRSAT.ps1")
+Invoke-Expression $($ClientRSAT.Content)
+
 $Browsers = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Management%20PC/ManagementPCBrowsers.ps1")
 Invoke-Expression $($Browsers.Content)
 
