6a84b9891cb0b2d6d46fd0641c4cff5853cb198b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-30 09:08:48 -0400

    Update ManagementPC.ps1

--- WinGet /Install /Custom Configurations/Management PC/ManagementPC.ps1
+++ WinGet /Install /Custom Configurations/Management PC/ManagementPC.ps1
@@ -1,1 +1,15 @@
-
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '           MANAGEMENT PC SOFTWARE CONFIGURATION                  ' -ForegroundColor DarkBlue -BackgroundColor White 
+Write-Host '          The following software will be installed:              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '  - Web Browsers: Google Chrome                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Programming: VS Code, AI Code Editor,                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - VDI/Remote Connections: AnyDesk                              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - VDI/Remote Connections: Microsoft 365 VDI                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: Windows 11 SDK, ADK, WinPE Add-On                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Security: DOWNLOAD ONLY - MalwareBytes, Norton360            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '    Uninstallation of software will be available                 ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '    after script completes                                       ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White
+pause
