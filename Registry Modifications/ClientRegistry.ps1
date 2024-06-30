9b93c18f0eda8a33564c4f2bb0e8e96be59a32b3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-06-30 09:24:55 -0400

    Update ClientRegistry.ps1

--- Registry Modifications/ClientRegistry.ps1
+++ Registry Modifications/ClientRegistry.ps1
@@ -56,7 +56,7 @@ $AdobeRegFile1Url = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%
 $AdobeRegFile2Url = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Software/Adobe/Acrobat2.reg"
 $AdobeRegFile3Url = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Software/Adobe/AdobeCC.reg"
 $AdobeRegFile4Url = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Software/Adobe/AdobeCC2.reg"
-$AdobeRegFile5Url = ""
+$AdobeRegFile5Url = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Software/Adobe/AdobeCC3.reg"
 $PCMark10RegFileUrl = "https://github.com/osdcloudcline/Scripts/raw/main/REGFiles/PCMark10.reg"
 $3DMarkRegFileUrl = "https://github.com/osdcloudcline/Scripts/raw/main/REGFiles/3DMark.reg"
 
