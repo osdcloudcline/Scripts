c4509302a0f9c9d1ee3d1364485f240174bfe90e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-01 11:06:23 -0400

    Create Optiplex3060.ps1

---
+++ WinGet /Update/System Specific/Dell/Desktops/Micro Form Factor/Optiplex3060.ps1
@@ -0,0 +1,18 @@
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '              DELL OPTIPLEX 3060 UPDATE                          ' -ForegroundColor DarkBlue -BackgroundColor White 
+Write-Host '          The following software will be installed:              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '  - Web Browsers: Google Chrome                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Cloud Backup: Google Drive                                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Media: VLC Player, MediaInfo                                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Messaging: Cisco Webex and WebEx Meetings                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Productivity: Adobe Reader                                   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - VDI/Remote Connections: Microsoft Remote Desktop,            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: Driver Booster 11                `                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - System: Windows Terminal, Microsoft PowerShell,              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - System: Visual C++ Redistributables                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '    Uninstallation of software will be available                 ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '    after script completes                                       ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White
+pause
