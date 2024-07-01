c5cfe01a983f134434ed5b62401d344a9f773e3e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-01 12:02:14 -0400

    Update Optiplex3060.ps1

--- WinGet /Update/System Specific/Dell/Desktops/Micro Form Factor/Optiplex3060.ps1
+++ WinGet /Update/System Specific/Dell/Desktops/Micro Form Factor/Optiplex3060.ps1
@@ -34,3 +34,6 @@ Invoke-Expression $($Productivity.Content)
 
 $VDI = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/VDI%20Remote%20Connections/System%20Specific/Dell/Desktops/Micro%20Form%20Factor/Optiplex/3060/3060VDIRemote.ps1")
 Invoke-Expression $($VDI.Content)
+
+$Utilities = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Utilities/System%20Specific/Dell/Desktops/Micro%20Form%20Factor/Optiplex/3060/3060Utilties.ps1")
+Invoke-Expression $($Utilities.Content)
