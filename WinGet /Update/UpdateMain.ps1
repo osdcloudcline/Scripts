6a43522f36e6bc05f115cdb7047d16bd432d555a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-01 12:32:31 -0400

    Update UpdateMain.ps1

--- WinGet /Update/UpdateMain.ps1
+++ WinGet /Update/UpdateMain.ps1
@@ -28,43 +28,42 @@ do
   {
 
   '1' { cls
-        $WinGetSTDSoftwareScript = Invoke-WebRequest ""
+        $WinGetSTDSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetSTDSoftwareScript.Content)
         }
   '2' { cls
-        $WinGetADVSoftwareScript = Invoke-WebRequest "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/System-LOB%20Specific/UpdateADVSoftware.ps1"
+        $WinGetADVSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetADVSoftwareScript.Content)
         }
   '3' { cls
-        $WinGetCORPSoftwareScript = Invoke-WebRequest ""
+        $WinGetCORPSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetCORPSoftwareScript.Content)
         }
   '4' { cls
-        $WinGetVDISoftwareScript = Invoke-WebRequest ""
+        $WinGetVDISoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetVDISoftwareScript.Content)
         }
   '5' { cls
-        $WinGetManagementPCSoftwareScript = Invoke-WebRequest ""
+        $WinGetManagementPCSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetManagementPCSoftwareScript.Content)
         }
   '6' { cls
-        $WinGetHoneypotSoftwareScript = Invoke-WebRequest ""
+        $WinGetHoneypotSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetHoneypotSoftwareScript.Content)
         }
   '7' { cls
-        $WinGetITTechSoftwareScript = Invoke-WebRequest ""
+        $WinGetITTechSoftwareScript = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetITTechSoftwareScript.Content)
         }
   '8' { cls
-        $WinGetIndividualPCScripts = Invoke-WebRequest ""
-        Invoke-Expression $($WinGetIndividualPCScripts.Content)
+        Show-PCUpdateMain
         }
  '9' { cls
-        $WinGetIndividualSoftwareScripts = Invoke-WebRequest ""
+        $WinGetIndividualSoftwareScripts = Invoke-WebRequest ("")
         Invoke-Expression $($WinGetIndividualSoftwareScripts.Content)
         }
   '10' { cls
-        $WinGetMainMenuScript = Invoke-WebRequest "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1"
+        $WinGetMainMenuScript = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1")
         Invoke-Expression $($WinGetMainMenuScript.Content)
         }
 '11'{exit}
@@ -73,6 +72,79 @@ do
     until ($selection -eq '11'){}
     }
 
+##########################################################
+# Main Menu
+##########################################################
+Function Show-PCUpdateMain{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'Software Update -  Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
 
+Clear-Host
+Write-Host "======= $Title ======"
+Write-Host " 1. Update Jenn 2nd PC - Optiplex 3060"
+Write-Host " 2. Update Chris 2nd PC - Optiplex 7010"
+Write-Host " 3. Update Jess Laptop"
+Write-Host " 4. Update Bryan Desktop"
+Write-Host " 5. Update Bryan Laptop"
+Write-Host " 6. Update "
+Write-Host " 7. Update "
+Write-Host " 8. "
+Write-Host " 9. "
+Write-Host " 10. Return to Main Menu" 
+Write-Host " 11. Exit PowerShell"
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+
+  '1' { cls
+        $JennPC = Invoke-WebRequest ("")
+        Invoke-Expression $($JennPC.Content)
+        }
+  '2' { cls
+        $ChrisPC = Invoke-WebRequest ("")
+        Invoke-Expression $($ChrisPC.Content)
+        }
+  '3' { cls
+        $JessLaptop = Invoke-WebRequest ("")
+        Invoke-Expression $($JessLaptop.Content)
+        }
+  '4' { cls
+        $BryanDesktop = Invoke-WebRequest ("")
+        Invoke-Expression $($BryanDesktop.Content)
+        }
+  '5' { cls
+        $BryanLaptop = Invoke-WebRequest ("")
+        Invoke-Expression $($BryanLaptop.Content)
+        }
+  '6' { cls
+        $WinGetHoneypotSoftwareScript = Invoke-WebRequest ("")
+        Invoke-Expression $($WinGetHoneypotSoftwareScript.Content)
+        }
+  '7' { cls
+        $WinGetITTechSoftwareScript = Invoke-WebRequest ("")
+        Invoke-Expression $($WinGetITTechSoftwareScript.Content)
+        }
+  '8' { cls
+        $WinGetIndividualPCScripts = Invoke-WebRequest ("")
+        Invoke-Expression $($WinGetIndividualPCScripts.Content)
+        }
+ '9' { cls
+        $WinGetIndividualSoftwareScripts = Invoke-WebRequest ("")
+        Invoke-Expression $($WinGetIndividualSoftwareScripts.Content)
+        }
+  '10' { cls
+        $WinGetMainMenuScript = Invoke-WebRequest ("")
+        Invoke-Expression $($WinGetMainMenuScript.Content)
+        }
+'11'{exit}
+    }
+    }
+    until ($selection -eq '11'){}
+    }
       
     Show-MainMenu
