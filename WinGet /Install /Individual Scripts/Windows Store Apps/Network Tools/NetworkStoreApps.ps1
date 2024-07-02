0269a41139cbce027d4df2c03fa79005151f8e66
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-02 10:43:21 -0400

    Update NetworkStoreApps.ps1

--- WinGet /Install /Individual Scripts/Windows Store Apps/Network Tools/NetworkStoreApps.ps1
+++ WinGet /Install /Individual Scripts/Windows Store Apps/Network Tools/NetworkStoreApps.ps1
@@ -1,1 +1,4 @@
-$StoreApp13 = "Network Tools Desktop Edition"
+$StoreApp1 = "Network Tools Desktop Edition"
+
+Write-Verbose "Installing $StoreApp1..." -Verbose
+winget install --id 9NW6L8PLQMM4 --source msstore
