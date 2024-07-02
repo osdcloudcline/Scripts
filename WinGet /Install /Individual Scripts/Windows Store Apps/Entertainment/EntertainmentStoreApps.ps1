732b488ed1fc9f339bfe09e254948ab476a70a7d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-02 10:28:36 -0400

    Update EntertainmentStoreApps.ps1

--- WinGet /Install /Individual Scripts/Windows Store Apps/Entertainment/EntertainmentStoreApps.ps1
+++ WinGet /Install /Individual Scripts/Windows Store Apps/Entertainment/EntertainmentStoreApps.ps1
@@ -9,3 +9,21 @@ $StoreApp7 = "Paramount +"
 
 Write-Verbose "Installing $StoreApp1..." -Verbose
 winget install --id 9WZDNCRFJ3TJ --source msstore
+
+Write-Verbose "Installing $StoreApp2..." -Verbose
+winget install --id 9NWD3S85MTFB --source msstore
+
+Write-Verbose "Installing $StoreApp3..." -Verbose
+winget install --id 9P6RC76MSMMJ --source msstore
+
+Write-Verbose "Installing $StoreApp4..." -Verbose
+winget install --id 9NXQXXLFST89 --source msstore
+
+Write-Verbose "Installing $StoreApp5..." -Verbose
+winget install --id 9WZDNCRFJ3L1 --source msstore
+
+Write-Verbose "Installing $StoreApp6..." -Verbose
+winget install --id 9NM4T8B9JQZ1 --source msstore
+
+Write-Verbose "Installing $StoreApp7..." -Verbose
+winget install --id 9WZDNCRFJ0WK --source msstore
