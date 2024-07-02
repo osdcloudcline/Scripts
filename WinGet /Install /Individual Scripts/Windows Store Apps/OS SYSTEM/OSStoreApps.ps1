c888529b00c789c141f09741f2734aa532a01002
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-02 10:45:41 -0400

    Update OSStoreApps.ps1

--- WinGet /Install /Individual Scripts/Windows Store Apps/OS SYSTEM/OSStoreApps.ps1
+++ WinGet /Install /Individual Scripts/Windows Store Apps/OS SYSTEM/OSStoreApps.ps1
@@ -1,2 +1,8 @@
 $StoreApp1 = "Microsoft PC Manager"
 $StoreApp2 = "Windows 365"
+
+Write-Verbose "Installing $StoreApp1..." -Verbose
+winget install --id 9PM860492SZD --source msstore
+
+Write-Verbose "Installing $StoreApp2..." -Verbose
+winget install --id 9N1F85V9T8BN --source msstore
