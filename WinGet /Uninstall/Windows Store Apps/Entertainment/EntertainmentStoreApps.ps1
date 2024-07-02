7ac83b3fa5de81af12d83e9f5a8c0d42b800c09d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-02 10:30:28 -0400

    Create EntertainmentStoreApps.ps1

---
+++ WinGet /Uninstall/Windows Store Apps/Entertainment/EntertainmentStoreApps.ps1
@@ -0,0 +1,29 @@
+$StoreApp1 = "Netflix"
+$StoreApp2 = "ESPN"
+$StoreApp3 = "Amazon Prime Video"
+$StoreApp4 = "Disney +"
+$StoreApp5 = "Hulu"
+$StoreApp6 = "Apple TV +"
+$StoreApp7 = "Paramount +"
+
+
+Write-Verbose "Installing $StoreApp1..." -Verbose
+winget uninstall --id 9WZDNCRFJ3TJ --source msstore
+
+Write-Verbose "Installing $StoreApp2..." -Verbose
+winget uninstall --id 9NWD3S85MTFB --source msstore
+
+Write-Verbose "Installing $StoreApp3..." -Verbose
+winget uninstall --id 9P6RC76MSMMJ --source msstore
+
+Write-Verbose "Installing $StoreApp4..." -Verbose
+winget uninstall --id 9NXQXXLFST89 --source msstore
+
+Write-Verbose "Installing $StoreApp5..." -Verbose
+winget uninstall --id 9WZDNCRFJ3L1 --source msstore
+
+Write-Verbose "Installing $StoreApp6..." -Verbose
+winget uninstall --id 9NM4T8B9JQZ1 --source msstore
+
+Write-Verbose "Installing $StoreApp7..." -Verbose
+winget uninstall --id 9WZDNCRFJ0WK --source msstore
