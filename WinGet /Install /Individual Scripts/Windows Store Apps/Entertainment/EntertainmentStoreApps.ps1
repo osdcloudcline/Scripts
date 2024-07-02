6146cd139183a700575d560fdf51ed9404e3918d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-02 10:27:23 -0400

    Update EntertainmentStoreApps.ps1

--- WinGet /Install /Individual Scripts/Windows Store Apps/Entertainment/EntertainmentStoreApps.ps1
+++ WinGet /Install /Individual Scripts/Windows Store Apps/Entertainment/EntertainmentStoreApps.ps1
@@ -5,3 +5,7 @@ $StoreApp4 = "Disney +"
 $StoreApp5 = "Hulu"
 $StoreApp6 = "Apple TV +"
 $StoreApp7 = "Paramount +"
+
+
+Write-Verbose "Installing $StoreApp1..." -Verbose
+winget install --id 9WZDNCRFJ3TJ --source msstore
