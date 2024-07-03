5428aa5f35288423e95106167c3cd7ffe81bcc0e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-03 09:07:08 -0400

    Update Edge.ps1

--- WinGet /Uninstall/Browsers/Edge.ps1
+++ WinGet /Uninstall/Browsers/Edge.ps1
@@ -1,1 +1,7 @@
+$app1 = "Microsoft Edge"
 
+Write-Verbose "Removing $app1..." -Verbose
+winget uninstall Microsoft.Edge 
+
+Write-Verbose "Removing $app1..." -Verbose
+Remove-appxpackage Microsoft.MicrosoftEdge.Stable_126.0.2592.81_neutral__8wekyb3d8bbwe
