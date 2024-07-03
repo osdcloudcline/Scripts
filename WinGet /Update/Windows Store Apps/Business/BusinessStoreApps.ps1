9a8240da89b4227107677c137938e347f865fcb8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-03 07:37:09 -0400

    Update BusinessStoreApps.ps1

--- WinGet /Update/Windows Store Apps/Business/BusinessStoreApps.ps1
+++ WinGet /Update/Windows Store Apps/Business/BusinessStoreApps.ps1
@@ -2,8 +2,8 @@ $StoreApp1 = "Microsoft Office 365"
 $StoreApp2 = "Company Portal"
 
 
-Write-Verbose "Installing $StoreApp1..." -Verbose
+Write-Verbose "Updating $StoreApp1..." -Verbose
 winget upgrade --id 9WZDNCRD29V9 --source msstore
 
-Write-Verbose "Installing $StoreApp2..." -Verbose
+Write-Verbose "Updating $StoreApp2..." -Verbose
 winget upgrade --id 9WZDNCRFJ3PZ --source msstore
