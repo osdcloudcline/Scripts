abbdaed17f92a3615915cd66527ebbcf388714e2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-03 08:52:58 -0400

    Update Chrome.ps1

--- WinGet /Uninstall/Browsers/Chrome.ps1
+++ WinGet /Uninstall/Browsers/Chrome.ps1
@@ -2,4 +2,5 @@ $app1 = "Google Chrome for Enterprise"
 
 Write-Host "Removing $app1..." -ForegroundColor Cyan
 
-winget uninstall --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+winget uninstall --id Google.Chrome 
+
