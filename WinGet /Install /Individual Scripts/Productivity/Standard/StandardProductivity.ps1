4b4be302be4873643af2d28fa649ae073ddb4ec5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-03 19:13:30 -0400

    Update StandardProductivity.ps1

--- WinGet /Install /Individual Scripts/Productivity/Standard/StandardProductivity.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Standard/StandardProductivity.ps1
@@ -9,3 +9,11 @@ mkdir $OfficeGHdownloads
 $Office2021FilesUrl = "https://github.com/osdcloudcline/Software/raw/main/Productivity/Microsoft/Office%202021/OSDCloud-Office2021.zip"
 Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
 Expand-Archive -Path "$OfficeGHdownloads\OSDCloud-Office2021.zip" -DestinationPath $OfficeGHdownloads
+pause
+Write-Host "Processing: $app1 download..." -ForegroundColor Cyan
+$O21DLCMD = "C:\OSDCloud\GitHub\downloads\Office\OfficeCDNDownload.cmd"
+Start-Process -FilePath $O21DLCMD
+pause
+Write-Host "Processing: $app1 setup install..." -ForegroundColor Cyan
+$O21SetupCMD = "C:\OSDCloud\GitHub\downloads\Office\OfficeCDNSetup.cmd"
+Start-Process -FilePath $O21SetupCMD
