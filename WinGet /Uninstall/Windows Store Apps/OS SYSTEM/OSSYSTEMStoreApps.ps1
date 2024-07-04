847a21d6dfcd04055584ff86160cee01b78c8f57
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-04 07:30:21 -0400

    Update OSSYSTEMStoreApps.ps1

--- WinGet /Uninstall/Windows Store Apps/OS SYSTEM/OSSYSTEMStoreApps.ps1
+++ WinGet /Uninstall/Windows Store Apps/OS SYSTEM/OSSYSTEMStoreApps.ps1
@@ -1,1 +1,8 @@
+$StoreApp1 = "Microsoft PC Manager"
+$StoreApp2 = "Windows 365"
 
+Write-Verbose "Installing $StoreApp1..." -Verbose
+winget uninstall --id 9PM860492SZD --source msstore
+
+Write-Verbose "Installing $StoreApp2..." -Verbose
+winget uninstall --id 9N1F85V9T8BN --source msstore
