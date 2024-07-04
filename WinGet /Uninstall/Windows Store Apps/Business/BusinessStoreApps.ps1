aeb257287f3b4f7eb31d6b7f4dbe5f73ea38a53f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-04 07:24:59 -0400

    Update BusinessStoreApps.ps1

--- WinGet /Uninstall/Windows Store Apps/Business/BusinessStoreApps.ps1
+++ WinGet /Uninstall/Windows Store Apps/Business/BusinessStoreApps.ps1
@@ -3,7 +3,7 @@ $StoreApp2 = "Company Portal"
 
 
 Write-Verbose "Installing $StoreApp1..." -Verbose
-winget install --id 9WZDNCRD29V9 --source msstore
+winget uninstall --id 9WZDNCRD29V9 --source msstore
 
 Write-Verbose "Installing $StoreApp2..." -Verbose
-winget install --id 9WZDNCRFJ3PZ --source msstore
+winget uninstall --id 9WZDNCRFJ3PZ --source msstore
