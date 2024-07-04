49fd32a3385adb94153eae9c52d6a2e9d96ec45f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-04 19:01:38 -0400

    Update Office365.ps1

--- WinGet /Uninstall/Productivity/Office365.ps1
+++ WinGet /Uninstall/Productivity/Office365.ps1
@@ -1,3 +1,9 @@
 $app1 = "Microsoft 365 (Office)"
+$app2 = "Microsoft Office 365 for enterprise"
 
 
+Write-Host "Processing unistallation: $app1..." -ForegroundColor Cyan
+winget uninstall --id 9WZDNCRD29V9 --source msstore
+
+Write-Host "Processing unistallation: $app2..." -ForegroundColor Cyan
+winget uninstall --id Microsoft.Office
