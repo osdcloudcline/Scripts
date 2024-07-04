860ab341c9ee722df49ac7c0bcf4f1d16aa8f215
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-04 08:30:21 -0400

    Update BusinessStoreApps.ps1

--- WinGet /Uninstall/Windows Store Apps/Business/BusinessStoreApps.ps1
+++ WinGet /Uninstall/Windows Store Apps/Business/BusinessStoreApps.ps1
@@ -2,8 +2,8 @@ $StoreApp1 = "Microsoft Office 365"
 $StoreApp2 = "Company Portal"
 
 
-Write-Verbose "Installing $StoreApp1..." -Verbose
+Write-Verbose "Uninstalling $StoreApp1..." -Verbose
 winget uninstall --id 9WZDNCRD29V9 --source msstore
 
-Write-Verbose "Installing $StoreApp2..." -Verbose
+Write-Verbose "Uninstalling $StoreApp2..." -Verbose
 winget uninstall --id 9WZDNCRFJ3PZ --source msstore
