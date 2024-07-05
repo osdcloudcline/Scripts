cbdfd2c5b25118fd1f7989c41ab5982d7cee5d41
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 10:41:40 -0400

    Update UpdateHDVideoConverterFactoryPro.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateHDVideoConverterFactoryPro.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateHDVideoConverterFactoryPro.ps1
@@ -1,1 +1,20 @@
+$app1 = "HD Video Converter Factory Pro"
 
+Write-Host "Processing: $app1..." -Verbose
+Write-Host 
+Write-Host "Acquiring $app1 from vendor website..." -ForegroundColor Cyan
+$HDVideoSource = "https://us.videoconverterfactory.com/download/hd-video-converter-pro.exe"
+$HDVideoDestination = "C:\OSDCloud\GitHub\downloads"
+$HDVideoSetup = "C:\OSDCloud\GitHub\downloads\hd-video-converter-pro.exe"
+$HDVideoSetupTP = [System.IO.File]::Exists("C:\OSDCloud\GitHub\downloads\hd-video-converter-pro.exe")
+If($HDVideoSetupTP -eq $false){
+Save-WebFile -SourceUrl $HDVideoSource  -DestinationDirectory $HDVideoDestination
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+Start-Process -FilePath $HDVideoSetup
+}
+ElseIf($HDVideoSetupTP -eq $true){
+Remove-Item -Path $HDVideoSetup -Force
+Save-WebFile -SourceUrl $HDVideoSource  -DestinationDirectory $HDVideoDestination
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+Start-Process -FilePath $HDVideoSetup
+}
