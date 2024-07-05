a2d472b67a9ecc473a296e156d44509e83458090
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 11:23:24 -0400

    Update UpdateFilezilla.ps1

--- WinGet /Update/Individual Scripts/File Transfer/Advanced/UpdateFilezilla.ps1
+++ WinGet /Update/Individual Scripts/File Transfer/Advanced/UpdateFilezilla.ps1
@@ -1,1 +1,12 @@
+$app1 = "FileZilla"
 
+Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+$FZSQLFile = "C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3"
+$FZSQLDetect = [System.IO.File]::Exists("C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3")
+If($FZSQLDetect -eq $true){
+Remove-Item -Path $FZSQLFile -Force
+choco upgrade filezilla  -y
+}
+Elseif ($FZSQLDetect -eq $false){
+choco upgrade filezilla  -y
+}
