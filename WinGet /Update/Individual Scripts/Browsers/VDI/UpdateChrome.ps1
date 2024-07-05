d3dcc63d8c80612c2683fed859ee862db8485e6e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-04 21:41:58 -0400

    Update UpdateChrome.ps1

--- WinGet /Update/Individual Scripts/Browsers/VDI/UpdateChrome.ps1
+++ WinGet /Update/Individual Scripts/Browsers/VDI/UpdateChrome.ps1
@@ -1,1 +1,4 @@
+$app1 = "Google Chrome for Enterprise"
 
+Write-Host "Upgrading: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget upgrade --id Google.Chrome
