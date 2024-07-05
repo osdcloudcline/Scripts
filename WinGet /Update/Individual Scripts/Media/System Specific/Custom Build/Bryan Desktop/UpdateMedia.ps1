a0f985cfa5e29c2fac0236cfe823f3e637c4641d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 09:20:00 -0400

    Update UpdateMedia.ps1

--- WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
+++ WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
@@ -1,6 +1,15 @@
 $app1 = "VLC Media Player"
 $app2 = "MediaInfo"
-
+$app3 = "MKVToolNix"
+$app4 = "Apple iTunes"
+$app5 = "MakeMKV"
+$app6 = "Handbrake"
+$app7 = "PowerISO"
+$app8 = "Audacity and FFMPEG"
+$app9 = "Logitech MyHarmony"
+$app10 = "GIMP"
+$app11 = "Kodi"
+$app12 = "HD Video Converter Factory Pro"
 
 Write-Host "Updating $app1..." -ForegroundColor Cyan
 winget upgrade --id VideoLAN.VLC
