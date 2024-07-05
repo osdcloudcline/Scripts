200337f5fa3cd22fc8c7616695bb7243f1fe6320
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 13:20:48 -0400

    Update UpdateFileTransfers.ps1

--- WinGet /Update/Individual Scripts/File Transfer/System Specific/Custom Built/Bryan Desktop/UpdateFileTransfers.ps1
+++ WinGet /Update/Individual Scripts/File Transfer/System Specific/Custom Built/Bryan Desktop/UpdateFileTransfers.ps1
@@ -1,6 +1,6 @@
 $app1 = "FileZilla"
 $app2 = "PuTTy"
-
+$app1 = "WinSCP"
 
 Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 $FZSQLFile = "C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3"
@@ -13,8 +13,9 @@ Elseif ($FZSQLDetect -eq $false){
 choco upgrade filezilla  -y
 }
 
-
-
 Write-Host "Processing updates: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id PuTTY.PuTTY
+
+Write-Host "Processing updates: $app3 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget upgrade --id WinSCP.WinSCP
 
