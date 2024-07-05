c59db740cfd9d4b51ba7db099b6a262f638fb588
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 13:25:21 -0400

    Update UpdateGaming.ps1

--- WinGet /Update/Individual Scripts/Gaming/Custom Built/Bryan Desktop/UpdateGaming.ps1
+++ WinGet /Update/Individual Scripts/Gaming/Custom Built/Bryan Desktop/UpdateGaming.ps1
@@ -1,1 +1,4 @@
+$app1 = "Steam"
 
+Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget upgrade --id Valve.Steam
