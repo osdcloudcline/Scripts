53eab2cc835d774cc5a9c11dd5e747ffce0bf7e2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 09:19:23 -0400

    Update UpdateMedia.ps1

--- WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
+++ WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
@@ -1,5 +1,11 @@
 $app1 = "VLC Media Player"
+$app2 = "MediaInfo"
+
 
 Write-Host "Updating $app1..." -ForegroundColor Cyan
-
 winget upgrade --id VideoLAN.VLC
+
+
+
+Write-Host "Updating $app2..." -ForegroundColor Cyan
+winget upgrade --id MediaArea.MediaInfo.GUI
