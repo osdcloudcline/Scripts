51a35ca4985a60098759473230bed791ff25892b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:28:30 -0400

    Update UpdateAnyDesk.ps1

--- WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateAnyDesk.ps1
+++ WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateAnyDesk.ps1
@@ -1,1 +1,4 @@
 $app1 = "AnyDesk"
+
+Write-Host "Processing updates: $app1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget upgrade --id AnyDeskSoftwareGmbH.AnyDesk 
