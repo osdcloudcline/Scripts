00c0c357d7f0c2f1ada3ffd0a38a1844530a309c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:26:28 -0400

    Update UpdateMSRDCSoftware.ps1

--- WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateMSRDCSoftware.ps1
+++ WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateMSRDCSoftware.ps1
@@ -1,3 +1,4 @@
 $app1 = "Microsoft Remote Desktop Client"
 
-
+Write-Host "Processing updates: $app1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget upgrade --id Microsoft.RemoteDesktopClient 
