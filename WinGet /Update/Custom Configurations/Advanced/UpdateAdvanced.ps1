9c701ff2e8f4b2310207e3b2bf489a4aef315f04
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 11:00:25 -0400

    Update UpdateAdvanced.ps1

--- WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
+++ WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
@@ -101,6 +101,9 @@ Invoke-Expression $($MediaInfo.Content)
 $MyHarmony = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Media/Advanced/UpdateMyHarmony.ps1")
 Invoke-Expression $($MyHarmony.Content) 
 
+$PowerISO = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Media/Advanced/UpdatePowerISO.ps1")
+Invoke-Expression $($PowerISO.Content)
+
 
 Write-Host "Processing updates for: Productivity software..." -ForegroundColor Cyan
 
