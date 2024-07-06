b3dcf45ab3eeefe617905a3658ad99c756c0df98
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 16:46:33 -0400

    Update OSStoreApps.ps1

--- WinGet /Update/Windows Store Apps/OS SYSTEM/OSStoreApps.ps1
+++ WinGet /Update/Windows Store Apps/OS SYSTEM/OSStoreApps.ps1
@@ -1,8 +1,6 @@
 $StoreApp1 = "Microsoft PC Manager"
-$StoreApp2 = "Windows 365 Azure Virtual Desktop"
 
 Write-Verbose "Updating $StoreApp1..." -Verbose
 winget upgrade --id 9PM860492SZD --source msstore
 
-Write-Verbose "Updating $StoreApp2..." -Verbose
-winget upgrade --id 9N1F85V9T8BN --source msstore
+
