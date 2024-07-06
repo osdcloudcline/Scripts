cdea2774ef33edd7c81a26895887da3e155e8e2d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 21:13:03 -0400

    Update UpdateVisualStudio2022.ps1

--- WinGet /Update/Individual Scripts/Programming/System Specific/Desktops/Custom Built/Bryan's Desktop/UpdateVisualStudio2022.ps1
+++ WinGet /Update/Individual Scripts/Programming/System Specific/Desktops/Custom Built/Bryan's Desktop/UpdateVisualStudio2022.ps1
@@ -1,1 +1,4 @@
+$app1 = "Visual Studio 2022 Enterprise"
 
+Write-Host "Processing updates: $app1 on $env:computername ..." -ForegroundColor Yellow
+winget upgrade --id Microsoft.VisualStudio.2022.Enterprise
