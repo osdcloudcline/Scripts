67d5639355dd2aefe56840a3033cdc5d80a55a92
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:28:53 -0400

    Update UpdateVMWareHorizonClient.ps1

--- WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateVMWareHorizonClient.ps1
+++ WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateVMWareHorizonClient.ps1
@@ -1,4 +1,4 @@
 $app1 = "VMWare Horizon Remote Desktop Connection Client"
 
-Write-Host "Processing updates: $app2 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Processing updates: $app1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 choco upgrade vmware-horizon-client -y
