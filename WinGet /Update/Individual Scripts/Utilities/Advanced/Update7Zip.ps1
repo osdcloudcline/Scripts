6ae1d8edb717d74497b2d1cbabb82f05dd208b9f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 08:23:16 -0400

    Update Update7Zip.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/Update7Zip.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/Update7Zip.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "7-Zip"
 
-Write-Verbose "Processing update: $Utilitiesapp1..." -Verbose
+Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id 7zip.7zip
