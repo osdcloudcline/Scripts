fa4e854d3d85c109821d27563d923e0a26564f5d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:37:29 -0400

    Update StandardVDIRDC.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Standard/StandardVDIRDC.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Standard/StandardVDIRDC.ps1
@@ -1,7 +1,7 @@
 $app1 = "Microsoft Remote Desktop Client"
 $app2 = "VMWare Horizon Remote Desktop Connection Client"
 $app3 = "AnyDesk"
-$app4 = "VMWare Workstation Professional"
+
 
 
 # Virtualization related software
@@ -16,7 +16,6 @@ choco install vmware-horizon-client -y
 Write-Host "Installing: $app3 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing: $app4 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
+
 
 
