e861d8928c538049ccf608aa3df8e0fe5ba5a30c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 08:23:31 -0400

    Update UpdateCCleaner.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateCCleaner.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateCCleaner.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "CCleaner"
 
-Write-Verbose "Processing update: $Utilitiesapp1..." -Verbose
+Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id Piriform.CCleaner
