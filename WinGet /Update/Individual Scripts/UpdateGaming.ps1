73c49fbcfacdd63c7eb805306a967bf9f1eb4e76
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:41:17 -0400

    Delete WinGet /Update/Individual Scripts/UpdateGaming.ps1

--- WinGet /Update/Individual Scripts/UpdateGaming.ps1
+++
@@ -1,8 +0,0 @@
-$app1 = "Steam"
-
-Write-Host "Beginning Application Upgrades on $env:computername..." -ForegroundColor Green
-
-Write-Host "Upgrading Gaming Applications on $env:computername..." -ForegroundColor Green
-
-Write-Host "Upgrading: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget upgrade --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
