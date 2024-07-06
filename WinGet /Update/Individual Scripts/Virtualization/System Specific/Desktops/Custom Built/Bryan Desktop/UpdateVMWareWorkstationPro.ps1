ec906ec63a0bbd5bc58bd0b5172da01316d13853
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-05 20:56:52 -0400

    Delete WinGet /Update/Individual Scripts/Virtualization/System Specific/Desktops/Custom Built/Bryan Desktop/UpdateVMWareWorkstationPro.ps1

--- WinGet /Update/Individual Scripts/Virtualization/System Specific/Desktops/Custom Built/Bryan Desktop/UpdateVMWareWorkstationPro.ps1
+++
@@ -1,4 +0,0 @@
-$app1 = "VMWare Workstation Professional"
-
-Write-Host "Processing updates: $app1 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco upgrade vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
