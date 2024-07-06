12dafcee82b3d06c179e070c2ffdb4f950f4b873
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 10:32:20 -0400

    Update UpdateAdvanced.ps1

--- WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
+++ WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
@@ -74,6 +74,10 @@ Invoke-Expression $($Steam.Content)
 
 Write-Host "Processing updates for: Media software..." -ForegroundColor Cyan
 
+$AudacityFFMPEG = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Media/Advanced/UpdateAudacityFFMPEG.ps1")
+Invoke-Expression $($AudacityFFMPEG.Content)
+
+
 Write-Host "Processing updates for: Productivity software..." -ForegroundColor Cyan
 
 Write-Host "Processing updates for: Programming software..." -ForegroundColor Cyan
