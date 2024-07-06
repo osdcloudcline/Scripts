89ce9e8cdd1e4d9cbaee3811076cf7920f35c760
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-06 16:38:35 -0400

    Update UpdateAdvanced.ps1

--- WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
+++ WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
@@ -165,7 +165,7 @@ Invoke-Expression $($LogitechOptions.Content)
 $LogitechUnifyingSoftware = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Utilities/Advanced/UpdateLogitechUnifyingSoftware.ps1")
 Invoke-Expression $($LogitechUnifyingSoftware.Content)
 
-$Rufus = Invoke-WebRequest("")
+$Rufus = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Utilities/Advanced/UpdateRufus.ps1")
 Invoke-Expression $($Rufus.Content)
 
 $SamsungMagician = Invoke-WebRequest("")
