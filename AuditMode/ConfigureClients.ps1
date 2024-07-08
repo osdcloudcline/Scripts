c19042f3be319aadafd34ba59810e1627fdff342
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 00:22:43 -0400

    Update ConfigureClients.ps1

--- AuditMode/ConfigureClients.ps1
+++ AuditMode/ConfigureClients.ps1
@@ -1,36 +1,36 @@
 Write-Host "Beginning System Configuration..." -ForegroundColor Cyan
 
-Write-Verbose "Step 1 - Part 1 - PowerShell Package Providers..." -Verbose
+Write-Verbose "Step 1 - PowerShell Package Providers..." -Verbose
 
 $PSProviders = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Package%20Providers/Install.ps1")
 Invoke-Expression $($PSProviders.Content)
 
-Write-Verbose "Step 1 - Part 2 - PowerShell 5.1 Modules..." -Verbose
+Write-Verbose "Step 2 - PowerShell 5.1 Modules..." -Verbose
 
 $PS5Modules = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1")
 Invoke-Expression $($PS5Modules.Content)
 
-Write-Verbose "Step 1 - Part 3 - PowerShell 7.x Modules..." -Verbose 
+Write-Verbose "Step 3 - PowerShell 7.x Modules..." -Verbose 
 
 $PS7Modules = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1")
 Invoke-Expression $($PS7Modules.Content)
 
-Write-Verbose "Step 2 - Merging Registry Entries..." -Verbose
+Write-Verbose "Step 4 - Merging Registry Entries..." -Verbose
 
 $ClientRegistry = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/ClientRegistry.ps1")
 Invoke-Expression $($ClientRegistry.Content)
 
-Write-Verbose "Step 3 - Client Operating System RSAT Tools..." -Verbose
+Write-Verbose "Step 5 - Client Operating System RSAT Tools..." -Verbose
 
 $ClientRSAT = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/Client%20OS%20Roles%20/InstallClientRSAT.ps1")
 Invoke-Expression $($ClientRSAT.Content)
 
-Write-Verbose "Step 4 - Mandatory OS Pre-Requirements..." -Verbose
+Write-Verbose "Step 6 - Mandatory OS Pre-Requirements..." -Verbose
 
 $OS = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM/OS-Mandated%20Software/SYSTEM.ps1")
 Invoke-Expression $($OS.Content)
 
-Write-Verbose "Step 5 - Software Installation..." -Verbose
+Write-Verbose "Step 7 - Software Installation..." -Verbose
 
 $WinGetSoftware = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1")
 Invoke-Expression $($WingetSoftware.Content)
