57389abdc73920e3d2db4968966c2cc2af3e36f1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-07 22:32:24 -0400

    Update Install.ps1

--- PowerShell Modules/Package Providers/Install.ps1
+++ PowerShell Modules/Package Providers/Install.ps1
@@ -17,3 +17,22 @@ $PSProvider2 = "NuGet"
 $PSProvider3 = "WinGet"
 
 
+Write-Verbose "Installing: $PSProvider1..." -Verbose
+Install-PackageProvider -Name PackageManagement -Force -Source PSGallery
+
+Write-Verbose "Importing: $PSProvider1..." -Verbose
+Import-PackageProvider -Name PackageManagement -Force -Source PSGallery
+
+Write-Verbose "Installing: $PSProvider2..." -Verbose
+Install-PackageProvider -Name nuget -Force -Source PSGallery
+
+Write-Verbose "Importing: $PSProvider2..." -Verbose
+Import-PackageProvider -Name nuget -Force -Source PSGallery
+
+Write-Verbose "Installing: $PSProvider3..." -Verbose
+Install-PackageProvider -Name WinGet -Force -Source PSGallery
+
+Write-Verbose "Importing: $PSProvider3..." -Verbose
+Import-PackageProvider -Name WinGet -Force -Source PSGallery
+
+Stop-Transcript
