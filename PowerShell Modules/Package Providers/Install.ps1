c9cd3649b5c4dd7883413425f3f93a57be0696da
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-07 22:28:00 -0400

    Update Install.ps1

--- PowerShell Modules/Package Providers/Install.ps1
+++ PowerShell Modules/Package Providers/Install.ps1
@@ -2,4 +2,18 @@ $PSProvidersLogs = "C:\OSDCloud\DeploymentLogs\PSProviders\$env:computername-PSP
 Start-Transcript -Path $PSProvidersLogs
 Get-Date 
 
+############################
+### PS Module Group Names ##
+############################
+
+$PMGroup = "Operating System Package Providers"
+
+#############################
+## PS Package Providers   ##
+#############################
+
+$PSProvider1 = "Package Management (Oneget)"
+$PSProvider2 = "NuGet"
+$PSProvider3 = "WinGet"
+
 
