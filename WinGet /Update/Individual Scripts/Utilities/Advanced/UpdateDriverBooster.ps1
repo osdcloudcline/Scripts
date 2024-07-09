095e7f08c7134be918fc04f87edfa463fdefb40a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:55:16 -0400

    Update UpdateDriverBooster.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateDriverBooster.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateDriverBooster.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "IObit Driver Booster"
 
-Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id IObit.DriverBooster
