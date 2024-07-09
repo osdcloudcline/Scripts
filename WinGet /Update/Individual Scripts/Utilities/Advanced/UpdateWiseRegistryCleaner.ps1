1cd3ea646960d26f875404154f68b82d99e9ed56
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:57:37 -0400

    Update UpdateWiseRegistryCleaner.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateWiseRegistryCleaner.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateWiseRegistryCleaner.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "Wise Registry Cleaner"
 
-Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id XPDLS1XBTXVPP4 --source msstore
