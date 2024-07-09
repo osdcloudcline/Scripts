ea36aa9c93366a96968ee8f6e68e9d2add45076d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:48:09 -0400

    Update 3060Messaging.ps1

--- WinGet /Update/Individual Scripts/Messaging/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Messaging.ps1
+++ WinGet /Update/Individual Scripts/Messaging/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Messaging.ps1
@@ -1,9 +1,9 @@
 $Messaging1 = "Cisco WebEx"
 $Messaging2 = "Cisco WebEx Meetings"
 
-Write-Verbose "Updating $Messaging1 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $Messaging1 on $env:computername..." -Verbose
 winget upgrade --id XPDLZV7R88FF1P
 
-Write-Verbose "Updating $Messaging2 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $Messaging2 on $env:computername..." -Verbose
 winget uninstall --id Cisco.CiscoWebexMeetings
 winget install --id Cisco.CiscoWebexMeetings
