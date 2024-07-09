1684fa2aa54d34ce30d725dd5332adcd0ed3a3f8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-09 00:41:24 -0400

    Update ITCloudBackup.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/IT Tech/ITCloudBackup.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/IT Tech/ITCloudBackup.ps1
@@ -9,6 +9,7 @@ $app2 = "Nextcloud Desktop Client"
 $app3 = "DropBox"
 $app4 = "Microsoft OneDrive"
 $app5 = "User Profile Backup and Restore"
+$app6 = "Apple iCloud"
 
 Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
 
@@ -23,29 +24,25 @@ Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
-
-
 
 Write-Verbose "Processing: $app2..." -Verbose
 Write-Host
 Write-Host "Installing $app2..." -ForegroundColor Cyan
-
 winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app3..." -Verbose
 Write-Host
 Write-Host "Installing $app3..." -ForegroundColor Cyan
-
 winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app4..." -Verbose
 Write-Host
 Write-Host "Installing $app4..." -ForegroundColor Cyan
-
 winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
+Write-Verbose "Processing: $app5..." -Verbose
+Write-Host
 Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
 
 $UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
@@ -58,5 +55,10 @@ $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
 $Shortcut.TargetPath = "C:\downloads\UPBR.exe"
 $Shortcut.Save()
+
+Write-Verbose "Processing: $app6..." -Verbose
+Write-Host
+Write-Host "Installing $app6..." -ForegroundColor Cyan
+winget install --id Apple.iCloud --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
