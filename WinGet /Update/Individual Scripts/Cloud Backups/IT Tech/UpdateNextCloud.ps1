eed29cac3b3e10de1f198e49a633f0ca66db3191
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:19:48 -0400

    Update UpdateNextCloud.ps1

--- WinGet /Update/Individual Scripts/Cloud Backups/IT Tech/UpdateNextCloud.ps1
+++ WinGet /Update/Individual Scripts/Cloud Backups/IT Tech/UpdateNextCloud.ps1
@@ -1,4 +1,4 @@
 $app1 = "Nextcloud Desktop Client"
 
-Write-Host "Processing update: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Nextcloud.NextcloudDesktop
