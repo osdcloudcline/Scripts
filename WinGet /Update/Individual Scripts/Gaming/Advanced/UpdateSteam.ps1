e80e27a3ed53473b9cfddd4db94591cb6e745a80
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:33:05 -0400

    Update UpdateSteam.ps1

--- WinGet /Update/Individual Scripts/Gaming/Advanced/UpdateSteam.ps1
+++ WinGet /Update/Individual Scripts/Gaming/Advanced/UpdateSteam.ps1
@@ -1,4 +1,4 @@
 $app1 = "Steam"
 
-Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Valve.Steam
