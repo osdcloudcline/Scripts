8ff18d8d9ceb5892d3ad44177b4c053ed0bf5bc1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:57:01 -0400

    Update UpdateSamsungMagician.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateSamsungMagician.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateSamsungMagician.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "Samsung Magician"
 
-Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id Samsung.SamsungMagician
