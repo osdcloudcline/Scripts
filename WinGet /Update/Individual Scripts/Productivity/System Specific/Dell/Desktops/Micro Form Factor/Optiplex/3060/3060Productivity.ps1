8c61d977113445376e7ece4f3d330dfbb4035e44
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:52:43 -0400

    Update 3060Productivity.ps1

--- WinGet /Update/Individual Scripts/Productivity/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Productivity.ps1
+++ WinGet /Update/Individual Scripts/Productivity/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Productivity.ps1
@@ -1,5 +1,5 @@
 $Productivity1 = "Adobe Reader DC"
 
 
-Write-Verbose "Updating $Productivity1 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $Productivity1..." -Verbose
 winget upgrade --id Adobe.Acrobat.Reader.64-bit
