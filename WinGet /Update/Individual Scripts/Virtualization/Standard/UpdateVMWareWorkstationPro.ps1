85962398621cb1b923f34b25bcb8ff1d23cab6fd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 22:12:26 -0400

    Update UpdateVMWareWorkstationPro.ps1

--- WinGet /Update/Individual Scripts/Virtualization/Standard/UpdateVMWareWorkstationPro.ps1
+++ WinGet /Update/Individual Scripts/Virtualization/Standard/UpdateVMWareWorkstationPro.ps1
@@ -1,4 +1,4 @@
 $app1 = "VMWare Workstation Professional"
 
-Write-Host "Processing updates: $app1 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Write-Host "Checking for and processing update: $app1..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
 choco upgrade vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
