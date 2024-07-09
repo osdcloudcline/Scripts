095eb9485bf12cccd5ed8697210c6175a9edec5c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:36:30 -0400

    Update UpdateMyHarmony.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateMyHarmony.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateMyHarmony.ps1
@@ -1,4 +1,4 @@
 $app1 = "Logitech MyHarmony"
 
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Logitech.MyHarmony
