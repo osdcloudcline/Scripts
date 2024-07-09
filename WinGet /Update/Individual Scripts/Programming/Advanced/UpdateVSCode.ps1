55c1e3d408ef4592e6bcf4e877536d42814f9d4e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:53:42 -0400

    Update UpdateVSCode.ps1

--- WinGet /Update/Individual Scripts/Programming/Advanced/UpdateVSCode.ps1
+++ WinGet /Update/Individual Scripts/Programming/Advanced/UpdateVSCode.ps1
@@ -1,4 +1,4 @@
 $app1 = "VS Code"
 
-Write-Host "Processing updates: $app1 on $env:computername ..." -ForegroundColor Yellow
+Write-Host "Checking for and processing update: $app1 on $env:computername ..." -ForegroundColor Yellow
 winget upgrade --id Microsoft.VisualStudioCode
