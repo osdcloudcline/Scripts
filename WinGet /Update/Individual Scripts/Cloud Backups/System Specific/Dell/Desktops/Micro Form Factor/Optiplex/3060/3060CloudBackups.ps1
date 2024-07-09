972cf67fcf5c66b7321561f1b951bf4bdf9c63e5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:22:24 -0400

    Update 3060CloudBackups.ps1

--- WinGet /Update/Individual Scripts/Cloud Backups/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060CloudBackups.ps1
+++ WinGet /Update/Individual Scripts/Cloud Backups/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060CloudBackups.ps1
@@ -1,4 +1,4 @@
 $CloudBackups1 = "Google Drive"
 
-Write-Verbose "Updating $CloudBackups1 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $CloudBackups1 on $env:computername..." -Verbose
 winget upgrade --id Google.GoogleDrive
