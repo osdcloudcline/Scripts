827610fdf25b5bada7fc6cf7a143ab9cf7baaf1a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:46:20 -0400

    Update UpdateMedia.ps1

--- WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
+++ WinGet /Update/Individual Scripts/Media/System Specific/Custom Build/Bryan Desktop/UpdateMedia.ps1
@@ -11,43 +11,42 @@ $app10 = "GIMP"
 $app11 = "Kodi"
 $app12 = "HD Video Converter Factory Pro"
 
-Write-Verbose "Updating Media-related software..." -Verbose
-Write-Host
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id VideoLAN.VLC
 
-Write-Host "Processing: $app2..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app2..." -ForegroundColor Cyan
 winget upgrade --id MediaArea.MediaInfo.GUI
 
-Write-Host "Processing: $app3..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app3..." -ForegroundColor Cyan
 winget upgrade --id MoritzBunkus.MKVToolNix
 
-Write-Host "Processing: $app4..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app4..." -ForegroundColor Cyan
 winget upgrade --id Apple.iTunes
 
-Write-Host "Processing: $app5..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app5..." -ForegroundColor Cyan
 winget upgrade --id GuinpinSoft.MakeMKV
 
-Write-Host "Processing: $app6..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app6..." -ForegroundColor Cyan
 winget upgrade --id HandBrake.HandBrake
 
-Write-Host "Processing: $app7..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app7..." -ForegroundColor Cyan
 winget upgrade --id PowerSoftware.PowerISO 
 
-Write-Host "Processing: $app8..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app8..." -ForegroundColor Cyan
 winget upgrade --id Audacity.Audacity
 choco upgrade audacity-ffmpeg -y
 
-Write-Host "Processing: $app9..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app9..." -ForegroundColor Cyan
 winget upgrade --id Logitech.MyHarmony
 
-Write-Host "Processing: $app10..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app10..." -ForegroundColor Cyan
 winget upgrade --id GIMP.GIMP
 
-Write-Host "Processing: $app11..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app11..." -ForegroundColor Cyan
 winget install --id XBMCFoundation.Kodi
 
-Write-Host "Processing: $app12..." -Verbose
+Write-Host "Checking for and processing update: $app12..." -Verbose
 Write-Host 
 Write-Host "Acquiring $app12 from vendor website..." -ForegroundColor Cyan
 $HDVideoSource = "https://us.videoconverterfactory.com/download/hd-video-converter-pro.exe"
