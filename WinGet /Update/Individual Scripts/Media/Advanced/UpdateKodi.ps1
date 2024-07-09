188e087347e13e2fc4442f687701270008f61e25
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:35:25 -0400

    Update UpdateKodi.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateKodi.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateKodi.ps1
@@ -1,4 +1,4 @@
 $app1 = "Kodi"
 
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id XBMCFoundation.Kodi
