7ec47fdc60ecc80c0b728065731234d12a6ec9a7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-09 00:40:14 -0400

    Update AdvancedCloudBackup.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/AdvancedCloudBackup.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/AdvancedCloudBackup.ps1
@@ -9,12 +9,12 @@ $app2 = "Nextcloud Desktop Client"
 $app3 = "DropBox"
 $app4 = "Microsoft OneDrive"
 $app5 = "User Profile Backup and Restore"
+$app6 = "Apple iCloud"
 
 Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
 
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
 Import-Module -Name OSD -Force
-
 
 
 # Cloud Backup software
@@ -23,7 +23,6 @@ Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
 
@@ -31,21 +30,20 @@ winget install --id Google.GoogleDrive --exact --accept-source-agreements  --acc
 Write-Verbose "Processing: $app2..." -Verbose
 Write-Host
 Write-Host "Installing $app2..." -ForegroundColor Cyan
-
 winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app3..." -Verbose
 Write-Host
 Write-Host "Installing $app3..." -ForegroundColor Cyan
-
 winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app4..." -Verbose
 Write-Host
 Write-Host "Installing $app4..." -ForegroundColor Cyan
-
 winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
 
+Write-Verbose "Processing: $app5..." -Verbose
+Write-Host
 Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
 
 $UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
@@ -58,5 +56,10 @@ $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
 $Shortcut.TargetPath = "C:\downloads\UPBR.exe"
 $Shortcut.Save()
+
+Write-Verbose "Processing: $app6..." -Verbose
+Write-Host
+Write-Host "Installing $app6..." -ForegroundColor Cyan
+winget install --id Apple.iCloud --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
