f9d4f8fe9f7b948fdb1c6f111319add8b9e29ab6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 22:02:00 -0400

    Update UpdateWindows365AzureClient.ps1

--- WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateWindows365AzureClient.ps1
+++ WinGet /Update/Individual Scripts/VDI Remote Connections/Advanced/UpdateWindows365AzureClient.ps1
@@ -1,4 +1,4 @@
 $app1 = "Windows 365 Azure Virtual Desktop"
 
-Write-Host "Processing updates: $app1 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Write-Host "Checking for and processing update: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
 winget upgrade --id 9N1F85V9T8BN --source msstore
