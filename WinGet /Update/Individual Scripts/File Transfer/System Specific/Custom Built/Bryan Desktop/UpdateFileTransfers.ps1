a443e70a7900dc7314965e2e849fe3233bed3524
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:32:39 -0400

    Update UpdateFileTransfers.ps1

--- WinGet /Update/Individual Scripts/File Transfer/System Specific/Custom Built/Bryan Desktop/UpdateFileTransfers.ps1
+++ WinGet /Update/Individual Scripts/File Transfer/System Specific/Custom Built/Bryan Desktop/UpdateFileTransfers.ps1
@@ -2,7 +2,7 @@ $app1 = "FileZilla"
 $app2 = "PuTTy"
 $app1 = "WinSCP"
 
-Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 $FZSQLFile = "C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3"
 $FZSQLDetect = [System.IO.File]::Exists("C:\Users\$env:username\AppData\Roaming\FileZilla\queue.sqlite3")
 If($FZSQLDetect -eq $true){
@@ -13,9 +13,9 @@ Elseif ($FZSQLDetect -eq $false){
 choco upgrade filezilla  -y
 }
 
-Write-Host "Processing updates: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app2 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id PuTTY.PuTTY
 
-Write-Host "Processing updates: $app3 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app3 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id WinSCP.WinSCP
 
