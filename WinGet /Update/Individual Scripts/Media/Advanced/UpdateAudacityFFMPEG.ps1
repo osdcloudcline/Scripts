c2ccb9f140cd0f97f8dbf9784b980b27e5a29ffe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:34:15 -0400

    Update UpdateAudacityFFMPEG.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateAudacityFFMPEG.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateAudacityFFMPEG.ps1
@@ -1,5 +1,5 @@
 $app1 = "Audacity and FFMPEG"
 
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Audacity.Audacity
 choco upgrade audacity-ffmpeg -y
