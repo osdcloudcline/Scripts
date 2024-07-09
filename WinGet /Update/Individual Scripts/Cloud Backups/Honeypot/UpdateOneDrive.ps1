88d135a25b0f1931ec1c0355f34433762a5a78ea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:18:53 -0400

    Update UpdateOneDrive.ps1

--- WinGet /Update/Individual Scripts/Cloud Backups/Honeypot/UpdateOneDrive.ps1
+++ WinGet /Update/Individual Scripts/Cloud Backups/Honeypot/UpdateOneDrive.ps1
@@ -1,4 +1,4 @@
 $app1 = "Microsoft OneDrive"
 
-Write-Host "Processing update: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Microsoft.OneDrive
