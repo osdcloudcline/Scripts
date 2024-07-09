0565335880de3d28029e549db5aafa2e8e647688
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:36:00 -0400

    Update UpdateMakeMKV.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateMakeMKV.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateMakeMKV.ps1
@@ -1,4 +1,4 @@
 $app1 = "MakeMKV"
 
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id GuinpinSoft.MakeMKV
