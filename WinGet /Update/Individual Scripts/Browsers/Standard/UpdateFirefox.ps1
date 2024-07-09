c491537857b284da468fff94a0e1de43992d5f7f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:14:00 -0400

    Update UpdateFirefox.ps1

--- WinGet /Update/Individual Scripts/Browsers/Standard/UpdateFirefox.ps1
+++ WinGet /Update/Individual Scripts/Browsers/Standard/UpdateFirefox.ps1
@@ -1,4 +1,4 @@
 $app1 = "Mozilla Firefox"
 
-Write-Host "Upgrading: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id Mozilla.Firefox
