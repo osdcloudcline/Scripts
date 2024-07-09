b5f75031d2bc92290c98fa558170dd7f83c6788e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:46:52 -0400

    Update 3060Media.ps1

--- WinGet /Update/Individual Scripts/Media/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Media.ps1
+++ WinGet /Update/Individual Scripts/Media/System Specific/Dell/Desktops/Micro Form Factor/Optiplex/3060/3060Media.ps1
@@ -1,8 +1,8 @@
 $Media1 = "VLC Player"
 $Media2 = "MediaInfo"
 
-Write-Verbose "Updating $Media1 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $Media1 on $env:computername..." -Verbose
 winget upgrade --id VideoLAN.VLC
 
-Write-Verbose "Updating $Media2 on $env:computername..." -Verbose
+Write-Verbose "Checking for and processing update: $Media2 on $env:computername..." -Verbose
 winget upgrade --id MediaArea.MediaInfo.GUI
