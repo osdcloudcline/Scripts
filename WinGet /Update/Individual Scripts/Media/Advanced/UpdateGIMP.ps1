e816078ec1d31df0762c5e7f0b76e64b9f16680b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:34:33 -0400

    Update UpdateGIMP.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/UpdateGIMP.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/UpdateGIMP.ps1
@@ -1,4 +1,4 @@
 $app1 = "GIMP"
 
-Write-Host "Processing: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id GIMP.GIMP
