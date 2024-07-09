a066389d38fda559b86f3dd9beec2008ca8bc84f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:52:06 -0400

    Update AcrobatReaderDC.ps1

--- WinGet /Update/Individual Scripts/Productivity/Standard/AcrobatReaderDC.ps1
+++ WinGet /Update/Individual Scripts/Productivity/Standard/AcrobatReaderDC.ps1
@@ -1,5 +1,5 @@
 $app1 = "Adobe Acrobat Reader"
 
-Write-Host "Processing update: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Adobe.Acrobat.Reader.64-bit
 
