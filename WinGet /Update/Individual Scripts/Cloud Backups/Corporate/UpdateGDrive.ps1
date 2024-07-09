b5c46c768e09864d8ff6465f32f24dc2d30f534d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:18:17 -0400

    Update UpdateGDrive.ps1

--- WinGet /Update/Individual Scripts/Cloud Backups/Corporate/UpdateGDrive.ps1
+++ WinGet /Update/Individual Scripts/Cloud Backups/Corporate/UpdateGDrive.ps1
@@ -1,4 +1,4 @@
 $app1 = "Google Drive"
 
-Write-Host "Processing updates: $app1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Google.GoogleDrive
