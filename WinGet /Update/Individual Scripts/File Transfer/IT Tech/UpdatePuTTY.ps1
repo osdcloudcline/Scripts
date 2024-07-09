ce5d14785fba1e45d6cdfe412dd64c8c53736d57
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:30:47 -0400

    Update UpdatePuTTY.ps1

--- WinGet /Update/Individual Scripts/File Transfer/IT Tech/UpdatePuTTY.ps1
+++ WinGet /Update/Individual Scripts/File Transfer/IT Tech/UpdatePuTTY.ps1
@@ -1,4 +1,4 @@
 $app1 = "PuTTy"
 
-Write-Host "Processing updates: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget upgrade --id PuTTY.PuTTY
