0ecb7a483a7475a4f93b204ad9c7bba6a514d09a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-08 21:57:18 -0400

    Update UpdateWindowsAdminCenter.ps1

--- WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateWindowsAdminCenter.ps1
+++ WinGet /Update/Individual Scripts/Utilities/Advanced/UpdateWindowsAdminCenter.ps1
@@ -1,4 +1,4 @@
 $Utilitiesapp1 = "Windows Admin Center"
 
-Write-Host "Processing update: $Utilitiesapp1..." -ForegroundColor Cyan
+Write-Host "Checking for and processing update: $Utilitiesapp1..." -ForegroundColor Cyan
 winget upgrade --id Microsoft.WindowsAdminCenter
