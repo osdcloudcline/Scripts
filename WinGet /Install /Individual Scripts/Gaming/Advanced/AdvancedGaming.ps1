4f257eeffd9590f9c7e422c2c4dfaf323e0d2a6d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:04:14 -0400

    Update AdvancedGaming.ps1

--- WinGet /Install /Individual Scripts/Gaming/Advanced/AdvancedGaming.ps1
+++ WinGet /Install /Individual Scripts/Gaming/Advanced/AdvancedGaming.ps1
@@ -7,12 +7,8 @@ $app1 = "Steam"
 
 Write-Host "Installed on: $Date"
 
-Write-Host "Installing Gaming Applications..." -ForegroundColor Green
-Write-Host
-Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
