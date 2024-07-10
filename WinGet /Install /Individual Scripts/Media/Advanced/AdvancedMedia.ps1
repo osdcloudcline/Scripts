6a6e51c9f65804b0b1dd24a2b6ee0c7eb4a65a11
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:06:14 -0400

    Update AdvancedMedia.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/AdvancedMedia.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/AdvancedMedia.ps1
@@ -18,74 +18,51 @@ $app11 = "Kodi"
 $app12 = "HD Video Converter Factory Pro"
 
 # Media-related software
-Write-Host "Installing Media Applications..." -ForegroundColor Green
-Write-Host
-Write-Verbose "Processing: $app1..." -Verbose
+
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
 
-
-Write-Verbose "Processing: $app2..." -Verbose
 Write-Host
 Write-Host "Installing $app2..." -ForegroundColor Cyan
-
 winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app3..." -Verbose
 Write-Host
 Write-Host "Installing $app3..." -ForegroundColor Cyan
-
 winget install --id MoritzBunkus.MKVToolNix --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app4..." -Verbose
 Write-Host
 Write-Host "Installing $app4..." -ForegroundColor Cyan
-
 winget install --id Apple.iTunes --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app5..." -Verbose
 Write-Host
 Write-Host "Installing $app5..." -ForegroundColor Cyan
-
 winget install --id GuinpinSoft.MakeMKV --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app6..." -Verbose
 Write-Host
 Write-Host "Installing $app6..." -ForegroundColor Cyan
-
 winget install --id HandBrake.HandBrake --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app7..." -Verbose
+
 Write-Host
 Write-Host "Installing $app7..." -ForegroundColor Cyan
-
 winget install --id PowerSoftware.PowerISO --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app8..." -Verbose
 Write-Host
 Write-Host "Installing $app8..." -ForegroundColor Cyan
-
 winget install --id Audacity.Audacity --exact --accept-source-agreements  --accept-source-agreements --force
 choco install audacity-ffmpeg -y
 
-Write-Verbose "Processing: $app9..." -Verbose
 Write-Host
 Write-Host "Installing $app9..." -ForegroundColor Cyan
-
 winget install --id Logitech.MyHarmony --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app10..." -Verbose
 Write-Host
 Write-Host "Installing $app10..." -ForegroundColor Cyan
-
 winget install --id GIMP.GIMP --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Verbose "Processing: $app11..." -Verbose
 Write-Host
 Write-Host "Installing $app11..." -ForegroundColor Cyan
-
 winget install --id XBMCFoundation.Kodi --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Verbose "Processing: $app12..." -Verbose
