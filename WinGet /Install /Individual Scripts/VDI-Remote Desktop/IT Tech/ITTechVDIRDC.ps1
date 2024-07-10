3b210efe036ed11119ffb73668f7c9df71625caf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:14:03 -0400

    Update ITTechVDIRDC.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/IT Tech/ITTechVDIRDC.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/IT Tech/ITTechVDIRDC.ps1
@@ -1,22 +1,20 @@
 $app1 = "Microsoft Remote Desktop Client"
 $app2 = "VMWare Horizon Remote Desktop Connection Client"
 $app3 = "AnyDesk"
-$app4 = "VMWare Workstation Professional"
+
 
 
 # Virtualization related software
 Write-Host "Installing Virtualization and Remote Desktop Applications on $env:computername..." -ForegroundColor Green
 
-Write-Host "Installing: $app1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing: $app2 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app2..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 choco install vmware-horizon-client -y
 
-Write-Host "Installing: $app3 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app3..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host "Installing: $app4 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
 
 
