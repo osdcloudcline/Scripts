36523f316c92bfe793628a0c438f6cad54ced50b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:12:23 -0400

    Update AdvancedVDIRDC.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AdvancedVDIRDC.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AdvancedVDIRDC.ps1
@@ -6,13 +6,13 @@ $app3 = "AnyDesk"
 # Virtualization related software
 Write-Host "Installing Virtualization and Remote Desktop Applications on $env:computername..." -ForegroundColor Green
 
-Write-Host "Installing: $app1 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing: $app2 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app2..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 choco install vmware-horizon-client -y
 
-Write-Host "Installing: $app3 on $env:computername..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Write-Host "Installing: $app3..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
 
 
