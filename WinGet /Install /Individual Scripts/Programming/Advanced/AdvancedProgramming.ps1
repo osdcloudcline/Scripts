531cb2bd2068b356f0e1fbbdaca6be770ef09faa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:07:40 -0400

    Update AdvancedProgramming.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/AdvancedProgramming.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/AdvancedProgramming.ps1
@@ -8,23 +8,15 @@ $app1 = "VS Code"
 $app2 = "AI Cursor Editor"
 $app3 = "Microsoft Visual Studio Enterprise 2022"
 
-Write-Host "Installing Programming Applications..." -ForegroundColor Green
-Write-Host
-Write-Verbose "Processing: $app1..." -Verbose
+
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
 
 Write-Host
-Write-Verbose "Processing: $app2..." -Verbose
-Write-Host
 Write-Host "Installing $app2..." -ForegroundColor Cyan
-
 winget install --id CursorAI,Inc.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
 
-Write-Host
-Write-Verbose "Processing: $app3..." -Verbose
 Write-Host
 Write-Host "Installing $app3..." -ForegroundColor Cyan
 choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
