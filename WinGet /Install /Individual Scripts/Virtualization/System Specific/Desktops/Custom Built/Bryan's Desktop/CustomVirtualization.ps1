132965652d6f418a8f33d0cbfc625b2a1f56ce2a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-10 01:15:52 -0400

    Update CustomVirtualization.ps1

--- WinGet /Install /Individual Scripts/Virtualization/System Specific/Desktops/Custom Built/Bryan's Desktop/CustomVirtualization.ps1
+++ WinGet /Install /Individual Scripts/Virtualization/System Specific/Desktops/Custom Built/Bryan's Desktop/CustomVirtualization.ps1
@@ -1,4 +1,4 @@
 $app1 = "VMWare Workstation Professional"
 
-Write-Host "Installing: $app1 on $env:computername ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Write-Host "Installing: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
 choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
