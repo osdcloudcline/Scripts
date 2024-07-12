8974e1c24412a842404560672d2baf2493817cd3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:12:01 -0400

    Update GDrive.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GDrive.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GDrive.ps1
@@ -5,3 +5,8 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "Google Drive"
+
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
