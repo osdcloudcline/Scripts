1ff96f897fad51ee5f27f0a638193b2173d1a1f6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:16:39 -0400

    Update DropBox.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/DropBox.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/DropBox.ps1
@@ -5,3 +5,7 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "DropBox"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
