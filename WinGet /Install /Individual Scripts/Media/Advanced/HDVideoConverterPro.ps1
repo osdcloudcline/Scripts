3ce6062b12a76f390c3c184b48282b79d26916ba
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 12:38:17 -0400

    Update HDVideoConverterPro.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/HDVideoConverterPro.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/HDVideoConverterPro.ps1
@@ -3,3 +3,17 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMe
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "HD Video Converter Factory Pro"
+
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host 
+Write-Host "Acquiring $app1 from vendor website..." -ForegroundColor Cyan
+$HDVideoSource = "https://us.videoconverterfactory.com/download/hd-video-converter-pro.exe"
+$HDVideoDestination = "C:\OSDCloud\GitHub\downloads"
+$HDVideoSetup = "C:\OSDCloud\GitHub\downloads\hd-video-converter-pro.exe"
+Save-WebFile -SourceUrl $HDVideoSource  -DestinationDirectory $HDVideoDestination
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+Start-Process -FilePath $HDVideoSetup
+
+Stop-Transcript
