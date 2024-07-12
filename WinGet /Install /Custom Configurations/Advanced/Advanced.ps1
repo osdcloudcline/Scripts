d46a48bb06e66ed24e0c2247c1964e0ad1b9001d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 09:23:37 -0400

    Update Advanced.ps1

--- WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
+++ WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
@@ -69,7 +69,7 @@ Invoke-Expression $($FileZilla.Content)
 $PuTTY = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/File%20Transfer/Advanced/PuTTY.ps1")
 Invoke-Expression $($PuTTY.Content)
 
-$WinSCP = Invoke-WebRequest("")
+$WinSCP = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/File%20Transfer/Advanced/WinSCP.ps1")
 Invoke-Expression $($WinSCP.Content)
 
 Write-Host "Processing install for: Gaming software..." -ForegroundColor Cyan
