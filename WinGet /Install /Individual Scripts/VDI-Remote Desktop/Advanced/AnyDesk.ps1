90f6f6b9fbb62f07964a27c718aee22f8482a195
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 19:48:38 -0400

    Update AnyDesk.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AnyDesk.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AnyDesk.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "AnyDesk"
+
+Write-Host
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
