e1aee1837f6c418e91de4a61ea7ecf21e89a407a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 12:35:15 -0400

    Update FFMPEG.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/FFMPEG.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/FFMPEG.ps1
@@ -3,3 +3,11 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMe
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "FFMPEG for Audacity"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+choco install audacity-ffmpeg -y
+
+Stop-Transcript
