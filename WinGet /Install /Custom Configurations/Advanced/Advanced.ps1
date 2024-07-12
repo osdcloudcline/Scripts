72dc93f5ac6c0cf1ae137abd69bd12433c4ae52a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 13:44:02 -0400

    Update Advanced.ps1

--- WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
+++ WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
@@ -81,9 +81,44 @@ Invoke-Expression $($Steam.Content)
 
 Write-Host "Processing install for: Media software..." -ForegroundColor Cyan
 
-$Audacity = Invoke-WebRequest("")
+$Audacity = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/Audacity.ps1")
 Invoke-Expression $($Audacity.Content)
 
+$FFMPEG = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/FFMPEG.ps1")
+Invoke-Expression $($FFMPEG.Content)
+
+$GIMP = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/GIMP.ps1")
+Invoke-Expression $($GIMP.Content)
+
+$HDVideo = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/HDVideoConverterPro.ps1")
+Invoke-Expression $($HDVideo.Content)
+
+$Handbrake = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/Handbrake.ps1")
+Invoke-Expression $($Handbrake.Content)
+
+$Kodi = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/Kodi.ps1")
+Invoke-Expression $($Kodi.Content)
+
+$MKVToolNix = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/MKVToolNix.ps1")
+Invoke-Expression $($MKVToolNix.Content)
+
+$MakeMKV = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/MakeMKV.ps1")
+Invoke-Expression $($MakeMKV.Content)
+
+$MediaInfo = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/MediaInfo.ps1")
+Invoke-Expression $($MediaInfo.Content)
+
+$MyHarmony = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/MyHarmony.ps1")
+Invoke-Expression $($MyHarmony.Content)
+
+$PowerISO = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/PowerISO.ps1")
+Invoke-Expression $($PowerISO.Content)
+
+$VLCPlayer = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/VLCMediaPlayer.ps1")
+Invoke-Expression $($VLCPlayer.Content)
+
+$iTunes = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Advanced/iTunes.ps1")
+Invoke-Expression $($iTunes.Content)
 
 
 Write-Host "Processing install for: Productivity software..." -ForegroundColor Cyan
