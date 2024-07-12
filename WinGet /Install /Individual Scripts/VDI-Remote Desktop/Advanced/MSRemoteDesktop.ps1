9bffee355699984a3cbbfb451062dbad34216bc9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 19:49:54 -0400

    Update MSRemoteDesktop.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/MSRemoteDesktop.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/MSRemoteDesktop.ps1
@@ -4,10 +4,10 @@ $Date = Get-Date
 
 Write-Host "Installed on: $Date"
 
-$app1 = "AnyDesk"
+$app1 = "Microsoft Remote Desktop Client"
 
 Write-Host
 Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
+winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
