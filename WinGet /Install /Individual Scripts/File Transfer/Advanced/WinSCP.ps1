af65c1891c63a36ff8887909c14107430dc391da
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 09:19:26 -0400

    Update WinSCP.ps1

--- WinGet /Install /Individual Scripts/File Transfer/Advanced/WinSCP.ps1
+++ WinGet /Install /Individual Scripts/File Transfer/Advanced/WinSCP.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "WinSCP"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id WinSCP.WinSCP --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
