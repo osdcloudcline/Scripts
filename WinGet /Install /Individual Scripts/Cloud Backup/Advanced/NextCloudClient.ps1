dae8e0f4c1ba2f4fa3f123a3c8e677b9d9745eaa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:14:19 -0400

    Update NextCloudClient.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/NextCloudClient.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/NextCloudClient.ps1
@@ -5,3 +5,6 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "Nextcloud Desktop Client"
+
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
