fd78674b39b35c94808371ad0fc7a0ef513c283c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 15:04:03 -0400

    Update VSCode.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/VSCode.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/VSCode.ps1
@@ -1,1 +1,7 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedProgramming-VSCodeInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VS Code"
