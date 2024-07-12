d094fc3ccf2fcacd83cfb763d4369b9f419f4d54
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:20:33 -0400

    Update iCloud.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/iCloud.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/iCloud.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "Apple iCloud"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Apple.iCloud --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
