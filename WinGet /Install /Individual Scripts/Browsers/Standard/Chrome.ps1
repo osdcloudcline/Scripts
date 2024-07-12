4c7318a03fc3c9a608b611266052e9a317b16c66
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:00:00 -0400

    Update Chrome.ps1

--- WinGet /Install /Individual Scripts/Browsers/Standard/Chrome.ps1
+++ WinGet /Install /Individual Scripts/Browsers/Standard/Chrome.ps1
@@ -1,1 +1,14 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\$env:computername-StandardBrowsersChromeInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Google Chrome for Enterprise"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+Write-Host
+
+Stop-Transcript
