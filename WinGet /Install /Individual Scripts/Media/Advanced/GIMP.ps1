ee77bd14bdfde060e94167788ca089a9b0062ce0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 12:36:46 -0400

    Update GIMP.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/GIMP.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/GIMP.ps1
@@ -3,3 +3,11 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMe
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "GIMP"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id GIMP.GIMP --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
