02a6904d2457d66b3a5f04ab7e8396551522833f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 15:06:14 -0400

    Update VS2022Enterprise.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/VS2022Enterprise.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/VS2022Enterprise.ps1
@@ -3,3 +3,5 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedPr
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "Microsoft Visual Studio Enterprise 2022"
