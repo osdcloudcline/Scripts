cbcce7286160c50c44b1a8b45473d96bf13dc50c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 19:50:49 -0400

    Update VMWareHorizon.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/VMWareHorizon.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/VMWareHorizon.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedVDI-VMWAreHorizonInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VMWare Horizon Client"
+
+Write-Host
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
