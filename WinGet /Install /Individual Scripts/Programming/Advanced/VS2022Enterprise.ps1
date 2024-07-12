4ce3aa81958e94a18f010a909110b6464cb2b6c7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 15:08:17 -0400

    Update VS2022Enterprise.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/VS2022Enterprise.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/VS2022Enterprise.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "Microsoft Visual Studio Enterprise 2022"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id CursorAI,Inc.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
