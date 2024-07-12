4011397bbf8a975204affa9d9f27f9a34c500de7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 12:42:27 -0400

    Update MKVToolNix.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/MKVToolNix.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/MKVToolNix.ps1
@@ -9,3 +9,5 @@ $app1 = "MKVToolNix"
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
 winget install --id MoritzBunkus.MKVToolNix --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
