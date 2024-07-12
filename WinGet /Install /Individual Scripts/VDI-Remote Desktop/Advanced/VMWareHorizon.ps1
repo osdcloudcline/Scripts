a7f6c8759bb1ff1fe1f42ce0e10447d941b226df
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 19:51:21 -0400

    Update VMWareHorizon.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/VMWareHorizon.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/VMWareHorizon.ps1
@@ -8,6 +8,6 @@ $app1 = "VMWare Horizon Client"
 
 Write-Host
 Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements  --accept-source-agreements --force
+choco install vmware-horizon-client -y
 
 Stop-Transcript
