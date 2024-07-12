95c13729ffe30dfb4e60dd37a78ad1df58faee4c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 15:11:52 -0400

    Update Advanced.ps1

--- WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
+++ WinGet /Install /Custom Configurations/Advanced/Advanced.ps1
@@ -141,8 +141,14 @@ Invoke-Expression $($Office2024.Content)
 
 Write-Host "Processing install for: Programming software..." -ForegroundColor Cyan
 
-$Programming = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Programming/Advanced/AdvancedProgramming.ps1")
-Invoke-Expression $($Programming.Content)
+$VSCode = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Programming/Advanced/VSCode.ps1")
+Invoke-Expression $($VSCode.Content)
+
+$AICursor = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Programming/Advanced/AICursorEditor.ps1")
+Invoke-Expression $($AICursor.Content)
+
+$VS2022Enterprise = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Programming/Advanced/VS2022Enterprise.ps1")
+Invoke-Expression $($VS2022Enterprise.Content)
 
 Write-Host "Processing install for: Vitual Desktop Interface software..." -ForegroundColor Cyan
 
