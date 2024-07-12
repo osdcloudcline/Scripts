85114177bfb4674b8ba2507ecb77fec1945ff59f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 12:51:01 -0400

    Update iTunes.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/iTunes.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/iTunes.ps1
@@ -3,3 +3,11 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMe
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "Apple iTunes"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Apple.iTunes --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
