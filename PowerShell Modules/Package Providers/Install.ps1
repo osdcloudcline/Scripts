d7bc29a0784f31296b9d52eb4b9c3c09c6056cec
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-11 23:50:50 -0400

    Update Install.ps1

--- PowerShell Modules/Package Providers/Install.ps1
+++ PowerShell Modules/Package Providers/Install.ps1
@@ -12,29 +12,30 @@ $PMGroup = "Operating System Package Providers"
 ## PS Package Providers   ##
 #############################
 
-$PSProvider1 = "Package Management (Oneget)"
+$PSProvider1 = "PowerShellGet (Oneget)"
 $PSProvider2 = "NuGet"
 $PSProvider3 = "WinGet"
-$PSProvider4 = "chocolatey"
+$PSProvider4 = "Chocolatier"
+$PSProvider5 = "Programs"
 
 Write-Verbose "Installing: $PSProvider1..." -Verbose
-Install-PackageProvider -Name PackageManagement -Force
+Install-PackageProvider -Name PowerShellGet -RequiredVersion 2.2.5.0 -Force
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Importing: $PSProvider1..." -Verbose
-Import-PackageProvider -Name PackageManagement -Force 
+Import-PackageProvider -Name PowerShellGet -Force 
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Installing: $PSProvider2..." -Verbose
-Install-PackageProvider -Name nuget -Force 
+Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force 
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Importing: $PSProvider2..." -Verbose
-Import-PackageProvider -Name nuget -Force 
+Import-PackageProvider -Name NuGet -Force 
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Installing: $PSProvider3..." -Verbose
-Install-PackageProvider -Name WinGet -Force 
+Install-PackageProvider -Name WinGet -RequiredVersion 0.0.8.0 -Force 
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Importing: $PSProvider3..." -Verbose
@@ -42,10 +43,17 @@ Import-PackageProvider -Name WinGet -Force
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Installing: $PSProvider4..." -Verbose
-Install-PackageProvider -Name chocolatey -Force 
+Install-PackageProvider -Name Chocolatier -RequiredVersion 1.2.0.0 -Force 
 Start-Sleep -Seconds 5
 Write-Host
 Write-Verbose "Importing: $PSProvider4..." -Verbose
-Import-PackageProvider -Name chocolatey -Force 
+Import-PackageProvider -Name Chocolatier -Force 
+Write-Host
+Write-Verbose "Installing: $PSProvider5..." -Verbose
+Install-PackageProvider -Name Programs  -RequiredVersion 3.0.0.0 -Force 
+Start-Sleep -Seconds 5
+Write-Host
+Write-Verbose "Importing: $PSProvider5..." -Verbose
+Import-PackageProvider -Name Programs  -Force 
 Write-Host
 Stop-Transcript
