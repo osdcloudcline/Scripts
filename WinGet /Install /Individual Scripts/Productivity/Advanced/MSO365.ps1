3a3f609c7065b8be5a9ac931be4d97fb3eb25f23
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 14:52:58 -0400

    Update MSO365.ps1

--- WinGet /Install /Individual Scripts/Productivity/Advanced/MSO365.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Advanced/MSO365.ps1
@@ -2,7 +2,7 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedPr
 
 
 
-$app1 = "Adobe Acrobat Reader"
+$app1 = "Microsoft 365 (Office)"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
 winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
