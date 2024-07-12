5c2b458141f887078a36ba9352babe347038505b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 09:34:45 -0400

    Update Audacity.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/Audacity.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/Audacity.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-AudacityInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Audacity"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Audacity.Audacity --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
