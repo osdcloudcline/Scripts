34d9275d5e32994da2aed0facaa1b277e30f1e6d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 14:55:10 -0400

    Update Office365AppsEnterprise.ps1

--- WinGet /Install /Individual Scripts/Productivity/Advanced/Office365AppsEnterprise.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Advanced/Office365AppsEnterprise.ps1
@@ -1,10 +1,8 @@
 Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedProductivity-MSO365AppsEnterpriseInstall.log"
 
-
-
 $app1 = "Microsoft Office 365 for enterprise"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
-winget install --id 9WZDNCRD29V9 --source msstore
+winget install --id Microsoft.Office --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
