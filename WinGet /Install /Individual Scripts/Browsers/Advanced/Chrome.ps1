a3458e6767bb2fd1beca5799952634ee4d4ac561
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 07:51:37 -0400

    Update Chrome.ps1

--- WinGet /Install /Individual Scripts/Browsers/Advanced/Chrome.ps1
+++ WinGet /Install /Individual Scripts/Browsers/Advanced/Chrome.ps1
@@ -5,3 +5,10 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "Google Chrome for Enterprise"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+Write-Host
+
+
