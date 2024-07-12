e6dfd8e383a37a3d55183e34b380448d33ca8405
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 14:57:15 -0400

    Update Office2021.ps1

--- WinGet /Install /Individual Scripts/Productivity/Advanced/Office2021.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Advanced/Office2021.ps1
@@ -3,11 +3,21 @@ Import-Module -Name OSD -Force
 
 Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedProductivity-Office2021Install.log"
 
-
-
 $app1 = "Microsoft Office 2021 LTSC"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
-winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
+$OfficeGHdownloads = "C:\OSDCloud\GitHub\downloads\Office" 
+mkdir $OfficeGHdownloads
+$Office2021FilesUrl = "https://github.com/osdcloudcline/Software/raw/main/Productivity/Microsoft/Office%202021/OSDCloud-Office2021.zip"
+Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
+Expand-Archive -Path "$OfficeGHdownloads\OSDCloud-Office2021.zip" -DestinationPath $OfficeGHdownloads
+pause
+Write-Host "Processing: $app1 download..." -ForegroundColor Cyan
+$O21DLCMD = "C:\OSDCloud\GitHub\downloads\Office\OfficeCDNDownload.cmd"
+Start-Process -FilePath $O21DLCMD
+pause
+Write-Host "Processing: $app1 setup install..." -ForegroundColor Cyan
+$O21SetupCMD = "C:\OSDCloud\GitHub\downloads\Office\OfficeCDNSetup.cmd"
+Start-Process -FilePath $O21SetupCMD
 
 Stop-Transcript
