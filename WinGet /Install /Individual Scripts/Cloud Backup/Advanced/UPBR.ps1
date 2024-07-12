75249fbf94a82cfc96e7d265e8e315a6468a5e74
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:22:29 -0400

    Update UPBR.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/UPBR.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/UPBR.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "User Profile Backup and Restore"
+
+Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
