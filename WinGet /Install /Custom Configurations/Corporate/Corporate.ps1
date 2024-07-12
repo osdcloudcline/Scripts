f73eacfdd479ad46e55eec369786e50f7f907459
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:08:01 -0400

    Update Corporate.ps1

--- WinGet /Install /Custom Configurations/Corporate/Corporate.ps1
+++ WinGet /Install /Custom Configurations/Corporate/Corporate.ps1
@@ -23,8 +23,8 @@ Write-Host '                                                                 ' -
 pause
 
 
-$Browsers = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Corporate/CorporateBrowsers.ps1")
-Invoke-Expression $($Browsers.Content)
+$Chrome = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Corporate/Chrome.ps1")
+Invoke-Expression $($Chrome.Content)
 
 $CloudBackup = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Corporate/CorporateCloudBackup.ps1")
 Invoke-Expression $($CloudBackup.Content)
