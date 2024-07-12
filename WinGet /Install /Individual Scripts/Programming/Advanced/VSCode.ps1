5e29d2e2174b266d4c628cf81838685ef1e47169
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 15:06:59 -0400

    Update VSCode.ps1

--- WinGet /Install /Individual Scripts/Programming/Advanced/VSCode.ps1
+++ WinGet /Install /Individual Scripts/Programming/Advanced/VSCode.ps1
@@ -5,3 +5,9 @@ $Date = Get-Date
 Write-Host "Installed on: $Date"
 
 $app1 = "VS Code"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
