2ab3b1dfd24e6026e8bd9d6c05cb4e776ad1c7c3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 14:56:10 -0400

    Update Office2021.ps1

--- WinGet /Install /Individual Scripts/Productivity/Advanced/Office2021.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Advanced/Office2021.ps1
@@ -1,4 +1,13 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedProductivity-Office2021Install.log"
 
+
+
+$app1 = "Adobe Acrobat Reader"
+
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
