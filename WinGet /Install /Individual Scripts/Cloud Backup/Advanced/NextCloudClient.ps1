98a01ced0bf01c6594a701f42177e0128df912aa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:14:47 -0400

    Update NextCloudClient.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/NextCloudClient.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/NextCloudClient.ps1
@@ -8,3 +8,5 @@ $app1 = "Nextcloud Desktop Client"
 
 Write-Host "Installing $app1..." -ForegroundColor Cyan
 winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
