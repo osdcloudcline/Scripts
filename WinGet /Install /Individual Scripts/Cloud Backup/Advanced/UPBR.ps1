6e9b285d43306653beb89fe86bea0a8d5d62fb63
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:23:01 -0400

    Update UPBR.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/UPBR.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/UPBR.ps1
@@ -11,3 +11,17 @@ Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
 Import-Module -Name OSD -Force
 
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host
+Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -ForegroundColor Green
+
+$UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
+$UPBRFilePath = "C:\downloads\UserProfileBackupRestore.exe"
+$UPBRFilePathNewName = "C:\downloads\UPBR.exe"
+$UPBRDir = "C:\downloads"
+Save-WebFile -SourceUrl $UPBR -DestinationDirectory $UPBRDir
+Rename-Item -Path $UPBRFilePath  -NewName $UPBRFilePathNewName
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
+$Shortcut.TargetPath = "C:\downloads\UPBR.exe"
+$Shortcut.Save()
