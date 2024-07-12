01bd91e9bc78f38fe121e4cb2f9cfc9691e20943
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-12 08:06:41 -0400

    Update Chrome.ps1

--- WinGet /Install /Individual Scripts/Browsers/Corporate/Chrome.ps1
+++ WinGet /Install /Individual Scripts/Browsers/Corporate/Chrome.ps1
@@ -6,14 +6,9 @@ Write-Host "Installed on: $Date"
 
 $app1 = "Google Chrome for Enterprise"
 
-
-
-Write-Host "Installing Web Browsing Applications..." -ForegroundColor Green
-Write-Host
 Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
