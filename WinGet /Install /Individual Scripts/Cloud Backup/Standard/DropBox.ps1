6f9be793d4bbfed637b8aa7a7b3bea0e34acca57
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 16:48:36 -0400

    Update DropBox.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Standard/DropBox.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Standard/DropBox.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Cloud Backup\$env:computername-DropBoxInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "DropBox"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
