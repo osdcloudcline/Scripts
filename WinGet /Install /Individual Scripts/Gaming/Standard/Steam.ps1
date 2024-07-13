4fabea7208ddaf3936c2b2309dd12d9f2b870b21
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 18:04:50 -0400

    Update and rename StandardGaming.ps1 to Steam.ps1

--- WinGet /Install /Individual Scripts/Gaming/Standard/Steam.ps1
+++ WinGet /Install /Individual Scripts/Gaming/Standard/Steam.ps1
@@ -1,18 +1,13 @@
-Start-Transcript -Path "C:\Logs\Powershell\Standard\$env:computername-StandardGamingInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Gaming\$env:computername-SteamInstall.log"
 
 $Date = Get-Date
 
 $app1 = "Steam"
 
-
 Write-Host "Installed on: $Date"
 
-Write-Host "Installing Gaming Applications..." -ForegroundColor Green
-Write-Host
-Write-Verbose "Processing: $app1..." -Verbose
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-
 winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
