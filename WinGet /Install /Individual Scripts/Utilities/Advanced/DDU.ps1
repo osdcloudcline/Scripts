7176a9b9988548e14beae8c7509a7494030ebf48
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 01:19:03 -0400

    Update DDU.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/DDU.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/DDU.ps1
@@ -9,3 +9,5 @@ $app1 = "Display Driver Uninstaller"
 Write-Host
 Write-Verbose "Installing $app1..." -Verbose
 winget install --id Wagnardsoft.DisplayDriverUninstaller --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
