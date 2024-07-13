1bfb39d189a219b80075fceb0d0bf3770822d196
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 01:04:53 -0400

    Update CCleaner.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/CCleaner.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/CCleaner.ps1
@@ -1,1 +1,11 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedUtilities-CCleanerInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "CCleaner"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Piriform.CCleaner --exact --accept-source-agreements  --accept-source-agreements --force
