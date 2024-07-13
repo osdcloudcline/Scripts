d5dd64ef5f2ab1a4482668a64b30a67a23820d87
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 01:13:46 -0400

    Update DDU.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/DDU.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/DDU.ps1
@@ -1,1 +1,11 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedUtilities-DDUInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Display Driver Uninstaller"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Wagnardsoft.DisplayDriverUninstaller --exact --accept-source-agreements  --accept-package-agreements --force
