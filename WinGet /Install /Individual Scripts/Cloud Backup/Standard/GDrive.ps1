2c5493227f2df7711906cbc61489199b00a3a571
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 17:56:58 -0400

    Update GDrive.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Standard/GDrive.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Standard/GDrive.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Cloud Backup\$env:computername-GDriveInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Google Drive"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
