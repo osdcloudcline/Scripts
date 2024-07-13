ae7dd2e9c90ec1130ab0176407801762bcf85b20
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 08:10:57 -0400

    Update LogitechUnifyingSoftware.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/LogitechUnifyingSoftware.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/LogitechUnifyingSoftware.ps1
@@ -7,7 +7,7 @@ Write-Host "Installed on: $Date"
 $app1 = "Logitech Unifying Software"
 
 Write-Host
-Write-Verbose "Installing $app27..." -Verbose
+Write-Verbose "Installing $app1..." -Verbose
 winget install --id Logitech.UnifyingSoftware --exact --accept-source-agreements  --accept-package-agreements --force
 
 Stop-Transcript
