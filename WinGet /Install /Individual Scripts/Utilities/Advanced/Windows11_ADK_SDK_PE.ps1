e58bcd6e08795428f84f9d522cf55be1cd453c5f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 01:09:09 -0400

    Update Windows11_ADK_SDK_PE.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/Windows11_ADK_SDK_PE.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/Windows11_ADK_SDK_PE.ps1
@@ -1,1 +1,46 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedUtilities-OSADKSDKPEInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Windows 11 22H2 SDK"
+$app2 = "Windows 11 22H2 ADK"
+$app3 = "Windows 11 22H2 ADK PE Add-On"
+
+
+Write-Host
+Write-Verbose "Acquiring $app1 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$Win11_22H2SDK = "C:\OSDCloud\GitHub\downloads\winsdksetup.exe"
+$Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe'
+Write-Verbose "Processing and Downloading: $app1 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_22H2SDKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath $Win11_22H2SDK
+
+pause
+
+Write-Host
+Write-Verbose "Acquiring $app2 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$Win11_22H2ADK = "C:\OSDCloud\GitHub\downloads\adksetup.exe"
+$Win11_22H2ADKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe'
+Write-Verbose "Processing and Downloading: $app2 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_22H2ADKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath $Win11_22H2ADK
+
+pause
+
+Write-Host
+Write-Verbose "Acquiring $app3 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$Win11_22H2PEADK = "C:\OSDCloud\GitHub\downloads\adkwinpesetup.exe"
+$Win11_22H2PEADKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe'
+Write-Verbose "Processing and Downloading: $app3 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_22H2PEADKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath $Win11_22H2PEADK 
+
+Stop-Transcript
