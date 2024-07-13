f9bd1ce9c0786ba29a57b8db405fda946eae99f9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 08:09:18 -0400

    Update LogitechUnifyingSoftware.ps1

--- WinGet /Install /Individual Scripts/Utilities/Advanced/LogitechUnifyingSoftware.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/LogitechUnifyingSoftware.ps1
@@ -3,3 +3,11 @@ Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedUt
 $Date = Get-Date
 
 Write-Host "Installed on: $Date"
+
+$app1 = "Logitech Unifying Software"
+
+Write-Host
+Write-Verbose "Installing $app27..." -Verbose
+winget install --id Logitech.UnifyingSoftware --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
