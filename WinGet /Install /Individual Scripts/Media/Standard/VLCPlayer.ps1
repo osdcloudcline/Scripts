2c62ef548b26a06659e5b4a603e0877ced02fe95
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 22:13:11 -0400

    Update VLCPlayer.ps1

--- WinGet /Install /Individual Scripts/Media/Standard/VLCPlayer.ps1
+++ WinGet /Install /Individual Scripts/Media/Standard/VLCPlayer.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-VLCPlayerInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VLC Media Player"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
