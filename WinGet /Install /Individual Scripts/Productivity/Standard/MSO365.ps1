f526959767bc521549aadc05662f4dbf02f3f72d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-14 00:15:06 -0400

    Update MSO365.ps1

--- WinGet /Install /Individual Scripts/Productivity/Standard/MSO365.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Standard/MSO365.ps1
@@ -1,1 +1,8 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Productivity\$env:computername-MSO365Install.log"
 
+$app1 = "Microsoft 365 (Office)"
+
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+winget install --id 9WZDNCRD29V9 --source msstore
+
+Stop-Transcript
