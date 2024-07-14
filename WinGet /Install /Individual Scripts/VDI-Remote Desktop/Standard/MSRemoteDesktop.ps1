25b10de1de9aeb8246763573de7ab0f866baf05e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-14 00:29:09 -0400

    Update MSRemoteDesktop.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Standard/MSRemoteDesktop.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Standard/MSRemoteDesktop.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\VDI\$env:computername-MSRemoteDesktopInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Microsoft Remote Desktop Client"
+
+Write-Host
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
