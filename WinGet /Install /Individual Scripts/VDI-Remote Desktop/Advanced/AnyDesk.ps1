e9b86b5010ac58eaf9a26a1f2ec70ae02b3cbd2a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-14 00:27:40 -0400

    Update AnyDesk.ps1

--- WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AnyDesk.ps1
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/Advanced/AnyDesk.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedVDI-AnyDeskInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\VDI\$env:computername-AnyDeskInstall.log"
 
 $Date = Get-Date
 
