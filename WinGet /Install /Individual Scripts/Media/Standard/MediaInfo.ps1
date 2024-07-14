7d495eaf1044025eeaf6827d875a160e5b994708
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 22:12:07 -0400

    Update MediaInfo.ps1

--- WinGet /Install /Individual Scripts/Media/Standard/MediaInfo.ps1
+++ WinGet /Install /Individual Scripts/Media/Standard/MediaInfo.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-MediaInfoInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "MediaInfo"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
