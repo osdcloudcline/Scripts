829288474823fad7a8a37dc54eae43b9c92ac96f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-14 00:18:53 -0400

    Create Office365AppsEnterprise.ps1

---
+++ WinGet /Install /Individual Scripts/Productivity/Standard/Office365AppsEnterprise.ps1
@@ -0,0 +1,8 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Productivity\$env:computername-MSO365AppsEnterpriseInstall.log"
+
+$app1 = "Microsoft Office 365 for enterprise"
+
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+winget install --id Microsoft.Office --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
