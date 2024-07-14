34e50f336abd62cace801f1fae60268970de624a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-13 22:11:38 -0400

    Update Kodi.ps1

--- WinGet /Install /Individual Scripts/Media/Standard/Kodi.ps1
+++ WinGet /Install /Individual Scripts/Media/Standard/Kodi.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Media\$env:computername-KodiInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Kodi"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id XBMCFoundation.Kodi --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
