42a8c100e9aeaf59743611264fe775b7d5216fa9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-15 06:24:48 -0400

    Update GetRemoteShares.ps1

--- Data Info Retrival/Network Devices/GetRemoteShares.ps1
+++ Data Info Retrival/Network Devices/GetRemoteShares.ps1
@@ -1,9 +1,10 @@
 Write-Host '                                                                                ' -BackgroundColor White                                                              
 Write-Host ' This script retrives the remote network shares of devices on the local network ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '       You will need to know the computer name of the remote PC,                ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host '       the username and password to connect                                     ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host ' This script maps the remote share to drive letter Z:                           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '       as well as the username and password to connect                          ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                                ' -BackgroundColor White 
-Write-Host ' Manually typing the following removes the mapped drive share:                  ' -ForegroundColor DarkRed -BackgroundColor White
-Write-Host '              NET USE Z: /discard                                               ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '         This script maps the remote share to drive letter Z:                   ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '                                                                                ' -BackgroundColor White 
+Write-Host '      Manually typing the following removes the mapped drive share:             ' -ForegroundColor DarkRed -BackgroundColor White
+Write-Host '                  NET USE Z: /discard                                           ' -ForegroundColor DarkRed -BackgroundColor White
 Write-Host '                                                                                ' -BackgroundColor White 
