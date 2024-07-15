d5f52d96bdbc78be14355364c8fd5399c7f5555b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-15 00:29:04 -0400

    Update WinGetMain.ps1

--- WinGet /WinGetMain.ps1
+++ WinGet /WinGetMain.ps1
@@ -1,1 +1,54 @@
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'Software Installation Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
 
+Clear-Host
+Write-Host "======= $Title ======"
+Write-Host " 1. Install Software - Main Menu"
+Write-Host " 2. Upgrade Software - Main Menu"
+Write-Host " 3. Uninstall Software - Main Menu" 
+Write-Host " 4. Search for packages with WinGet Package Manager"
+Write-Host " 5. Apply Windows 11 Start Menu Configurations - Main Menu" 
+Write-Host " 6. Return to Main Menu"
+Write-Host " 7. Exit PowerShell"
+do
+{
+$selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+  '1' {cls
+       $Install = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/InstallMain.ps1")
+       Invoke-Expression $($Install.Content)
+       }
+ '2'{cls
+      $Update = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/UpdateMain.ps1")
+      Invoke-Expression $($Update.Content)
+     }
+ '3'{cls
+     $Uninstall = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Uninstall/UninstallMainMenu.ps1")
+     Invoke-Expression $($Uninstall.Content)
+    }
+'4'{cls
+    $WingetPackage = Read-Host -Prompt 'Please enter a keyword or company name, EG: Adobe or Microsoft, that you want to search for'
+    winget search $WingetPackage
+    }
+'5'{cls
+    $SMMain = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/Start%20Menu%20Configurations/SMConfigMain.ps1")
+    Invoke-Expression $($SMMain.Content)
+    }
+'6'{cls
+    $Main = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/OSDMain.ps1")
+    Invoke-Expression $($Main.Content)
+ }
+ '7'{exit}
+ }
+ }
+ until ($selection -eq '7'){exit}
+}
+Show-MainMenu 
