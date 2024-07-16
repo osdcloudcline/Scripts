a85c655586e5a8a76621bac1f04a9c8189d54af0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-16 00:16:42 -0400

    Update CopyToMoveTo.ps1

--- Registry Modifications/Client Operating System/Scripts/CopyToMoveTo.ps1
+++ Registry Modifications/Client Operating System/Scripts/CopyToMoveTo.ps1
@@ -1,5 +1,5 @@
 $CopyTo = "CLIENT OS SYSTEM: Copy To Registry Modifications"
-
+$MoveTo = "CLIENT OS SYSTEM: Move To Registry Modifications"
 
 #  Copy To folder
 
@@ -7,7 +7,7 @@ Write-Verbose "Importing $CopyTo to $env:computername..." -Verbose
 
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\OFFLINE\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\OFFLINE\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" -force -ea SilentlyContinue };
 
-
+Write-Verbose "Importing $MoveTo to $env:computername..." -Verbose
 
 #  Move To folder
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\OFFLINE\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\OFFLINE\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}" -force -ea SilentlyContinue };
