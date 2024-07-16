a8e99f29babe6d417d80609d1d5ea1b91a5248c2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-16 00:39:16 -0400

    Update CopyToMoveTo.ps1

--- Registry Modifications/Client Operating System/Scripts/CopyToMoveTo.ps1
+++ Registry Modifications/Client Operating System/Scripts/CopyToMoveTo.ps1
@@ -5,7 +5,8 @@ $MoveTo = "CLIENT OS SYSTEM: Move To Registry Modifications"
 
 Write-Verbose "Importing $CopyTo and $MoveTo to $env:computername..." -Verbose
 
-if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" -force -ea SilentlyContinue };
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\CopyTo") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\CopyTo" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\CopyTo' -Name '(default)' -Value '{C2FBB630-2971-11D1-A18C-00C04FD75D13}' -PropertyType String -Force -ea SilentlyContinue;
 
 #  Move To folder
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}" -force -ea SilentlyContinue };
