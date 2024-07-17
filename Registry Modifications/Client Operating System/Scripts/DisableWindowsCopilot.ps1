3b561dcde24c2a8a2a295bb825d1ccbc0b41a1d8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-17 08:00:06 -0400

    Update DisableWindowsCopilot.ps1

--- Registry Modifications/Client Operating System/Scripts/DisableWindowsCopilot.ps1
+++ Registry Modifications/Client Operating System/Scripts/DisableWindowsCopilot.ps1
@@ -1,5 +1,6 @@
+$DisableCoPilot = "Disable Windows Co-Pilot"
 
-
+Write-Verbose "Importing Client OS SYSTEM: $DisableCoPilot to $env:computername..." -Verbose
 
 if((Test-Path -LiteralPath "HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot") -ne $true) {  New-Item "HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot' -Name 'TurnOffWindowsCopilot' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
