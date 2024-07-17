5e11902d60c2c9124003b10ee7c4cc2d79a4dc2b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-17 07:45:38 -0400

    Update 7Zip.ps1

--- WinGet /Install /Individual Scripts/Utilities/Corporate/7Zip.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Corporate/7Zip.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Utilities\$env:computername-7ZipInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "7-Zip"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id 7zip.7zip --exact --accept-source-agreements --accept-source-agreements --force
+
+Stop-Transcript
