7544b660cc50ea9b550d14c67b4c95c276012c41
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-17 08:03:29 -0400

    Update DisableWindowsRecall.ps1

--- Registry Modifications/Client Operating System/Scripts/DisableWindowsRecall.ps1
+++ Registry Modifications/Client Operating System/Scripts/DisableWindowsRecall.ps1
@@ -2,6 +2,5 @@ $DisableRecall = "Disable Windows Recall"
 
 Write-Verbose "Importing Client OS SYSTEM: $DisableRecall to $env:computername..." -Verbose
 
-
 if((Test-Path -LiteralPath "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI") -ne $true) {  New-Item "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\Software\Policies\Microsoft\Windows\WindowsAI' -Name 'DisableAIDataAnalysis' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
