35f716fde62e46c91a089bbf6565399fbc61107d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-17 07:57:18 -0400

    Update DisableBitLockerDeviceEncryption.ps1

--- Registry Modifications/Client Operating System/Scripts/DisableBitLockerDeviceEncryption.ps1
+++ Registry Modifications/Client Operating System/Scripts/DisableBitLockerDeviceEncryption.ps1
@@ -1,5 +1,6 @@
 $DisableBitLocker = "Disable Bitlocker OS Encryption"
 
+Write-Verbose "Importing Client OS SYSTEM: $DisableBitLocker to $env:computername..." -Verbose
 
 if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\BitLocker") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\BitLocker" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\BitLocker' -Name 'PreventDeviceEncryption' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
