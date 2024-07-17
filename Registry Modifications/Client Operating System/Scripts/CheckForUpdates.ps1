352f15f33f325f405a17496befb045200bb353b1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-17 08:13:51 -0400

    Create CheckForUpdates.ps1

---
+++ Registry Modifications/Client Operating System/Scripts/CheckForUpdates.ps1
@@ -0,0 +1,6 @@
+
+
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates" -force -ea SilentlyContinue };
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates\command") -ne $true) {  New-Item "HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates\command" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates' -Name 'SettingsURI' -Value 'ms-settings:windowsupdate' -PropertyType String -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\DesktopBackground\Shell\Check for Updates\command' -Name 'DelegateExecute' -Value '{556FF0D6-A1EE-49E5-9FA4-90AE116AD744}' -PropertyType String -Force -ea SilentlyContinue;
