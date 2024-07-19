cb3778f55b1432fc4c859d9459d8f3bfc3d79cca
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 19:49:35 -0400

    Update DropBox.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/DropBox.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/DropBox.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedCloudBackup-DropBoxInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud\Backup\$env:computername-DropBoxInstall.log"
 
 $Date = Get-Date
 
