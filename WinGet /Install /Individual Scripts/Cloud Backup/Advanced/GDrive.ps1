3f4ff97d7ff473038d8d2978b8d4546960d6503b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 19:51:35 -0400

    Update GDrive.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GDrive.ps1
+++ WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GDrive.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedCloudBackup-GDriveInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud Backup\$env:computername-GDriveInstall.log"
 
 $Date = Get-Date
 
