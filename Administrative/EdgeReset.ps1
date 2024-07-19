e5a397c52bd5e954995f36d7b11469086cdd5ef0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 01:08:37 -0400

    Update EdgeReset.ps1

--- Administrative/EdgeReset.ps1
+++ Administrative/EdgeReset.ps1
@@ -8,38 +8,18 @@ Write-Verbose "Killing MS Edge Browser Running Tasks..." -Verbose
 
 Get-Process -Name "*Edge*" | Stop-Process
 
-Write-Verbose "Renaming Edge User Data Folder to reset MS Edge Browser..." -Verbose
+Write-Verbose "Resetting Microsoft Edge to OOBE mode.." -Verbose
 
 $EdgeSettingsPath = "$env:userprofile\AppData\Local\Microsoft\Edge"
 
 cd $EdgeSettingsPath
 
 $UserData1 = "$env:userprofile\AppData\Local\Microsoft\Edge\User Data"
-$UserData2 = "$env:userprofile\AppData\Local\Microsoft\Edge\User Data0"
 
-$UD1Test = (Test-Path -Path $UserData1 -IfValid)
-$UD2Test = (Test-Path -Path $UserData2 -IsValid)
-
-If($UD1Test -eq $true) -and ($UD2Test - $eq $true){
 
 Write-Verbose "Deleting Edge User Data directories..." -Verbose
 rd $UserData1
-rd $UserData2
-}
-ElseIf($UD1Test -eq $false) -and ($UD2Test - $eq $false){
-Write-Verbose "Creating Edge User Data directories..." -Verbose
 md $UserData1
-}
-
-Rename-Item -Path "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data" -NewName "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data0" -Force 
-
-$EdgeEXE = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-
-Start-Process -FilePath $EdgeEXE
-
-Write-Verbose "Deleting Edge User Data Folder..." -Verbose
-
-rd "C:\Users\$env:username\AppData\Local\Microsoft\Edge\User Data0" -Force
 
 Write-Verbose "Edge Browser Reset is complete. Any corruption is fixed" -Verbose
 
