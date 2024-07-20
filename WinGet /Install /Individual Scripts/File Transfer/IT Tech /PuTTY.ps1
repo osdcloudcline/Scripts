c7a3d7cbc889f2934b2df5c679e44c8ad2eab536
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 23:48:21 -0400

    Create PuTTY.ps1

---
+++ WinGet /Install /Individual Scripts/File Transfer/IT Tech /PuTTY.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\IT Tech\File Transfer\$env:computername-PuTTYInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "PuTTy"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id PuTTY.PuTTY --exact --accept-source-agreements --architecture x64  --accept-source-agreements --force
+
+Stop-Transcript
