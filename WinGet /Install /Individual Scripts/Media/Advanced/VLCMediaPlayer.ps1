a73209a0e85d045f143ba3ede1c6c8be0a00ed68
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 00:02:48 -0400

    Update VLCMediaPlayer.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/VLCMediaPlayer.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/VLCMediaPlayer.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-VLCPlayerInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Media\$env:computername-VLCPlayerInstall.log"
 
 $Date = Get-Date
 
