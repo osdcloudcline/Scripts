04c9129d9c7a589e4b3cac4b4239cb940f87ef41
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 23:53:11 -0400

    Update WinSCP.ps1

--- WinGet /Install /Individual Scripts/File Transfer/Management PC/WinSCP.ps1
+++ WinGet /Install /Individual Scripts/File Transfer/Management PC/WinSCP.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Management PC\File Transfer\$env:computername-WinSCPInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "WinSCP"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id WinSCP.WinSCP --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
