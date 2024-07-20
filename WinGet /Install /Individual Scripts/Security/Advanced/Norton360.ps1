89b4138c5520078477398d42c66658fb3ec25354
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 07:39:41 -0400

    Update Norton360.ps1

--- WinGet /Install /Individual Scripts/Security/Advanced/Norton360.ps1
+++ WinGet /Install /Individual Scripts/Security/Advanced/Norton360.ps1
@@ -1,1 +1,15 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Security\$env:computername-Norton360Download.log"
 
+$app1 = "MalwareBytes"
+
+Install-Module -Name OSD -AllowClobber -SkipPublisherCheck -Force
+Import-Module -Name OSD -Force
+
+$MBDLURL = "https://www.malwarebytes.com/mwb-download/thankyou"
+
+$destination = "C:\downloads"
+
+Write-Verbose "Processing $app1 for download..." -Verbose
+Save-WebFile -SourceUrl $MBDLURL -DestinationDirectory $destination
+
+Stop-Transcript
