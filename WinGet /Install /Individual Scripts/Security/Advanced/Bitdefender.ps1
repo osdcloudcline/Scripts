35799a447d6bf71b376cf6614c969976c2ebd636
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 07:25:34 -0400

    Update Bitdefender.ps1

--- WinGet /Install /Individual Scripts/Security/Advanced/Bitdefender.ps1
+++ WinGet /Install /Individual Scripts/Security/Advanced/Bitdefender.ps1
@@ -1,1 +1,9 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Security\$env:computername-BitDefenderDownload.log"
 
+$app1 = "Bitdefender Total Security"
+
+Install-Module -Name OSD -AllowClobber -SkipPublisherCheck -Force
+Import-Module -Name OSD -Force
+
+
+$BDURLDL = ""
