a19061a21cbcb9cfae16453c0bf85cbe27576d6a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 00:01:32 -0400

    Update MediaInfo.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/MediaInfo.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/MediaInfo.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-MediaInfoInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Media\$env:computername-MediaInfoInstall.log"
 
 $Date = Get-Date
 
