b6016662a9b63e4adad5694749f48d0861e8a3df
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 07:29:38 -0400

    Update IPVanish.ps1

--- WinGet /Install /Individual Scripts/Security/Advanced/IPVanish.ps1
+++ WinGet /Install /Individual Scripts/Security/Advanced/IPVanish.ps1
@@ -1,1 +1,17 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Security\$env:computername-IPVanishDownload.log"
+
+$app1 = "IP Vanish VPN"
+
+Install-Module -Name OSD -AllowClobber -SkipPublisherCheck -Force
+Import-Module -Name OSD -Force
+
+$VPNURL = "https://www.ipvanish.com/software/setup-prod-v2/ipvanish-setup.exe"
+
+$destination = "C:\downloads"
+
+Write-Verbose "Processing $app1 for download..." -Verbose
+Save-WebFile -SourceUrl $VPNURL -DestinationDirectory $destination
+
+Stop-Transcript
+
 
