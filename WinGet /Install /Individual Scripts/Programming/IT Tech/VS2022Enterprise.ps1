490ae56e8f5d9f08d8aa25ed5aa928686c0c2e71
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 00:57:51 -0400

    Create VS2022Enterprise.ps1

---
+++ WinGet /Install /Individual Scripts/Programming/IT Tech/VS2022Enterprise.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\IT Tech\Programming\$env:computername-VSEnterprise2022Install.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Microsoft Visual Studio Enterprise 2022"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
+Stop-Transcript
