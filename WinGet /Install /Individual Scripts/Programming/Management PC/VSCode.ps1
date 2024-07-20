5dac2189fce8b198d2345d175f83e830f808fa06
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 01:00:44 -0400

    Create VSCode.ps1

---
+++ WinGet /Install /Individual Scripts/Programming/Management PC/VSCode.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Management PC\Programming\$env:computername-VSCodeInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VS Code"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
