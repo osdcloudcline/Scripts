3ae9dab06eb68ac00cf1e6bd58c2af7cbcdde289
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 23:59:04 -0400

    Update HDVideoConverterPro.ps1

--- WinGet /Install /Individual Scripts/Media/Advanced/HDVideoConverterPro.ps1
+++ WinGet /Install /Individual Scripts/Media/Advanced/HDVideoConverterPro.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\$env:computername-AdvancedMedia-HDVideoConverterInstall.log"
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Media\$env:computername-HDVideoConverterInstall.log"
 
 $Date = Get-Date
 
