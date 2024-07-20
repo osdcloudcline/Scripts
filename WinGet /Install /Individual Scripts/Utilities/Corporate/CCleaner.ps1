cb3c08dac42c5e6d4f0de3b3ffc1cd3031ce06cb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 10:23:45 -0400

    Create CCleaner.ps1

---
+++ WinGet /Install /Individual Scripts/Utilities/Corporate/CCleaner.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Utilities\$env:computername-CCleanerInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "CCleaner"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Piriform.CCleaner --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
