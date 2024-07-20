38c24ff439b389bcce268858d77f6fb096c762ae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-19 22:10:36 -0400

    Create NextCloudClient.ps1

---
+++ WinGet /Install /Individual Scripts/Cloud Backup/IT Tech/NextCloudClient.ps1
@@ -0,0 +1,12 @@
+Start-Transcript -Path "C:\Logs\Powershell\IT Tech\Cloud Backup\$env:computername-NextCloudClientInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Nextcloud Desktop Client"
+
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
