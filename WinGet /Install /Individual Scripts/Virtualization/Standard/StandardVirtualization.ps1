60c230a011a3f2e03a76d29464bed8eb161aedc8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 23:35:44 -0400

    Update StandardVirtualization.ps1

--- WinGet /Install /Individual Scripts/Virtualization/Standard/StandardVirtualization.ps1
+++ WinGet /Install /Individual Scripts/Virtualization/Standard/StandardVirtualization.ps1
@@ -1,4 +1,12 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\\Virtualization\$env:computername-VMWareWSProInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
 $app1 = "VMWare Workstation Professional"
 
-Write-Host "Installing: $app1..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Write-Host "Installing: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
 choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
+
+Stop-Transcript
