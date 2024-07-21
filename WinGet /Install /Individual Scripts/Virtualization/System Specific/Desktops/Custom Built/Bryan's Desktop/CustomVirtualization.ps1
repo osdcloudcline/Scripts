3a9cfba4bec384ce26c53c9df25e66e4581253b0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 23:38:11 -0400

    Delete WinGet /Install /Individual Scripts/Virtualization/System Specific directory

--- WinGet /Install /Individual Scripts/Virtualization/System Specific/Desktops/Custom Built/Bryan's Desktop/CustomVirtualization.ps1
+++
@@ -1,4 +0,0 @@
-$app1 = "VMWare Workstation Professional"
-
-Write-Host "Installing: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
