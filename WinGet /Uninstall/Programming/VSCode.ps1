c6e39fad8d8b60a124e3bd7dce9f3a4682f78cb4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-21 09:16:24 -0400

    Update VSCode.ps1

--- WinGet /Uninstall/Programming/VSCode.ps1
+++ WinGet /Uninstall/Programming/VSCode.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\UninstallProgramming\$env:computername-VSCode.log"
 
+$Date = Get-Date
+
+Write-Host "Uninstalled on: $Date"
+
+$app1 = "VS Code"
+
+Write-Host
+Write-Host "Uninstalling $app1..." -ForegroundColor Cyan
+winget uninstall --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
