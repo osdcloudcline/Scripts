419000709262a828f39da6e13c0b3cc6b8a7f3b7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-20 23:27:47 -0400

    Create AnyDesk.ps1

---
+++ WinGet /Install /Individual Scripts/VDI-Remote Desktop/IT Tech/AnyDesk.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\IT Tech\VDI\$env:computername-AnyDeskInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "AnyDesk"
+
+Write-Host
+Write-Host "Installing: $app1..."  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
