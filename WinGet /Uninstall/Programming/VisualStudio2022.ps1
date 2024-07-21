27e262621319c8a9095fcec949f7241a2f7575db
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-21 09:21:23 -0400

    Update VisualStudio2022.ps1

--- WinGet /Uninstall/Programming/VisualStudio2022.ps1
+++ WinGet /Uninstall/Programming/VisualStudio2022.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Uninstall\Programming\$env:computername-VSEnterprise2022.log"
 
+$Date = Get-Date
+
+Write-Host "Uninstalled on: $Date"
+
+$app1 = "Microsoft Visual Studio Enterprise 2022"
+
+Write-Host
+Write-Host "Uninstalling $app1..." -ForegroundColor Cyan
+choco uninstall visualstudio2022enterprise -y
+
+Stop-Transcript
