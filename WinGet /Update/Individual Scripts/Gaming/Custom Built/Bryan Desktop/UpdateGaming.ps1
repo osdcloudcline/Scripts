bdae7664cc95d3a03477e12529b1daf702d3f222
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-23 09:46:08 -0400

    Delete WinGet /Update/Individual Scripts/Gaming/Custom Built directory

--- WinGet /Update/Individual Scripts/Gaming/Custom Built/Bryan Desktop/UpdateGaming.ps1
+++
@@ -1,4 +0,0 @@
-$app1 = "Steam"
-
-Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget upgrade --id Valve.Steam
