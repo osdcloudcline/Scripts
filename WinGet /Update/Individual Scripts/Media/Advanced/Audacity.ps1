e3671eeef3f9e137750432d4f9baded9f24bf5ac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-23 09:50:10 -0400

    Update and rename UpdateAudacityFFMPEG.ps1 to Audacity.ps1

--- WinGet /Update/Individual Scripts/Media/Advanced/Audacity.ps1
+++ WinGet /Update/Individual Scripts/Media/Advanced/Audacity.ps1
@@ -1,5 +1,5 @@
-$app1 = "Audacity and FFMPEG"
+$app1 = "Audacity"
 
 Write-Host "Checking for and processing update: $app1..." -ForegroundColor Cyan
 winget upgrade --id Audacity.Audacity
-choco upgrade audacity-ffmpeg -y
+
