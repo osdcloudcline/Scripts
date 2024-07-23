92f361c39d76efe4ed384966c5771a9c0203cb35
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-23 09:43:40 -0400

    Delete WinGet /Update/Individual Scripts/System Specific directory

--- WinGet /Update/Individual Scripts/System Specific/Custom Built/Bryan Desktop/Browsers/Chrome.ps1
+++
@@ -1,4 +0,0 @@
-$app1 = "Google Chrome for Enterprise"
-
-Write-Host "Checking for and processing update: $app1 on $env:computername..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-winget upgrade --id Google.Chrome
