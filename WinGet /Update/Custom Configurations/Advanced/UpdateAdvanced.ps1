0d8ff3bdf440234cfc4a6a0c3d3922d8ba77296a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-23 09:03:02 -0400

    Update UpdateAdvanced.ps1

--- WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
+++ WinGet /Update/Custom Configurations/Advanced/UpdateAdvanced.ps1
@@ -34,7 +34,7 @@ Write-Host "Processing updates for: Web Browsers..." -ForegroundColor Cyan
 $Chrome = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Advanced/Chrome.ps1")
 Invoke-Expression $($Chrome.Content)
 
-$Edge = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Advanced/UpdateEdge.ps1")
+$Edge = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Advanced/Edge.ps1")
 Invoke-Expression $($Edge.Content)
 
 $Firefox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Advanced/UpdateFirefox.ps1")
