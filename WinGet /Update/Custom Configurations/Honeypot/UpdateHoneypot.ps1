011023f72bd863ecc9c8ab8d1211e0f3c18fc060
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-24 06:55:57 -0400

    Update UpdateHoneypot.ps1

--- WinGet /Update/Custom Configurations/Honeypot/UpdateHoneypot.ps1
+++ WinGet /Update/Custom Configurations/Honeypot/UpdateHoneypot.ps1
@@ -2,8 +2,10 @@ Write-Host '                                                                 ' -
 Write-Host '              HONEYPOT SOFTWARE CONFIGURATION                    ' -ForegroundColor DarkBlue -BackgroundColor White 
 Write-Host '          The following software will be updated:                ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '  - Cloud Backups: Microsoft OneDrive                            ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Web Browsers: Google Chrome                                  ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Media: VLC Player and MediaInfo                              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - OS SYSTEM: OS System Software                                ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Security:  IPVanish VPN                                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Security:  MalwareBytes                                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                 ' -BackgroundColor White 
@@ -11,17 +13,34 @@ pause
 
 Write-Host "Processing updates for: Web Browsers..." -ForegroundColor DarkBlue -BackgroundColor White
 
-$Browsers = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Honeypot/HoneypotBrowsers.ps1")
-Invoke-Expression $($Browsers.Content)
+$Chrome = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Honeypot/Chrome.ps1")
+Invoke-Expression $($Chrome.Content)
 
+$Firefox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Honeypot/Firefox.ps1")
+Invoke-Expression $($Firefox.Content)
+
+$Edge = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Browsers/Honeypot/Edge.ps1")
+Invoke-Expression $($Edge.Content)
+
+Write-Host "Processing updates for: Cloud Backups..." -ForegroundColor DarkBlue -BackgroundColor White
+
+$OneDrive = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Cloud%20Backups/Honeypot/OneDrive.ps1")
+Invoke-Expression $($OneDrive.Content)
 
 Write-Host "Processing updates for: Media software..." -ForegroundColor DarkBlue -BackgroundColor White
 
-$Media = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Honetypot/HoneypotMedia.ps1")
-Invoke-Expression $($Media.Content)
+$MediaInfo = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Media/Honeypot/MediaInfo.ps1")
+Invoke-Expression $($MediaInfo.Content)
 
+$VLCPlayer = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/Media/Honeypot/VLCPlayer.ps1")
+Invoke-Expression $($VLCPlayer.Content)
 
-Write-Host "Processing downloads for: Security software..." -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "Processing updates for: OS SYSTEM Software..." -ForegroundColor DarkBlue -BackgroundColor White
+
+$OS = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Update/Individual%20Scripts/OS%20SYSTEM/Clients/ClientSystemUpdates.ps1")
+Invoke-Expression $($OS.Content)
+
+Write-Host "Processing updates for: Security software..." -ForegroundColor DarkBlue -BackgroundColor White
 
 $FileDLs = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Security/Honeypot/VMFileDLs.ps1")
 Invoke-Expression $($FileDLs.Content)
