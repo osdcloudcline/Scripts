e73420183b75980521765f63228bb3753f1a002a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-25 00:37:48 -0400

    Update ConfigureClients.ps1

--- AuditMode/ConfigureClients.ps1
+++ AuditMode/ConfigureClients.ps1
@@ -30,6 +30,9 @@ Write-Verbose "Step 6 - Mandatory OS Pre-Requirements..." -Verbose
 $OS = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM/OS-Mandated%20Software/SYSTEM.ps1")
 Invoke-Expression $($OS.Content)
 
+$EdgeExt = Invoke-WebRequest("")
+Invoke-Expression $($EdgeExt.Content)
+
 Write-Verbose "Step 7 - Software Installation..." -Verbose
 
 $WinGetSoftware = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1")
