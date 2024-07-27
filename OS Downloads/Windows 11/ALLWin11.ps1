ccd445019aba39f08127a564b48922d4315c81bd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-27 00:11:20 -0400

    Update ALLWin11.ps1

--- OS Downloads/Windows 11/ALLWin11.ps1
+++ OS Downloads/Windows 11/ALLWin11.ps1
@@ -186,7 +186,7 @@ Write-Host "Processing: Windows 11 24H2 Build 26100.863...." -ForegroundColor Cy
 
 $ZIPDestination = "$env:userprofile\Downloads\OS\Windows11\24H2"
 
-$Windows1124H2URL = "https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/Windows%2011/24H2/26100.863_amd64_en-us_multi_ad1caad2_convert_virtual.zip"
+$Windows1124H2URL = "https://github.com/osdcloudcline/Scripts/raw/main/OS%20Downloads/Windows%2011/24H2/26100.1000_amd64_en-us_multi_0cda15f9_convert_virtual.zip"
 
 $ExtractionLocation = $ZIPDestination
 
@@ -200,7 +200,7 @@ Save-WebFile -SourceUrl $Windows1124H2URL -DestinationDirectory $ZIPDestination
 
 Write-Host "Expanding Windows 11 24H2 ZIP File..." -ForegroundColor Cyan 
 
-Expand-Archive -Path "$ZIPDestination\26100.863_amd64_en-us_multi_ad1caad2_convert_virtual.zip" -DestinationPath $ExtractionLocation
+Expand-Archive -Path "$ZIPDestination\26100.1000_amd64_en-us_multi_0cda15f9_convert_virtual.zip" -DestinationPath $ExtractionLocation
 Write-Host
 Write-Host "Downloading Windows 11 24H2 Build: 26100.863..." -ForegroundColor Cyan 
 
