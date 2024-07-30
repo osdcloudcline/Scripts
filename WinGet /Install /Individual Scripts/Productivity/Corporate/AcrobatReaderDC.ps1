5569877de46498796402b5431367409abcbf3f8a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-30 19:45:12 -0400

    Update AcrobatReaderDC.ps1

--- WinGet /Install /Individual Scripts/Productivity/Corporate/AcrobatReaderDC.ps1
+++ WinGet /Install /Individual Scripts/Productivity/Corporate/AcrobatReaderDC.ps1
@@ -1,6 +1,6 @@
 Start-Transcript -Path "C:\Logs\Powershell\Corporate\Productivity\$env:computername-AcrobatReaderDCInstall.log"
 
-$app1 = "Adobe Acrobat Reader"
+$app1 = "Adobe Acrobat Reader DC"
 
 Write-Host "Processing: $app1..." -ForegroundColor Cyan
 winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
