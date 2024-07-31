a315391adbef5be40290ca62f9ca4dc768d784cb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-31 00:19:47 -0400

    Update LogitechOptions.ps1

--- WinGet /Install /Individual Scripts/Utilities/Corporate/LogitechOptions.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Corporate/LogitechOptions.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Utilities\$env:computername-LogitechOpitionsInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Logitech Options"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Logitech.Options --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
