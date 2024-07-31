c944b705d2233db4369cd1cee1aa693df51d86d4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-31 00:21:27 -0400

    Update SamsungMagician.ps1

--- WinGet /Install /Individual Scripts/Utilities/Corporate/SamsungMagician.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Corporate/SamsungMagician.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Utilities\$env:computername-SamsungMagicianInstall.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Samsung Magician"
+
+Write-Host
+Write-Verbose "Installing $app14..." -Verbose
+winget install --id Samsung.SamsungMagician --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
