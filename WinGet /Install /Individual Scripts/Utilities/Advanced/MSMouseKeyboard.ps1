d7fe20c1761e4fc98d2cd52d1acc16a8bc16c748
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-31 01:00:26 -0400

    Create MSMouseKeyboard.ps1

---
+++ WinGet /Install /Individual Scripts/Utilities/Advanced/MSMouseKeyboard.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Advanced\Utilities\$env:computername-MSMouseKBCenterInstall.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Microsoft Mouse and Keyboard Center"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Microsoft.MouseandKeyboardCenter --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
