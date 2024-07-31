c900f715fea6d0e36778f001b468f15e5ba0e336
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-07-31 00:32:58 -0400

    Update MSMouseKeyboard.ps1

--- WinGet /Install /Individual Scripts/Utilities/Corporate/MSMouseKeyboard.ps1
+++ WinGet /Install /Individual Scripts/Utilities/Corporate/MSMouseKeyboard.ps1
@@ -1,1 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Utilities\$env:computername-Microsoft Mouse and Keyboard Center.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Microsoft Mouse and Keyboard Center"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Microsoft.MouseandKeyboardCenter --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
