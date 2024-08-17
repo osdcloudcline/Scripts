c7adf1b51ce5f108df6dd10d20e0f91f07255121
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 13:52:11 -0400

    Update Advanced.ps1

--- WinGet /Install /GUI/Advanced.ps1
+++ WinGet /Install /GUI/Advanced.ps1
@@ -19,7 +19,7 @@ Invoke-Expression $($OneDrive.Content)
 $UPBR = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Advanced/UPBR.ps1")
 Invoke-Expression $($UPBR.Content)
 
-$iCloud = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Advanced/iCloud.ps1")
+$iCloud = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Advanced/GUIiCloud.ps1")
 Invoke-Expression $($iCloud.Content)
 
 $FileZilla = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/File%20Transfer/Advanced/FileZilla.ps1")
