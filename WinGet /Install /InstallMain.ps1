83fada8b37c642e54561ec053239b1da8b1bad7b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 02:08:21 -0400

    Update InstallMain.ps1

--- WinGet /Install /InstallMain.ps1
+++ WinGet /Install /InstallMain.ps1
@@ -10,19 +10,27 @@ Function Show-MainMenu{
 
 Clear-Host
 Write-Host "======= $Title ======"
-Write-Host " 1. Install Standard Software Configuration"
-Write-Host " 2. Install Advanced Software Configuration"
-Write-Host " 3. Install Corporate Software Configuration"
-Write-Host " 4. Install Client VDI Software Configuration"
-Write-Host " 5. Install Server VDI Software Configuration"
-Write-Host " 6. Install Management PC Software Configuration"
-Write-Host " 7. Install Honeypot Configuration"
-Write-Host " 8. Install IT Tech Configuration"
-Write-Host " 9. Individual Install Scripts"
-Write-Host " 10. Manufacturer Specific Scripts"
-Write-Host " 11. Apply Start2.bin Start Menu Configurations"
-Write-Host " 12. Return to Winget Software Main Menu"
-Write-Host " 13. Exit PowerShell"
+Write-Host " 1. CLI Install Standard Software Configuration"
+Write-Host " 2. CLI Install Advanced Software Configuration"
+Write-Host " 3. CLI Install Corporate Software Configuration"
+Write-Host " 4. CLI Install Client VDI Software Configuration"
+Write-Host " 5. CLI Install Server VDI Software Configuration"
+Write-Host " 6. CLI Install Management PC Software Configuration"
+Write-Host " 7. CLI Install Honeypot Configuration"
+Write-Host " 8. CLI Install IT Tech Configuration"
+Write-Host " 9. GUI Install Standard Software Configuration"
+Write-Host " 10. GUI Install Advanced Software Configuration"
+Write-Host " 11. GUI Install Corporate Software Configuration"
+Write-Host " 12. GUI Install Client VDI Software Configuration"
+Write-Host " 13. GUI Install Server VDI Software Configuration"
+Write-Host " 14. GUI Install Management PC Software Configuration"
+Write-Host " 15. GUI Install Honeypot Configuration"
+Write-Host " 16. GUI Install IT Tech Configuration"
+Write-Host " 17. Individual Install Scripts"
+Write-Host " 18. Manufacturer Specific Scripts"
+Write-Host " 19. Apply Start2.bin Start Menu Configurations"
+Write-Host " 20. Return to Winget Software Main Menu"
+Write-Host " 21. Exit PowerShell"
 
 do
 { 
@@ -61,25 +69,57 @@ $selection = Read-Host 'Please choose an option'
        $ITTech = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/IT%20Tech/ITTech.ps1")
        Invoke-Expression $($ITTech.Content)
     }
- '9'{cls
-       $Individual = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/IndividualScriptsMain.ps1")
-       Invoke-Expression $($Individual.Content)
+ '9' {cls
+      $GUIStandard = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Standard/Standard.ps1")
+      Invoke-Expression $($GUIStandard.Content)
+       }
+ '10'{cls
+      $GUIAdvanced = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Advanced/Advanced.ps1")
+      Invoke-Expression $($GUIAdvanced.Content)
+     }
+ '11'{cls
+     $GUICorporate = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Corporate/Corporate.ps1")
+     Invoke-Expression $($GUICorporate.Content)
     }
-'10'{cls
-       $Manufacturer = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Manufacturer%20Specific/ManufacturerMain.ps1")
-       Invoke-Expression $($Manufacturer.Content)
+ '12'{cls
+    $GUIClientVDI = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/VDI/ClientVirtualDesktopInterface.ps1")
+    Invoke-Expression $($GUIClientVDI.Content)
+    }
+ '13'{cls
+    $GUIServerVDI = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/VDI/ServerVirtualDesktopInterface.ps1")
+    Invoke-Expression $($GUIServerVDI.Content)
     } 
-'11'{cls
+ '14'{cls
+    $GUIManagementPC = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Management%20PC/ManagementPC.ps1")
+    Invoke-Expression $($GUIManagementPC.Content)
+     }
+ '15'{cls
+    $GUIHoneypot = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Honeypot/Honeypot.ps1")
+    Invoke-Expression $($GUIHoneypot.Content)
+    }
+ '16'{cls
+    $GUIITTech = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/IT%20Tech/ITTech.ps1")
+    Invoke-Expression $($GUIITTech.Content)
+    }
+ '17'{cls
+    $Individual = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/IndividualScriptsMain.ps1")
+    Invoke-Expression $($Individual.Content)
+    }
+'18'{cls
+    $Manufacturer = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Manufacturer%20Specific/ManufacturerMain.ps1")
+    Invoke-Expression $($Manufacturer.Content)
+    } 
+'19'{cls
      $SMMain = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/Start%20Menu%20Configurations/SMConfigMain.ps1")
      Invoke-Expression $($SMMain.Content)
      }
- '12'{cls
+ '20'{cls
      $WinGetMain = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/WinGetMain.ps1")
      Invoke-Expression $($WinGetMain.Content)
     }
-'13'{exit}
+'21'{exit}
  }
  }
- until ($selection -eq '13'){exit}
+ until ($selection -eq '21'){exit}
 }
 Show-MainMenu 
