4916b21509cbd17bd961cbafd6de5ca110436f25
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 01:32:36 -0400

    Update ADVSplashScreen.ps1

--- OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
+++ OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
@@ -1,3 +1,38 @@
 $ADVprocesses = @(
   @{
-    Name = "Microsoft Windows "
+    Name = "Microsoft Windows PowerShell Providers"
+    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Package%20Providers/Install.ps1"
+    },
+  @{
+    Name = "Microsoft Windows PowerShell 5.1 Modules"
+    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1"
+    },
+  @{
+   Name = "Microsoft Windows PowerShell 7.1 Modules"
+   Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1"
+   },
+  @{
+  Name = "Client Operating System Registry Entries"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/ClientRegistry.ps1"
+  },
+  @{
+  Name = "Windows Client - Remote System Administration Tools"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/Client%20OS%20Roles%20/InstallClientRSAT.ps1"
+  },
+  @{
+  Name = "Mandatory Operating System related SYSTEM Software"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM/OS-Mandated%20Software/SYSTEM.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Custom%20Configurations/Advanced/Advanced.ps1"
+  },
+  @{
+  Name = " Updating Windows"
+  Script = ""
+  }
+  )
+
+  Start-SplashScreen -Processes $ADVprocesses -MessageHeading "Windows OOBE System Configuration"
+ 
+  
