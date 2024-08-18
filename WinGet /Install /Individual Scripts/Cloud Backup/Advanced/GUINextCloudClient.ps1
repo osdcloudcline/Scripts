f9f84bea73930c9860df5ab362d8af5098278489
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:04:20 -0400

    Delete WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUINextCloudClient.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUINextCloudClient.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud Backup\GUI\$env:computername-NextCloudClient.log"
-
-winget install --id Nextcloud.NextcloudDesktop --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
