820e44cb14d83aefdf5272c1af8745aaba8da8ea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 22:10:45 -0400

    Update ADVSplashScreen.ps1

--- OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
+++ OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
@@ -24,15 +24,52 @@ $ADVprocesses = @(
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM/OS-Mandated%20Software/SYSTEM.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package"
-  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced.ps1"
+  Name = "Advanced Client Software Installation Package - Web Browsers"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Browsers.ps1"
   },
   @{
-  Name = " Updating Windows"
+  Name = "Advanced Client Software Installation Package - Cloud Backup"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/CloudBackups.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - File Transfer"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/FileTransfer.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Gaming"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Gaming.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Media"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Media.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Productivity"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Productivity.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Programming"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Programming.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Remote Connections"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/RemoteConnections.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Utilities"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Utilities.ps1"
+  },
+  @{
+  Name = "Advanced Client Software Installation Package - Security"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Security.ps1"
+  },
+  
+  @{
+  Name = "Updating Windows"
   Script = ""
   }
   )
 
-  Start-SplashScreen -Processes $ADVprocesses -MessageHeading "Windows OOBE System Advanced Configuration"
+  Start-SplashScreen -Processes $ADVprocesses -MessageHeader "Windows OOBE System Advanced Configuration"
  
   
