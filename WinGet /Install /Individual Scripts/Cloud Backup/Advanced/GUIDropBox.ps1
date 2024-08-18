3c31bc1271be5d551deecccc3e5f44e7315569d6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:02:45 -0400

    Delete WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIDropBox.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIDropBox.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud Backup\GUI\$env:computername-DropBoxInstall.log"
-
-winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
