396b9c740230d1b8553fdea61fe6090cf937713c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:05:11 -0400

    Delete WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIOneDrive.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIOneDrive.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud Backup\GUI\$env:computername-OneDrive.log"
-
-winget install --id Microsoft.OneDrive --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
