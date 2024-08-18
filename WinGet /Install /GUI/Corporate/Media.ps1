cbc556d9863b10355be84a72d3dc1e941ace7f17
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 11:35:28 -0400

    Update Media.ps1

--- WinGet /Install /GUI/Corporate/Media.ps1
+++ WinGet /Install /GUI/Corporate/Media.ps1
@@ -1,4 +1,4 @@
-$MediaInfo = Invoke-WeBRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Corporate/MediaInfo.ps1")
+$MediaInfo = Invoke-WeBRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Corporate/GUI%20Scripts/MediaInfo.ps1")
 Invoke-Expression $($MediaInfo.Content)
 
 $VLCPlayer = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Corporate/VLCPlayer.ps1")
