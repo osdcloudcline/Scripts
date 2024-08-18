95368cc7ce4fa7e3f2802e33aa60a3c263a57ae3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 01:23:41 -0400

    Delete WinGet /Install /Individual Scripts/Browsers/IT Tech/GUIChrome.ps1

--- WinGet /Install /Individual Scripts/Browsers/IT Tech/GUIChrome.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\IT\GUI\$env:computername-Chrome.log"
-
-winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
