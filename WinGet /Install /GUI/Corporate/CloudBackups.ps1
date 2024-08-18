d7227dfc1cb8919e18358cd24f123cc2af385578
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:44:14 -0400

    Update CloudBackups.ps1

--- WinGet /Install /GUI/Corporate/CloudBackups.ps1
+++ WinGet /Install /GUI/Corporate/CloudBackups.ps1
@@ -1,1 +1,10 @@
+$DropBox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Corporate/GUI%20Scripts/DropBox.ps1")
+Invoke-Expression $($DropBox.Content)
+
+$GDrive = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Corporate/GUI%20Scripts/GDrive.ps1")
+Invoke-Expression $($GDrive.Content)
+
+$OneDrive = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Cloud%20Backup/Corporate/GUI%20Scripts/OneDrive.ps1")
+Invoke-Expression $($OneDrive.Content)
+
 
