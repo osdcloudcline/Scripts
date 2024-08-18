74afea31f50b1c24b329c963ebf44fd0493a8e3b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 01:17:37 -0400

    Update Browsers.ps1

--- WinGet /Install /GUI/Advanced/Browsers.ps1
+++ WinGet /Install /GUI/Advanced/Browsers.ps1
@@ -1,5 +1,5 @@
 $Chrome = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Advanced/GUI%20Scripts/Chrome.ps1")
 Invoke-Expression $($Chrome.Content)
 
-$Firefox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Advanced/GUIFirefox.ps1")
+$Firefox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Advanced/GUI%20Scripts/Firefox.ps1")
 Invoke-Expression $($Firefox.Content)
