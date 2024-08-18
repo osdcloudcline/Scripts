488f526f89526d48068d109f480ce851d79f6450
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 12:51:51 -0400

    Update Productivity.ps1

--- WinGet /Install /GUI/Advanced/Productivity.ps1
+++ WinGet /Install /GUI/Advanced/Productivity.ps1
@@ -7,8 +7,11 @@ Invoke-Expression $($MSO365.Content)
 $MSO365E = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/GUI%20Scripts/Office365Enterprise.ps1")
 Invoke-Expression $($MSO365E.Content)
 
-$Office2021GH = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office2021.ps1")
+$Office2021GH = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/GUI%20Scripts/Office2021GHDownload.ps1")
 Invoke-Expression $($Office2021GH.Content)
+
+$Office2021DL = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/GUI%20Scripts/Office2021GHDownload.ps1")
+Invoke-Expression $($Office2021DL.Content)
 
 $Office2024 = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office2024.ps1")
 Invoke-Expression $($Office2024.Content)
