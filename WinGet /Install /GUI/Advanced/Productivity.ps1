c180d1f5820a93b22a8b908980032455e06cdf98
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 12:48:38 -0400

    Update Productivity.ps1

--- WinGet /Install /GUI/Advanced/Productivity.ps1
+++ WinGet /Install /GUI/Advanced/Productivity.ps1
@@ -1,7 +1,7 @@
 $AcrobatReaderDC = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/GUI%20Scripts/AdobeReaderDC.ps1")
 Invoke-Expression $($AcrobatReaderDC.Content)
 
-$MSO365 = = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/MSO365.ps1")
+$MSO365 = = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/GUI%20Scripts/MSO365.ps1")
 Invoke-Expression $($MSO365.Content)
 
 $MSO365E = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office365AppsEnterprise.ps1")
