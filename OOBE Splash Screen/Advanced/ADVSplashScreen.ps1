720b265a06eb8685f7d8ca4904a8ab690f37c9c8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 22:27:20 -0400

    Update ADVSplashScreen.ps1

--- OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
+++ OOBE Splash Screen/Advanced/ADVSplashScreen.ps1
@@ -1,75 +1,74 @@
 $ADVprocesses = @(
   @{
-    Name = "Microsoft Windows PowerShell Providers"
+    Name = "Processing: Microsoft Windows PowerShell Providers"
     Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Package%20Providers/Install.ps1"
     },
   @{
-    Name = "Microsoft Windows PowerShell 5.1 Modules"
+    Name = "Processing: Microsoft Windows PowerShell 5.1 Modules"
     Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1"
     },
   @{
-   Name = "Microsoft Windows PowerShell 7.1 Modules"
+   Name = "Processing: Microsoft Windows PowerShell 7.1 Modules"
    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1"
    },
   @{
-  Name = "Client Operating System Registry Entries"
+  Name = "Processing: Client Operating System Registry Entries"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/ClientRegistry.ps1"
   },
   @{
-  Name = "Windows Client - Remote System Administration Tools"
+  Name = "Processing: Windows Client - Remote System Administration Tools"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/PostInstall/Client%20OS%20Roles%20/InstallClientRSAT.ps1"
   },
   @{
-  Name = "Mandatory Operating System related SYSTEM Software"
+  Name = "Processing: Mandatory Operating System-related SYSTEM Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/SYSTEM/OS-Mandated%20Software/SYSTEM.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Web Browsers"
+  Name = "Processing: Web Browser Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Browsers.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Cloud Backup"
+  Name = "Processing: Cloud Backup Software" 
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/CloudBackups.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - File Transfer"
+  Name = "Processing: File Transfer Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/FileTransfer.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Gaming"
+  Name = "Processing: Gaming Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Gaming.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Media"
+  Name = "Processing: Media Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Media.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Productivity"
+  Name = "Processing: Productivity Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Productivity.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Programming"
+  Name = "Processing: Programming Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Programming.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Remote Connections"
+  Name = "Processing: Remote Connection Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/RemoteConnections.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Utilities"
+  Name = "Processing: Utilities Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Utilities.ps1"
   },
   @{
-  Name = "Advanced Client Software Installation Package - Security"
+  Name = "Processing: Security Software"
   Script = "https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/GUI/Advanced/Security.ps1"
   },
-  
   @{
-  Name = "Updating Windows"
+  Name = "Processing: Updating Windows"
   Script = ""
   }
   )
 
-  Start-SplashScreen -Processes $ADVprocesses -MessageHeader "Windows OOBE System Advanced Configuration"
+  Start-SplashScreen -Processes $ADVprocesses -MessageHeader "Windows OOBE System Advanced Client Configuration"
  
   
