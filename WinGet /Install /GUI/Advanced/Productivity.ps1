4ff2b6800051dad7cea27e8516175a0279a74ed5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-17 21:58:55 -0400

    Create Productivity.ps1

---
+++ WinGet /Install /GUI/Advanced/Productivity.ps1
@@ -0,0 +1,17 @@
+$AcrobatReaderDC = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/AcrobatReaderDC,ps1")
+Invoke-Expression $($AcrobatReaderDC.Content)
+
+$MSO365 = = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/MSO365.ps1")
+Invoke-Expression $($MSO365.Content)
+
+$MSO365E = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office365AppsEnterprise.ps1")
+Invoke-Expression $($MSO365E.Content)
+
+$Office2021 = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office2021.ps1")
+Invoke-Expression $($Office2021.Content)
+
+$Office2024 = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Productivity/Advanced/Office2024.ps1")
+Invoke-Expression $($Office2024.Content)
+
+$VSCode = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Programming/Advanced/VSCode.ps1")
+Invoke-Expression $($VSCode.Content)
