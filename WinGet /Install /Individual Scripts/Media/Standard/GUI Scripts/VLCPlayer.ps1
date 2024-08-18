3e6ae9afa5f489b7f5bd89a18796cd7630c01bf0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 11:29:59 -0400

    Create VLCPlayer.ps1

---
+++ WinGet /Install /Individual Scripts/Media/Standard/GUI Scripts/VLCPlayer.ps1
@@ -0,0 +1,5 @@
+Start-Transcript -Path "C:\Logs\Powershell\Standard\Media\GUI\$env:computername-VLCPlayerInstall.log"
+
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
