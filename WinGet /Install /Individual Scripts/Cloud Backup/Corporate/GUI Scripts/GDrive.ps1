d95228846bf6e5da708601194f3f088736f9c49c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:39:53 -0400

    Create GDrive.ps1

---
+++ WinGet /Install /Individual Scripts/Cloud Backup/Corporate/GUI Scripts/GDrive.ps1
@@ -0,0 +1,5 @@
+Start-Transcript -Path "C:\Logs\Powershell\Corporate\Cloud Backup\GUI\$env:computername-GDriveInstall.log"
+
+winget install --id Google.GoogleDrive --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
