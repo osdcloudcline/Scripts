95596028a478115da295f2ab5f7bf81f8005fa3a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 01:18:04 -0400

    Delete WinGet /Install /Individual Scripts/Browsers/Advanced/GUIFirefox.ps1

--- WinGet /Install /Individual Scripts/Browsers/Advanced/GUIFirefox.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\GUI\$env:computername-Firefox.log"
-
-winget install --id Mozilla.Firefox --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
