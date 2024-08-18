43b167cee369b561ca8ebeb78657446f274cbd34
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-18 02:05:58 -0400

    Delete WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIiCloud.ps1

--- WinGet /Install /Individual Scripts/Cloud Backup/Advanced/GUIiCloud.ps1
+++
@@ -1,5 +0,0 @@
-Start-Transcript -Path "C:\Logs\Powershell\Advanced\Cloud Backup\GUI\$env:computername-iCloud.log"
-
-winget install --id Apple.iCloud --exact --accept-source-agreements  --accept-source-agreements --force
-
-Stop-Transcript
