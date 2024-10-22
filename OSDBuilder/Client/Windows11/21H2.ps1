fcde5eb7833b645e4a2c438a039ba5be9cd382bc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-22 11:11:49 -0400

    Update 21H2.ps1

--- OSDBuilder/Client/Windows11/21H2.ps1
+++ OSDBuilder/Client/Windows11/21H2.ps1
@@ -1,1 +1,25 @@
+$ISO = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/refs/heads/main/OS%20Downloads/Windows%2011/21H2/21H2.PS1")
+Invoke-Expression $($ISO.Content)
 
+$21H2OSDBuildLog = "C:\Logs\OSDBuilder\OS\Clients\Windows11\21H2.log"
+
+Start-Transcript
+
+$ISOPath = "C:\ISOs\OS\Clients\Win11\21H2\22000.2960.240504-2041.CO_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_EN-US.iso"
+$ISODrive = Get-DiskImage -ImagePath $ISOPath
+$ISOLetter = ($ISODrive | Get-Volume).DriveLetter
+
+Mount-DiskImage -ImagePath $ISOPath 
+$ISOLetter
+
+Write-Host "Importing OSD Media...." -ForegroundColor Cyan
+Import-OSMedia
+
+Write-Host "Updating the imported media..." -ForegroundColor Cyan 
+OSDBuilder -Download OneDrive
+Update-OSMedia -Download -Execute
+
+
+
+Write-Host "Creating new ISO..." -ForegroundColor Cyan 
+New-MediaISO
