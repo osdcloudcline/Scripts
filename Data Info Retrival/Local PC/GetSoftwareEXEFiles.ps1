42c9fa6db7cfccff66ed6da45a47d7d6c9de743d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-22 00:13:05 -0400

    Update GetSoftwareEXEFiles.ps1

--- Data Info Retrival/Local PC/GetSoftwareEXEFiles.ps1
+++ Data Info Retrival/Local PC/GetSoftwareEXEFiles.ps1
@@ -1,1 +1,6 @@
-
+$AppFileInventoryLog = "C:\Logs\Audit\EXE\ApplicationFiles.log"
+Start-Transcript -Path $AppFileInventoryLog
+$prog1 = "C:\Program Files"
+$prog2 = "C:\Program Files (x86)"
+Get-ChildItem -Path "$prog1", "$prog2" -Include *EXE* -Exclude $env:SystemRoot -Recurse -ErrorAction SilentlyContinue | Select-Object -Property Name, VersionInfo | Out-GridView
+Stop-Transcript
