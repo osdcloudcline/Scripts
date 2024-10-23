da132a018ba220ea900a7bc66f0ae31614152286
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-22 22:47:25 -0400

    Create W11-Pro-21H2.ps1

---
+++ OSDBuilder/Client/OSD Build Tasks/W11-Pro-21H2.ps1
@@ -0,0 +1,25 @@
+Write-Host "Importing OSDBuilder PowerShell Module..." -ForegroundColor Cyan
+Import-Module -Name OSDBuilder -Force
+Write-Host "Processing: $W11Pro21H2 OSDBuild Task Creation..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Pro21H2
+      
+pause
+Write-Host "Processing: $W11Pro21H2 .NET Framework 3.5 Enablement..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Pro21H2 -EnableNetFX3 
+      
+pause
+Write-Host "Processing: $W11Pro21H2 Operating System Feature Enablement..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Pro21H2 -EnableFeature
+      
+pause
+Write-Host "Processing: $W11Pro21H2 Operating System AppX Package Removal..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Pro21H2 -RemoveAppx
+      
+pause
+Write-Host "Processing: $W11Pro21H2 Operating System Capability Removal..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Pro21H2 -RemoveCapability
+      
+pause
+
+$21H2 = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/refs/heads/main/OSDBuilder/Client/Windows11/21H2.ps1")
+Invoke-Expression $($21H2.Content)
