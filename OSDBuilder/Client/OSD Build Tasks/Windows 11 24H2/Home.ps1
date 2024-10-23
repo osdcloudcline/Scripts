c8183d95bc90a23d5118b80fb4567fe5ec284077
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-23 07:55:01 -0400

    Create Home.ps1

---
+++ OSDBuilder/Client/OSD Build Tasks/Windows 11 24H2/Home.ps1
@@ -0,0 +1,28 @@
+# OSDBuilder Task name region
+
+$W11Home24H2 = "Windows 11 Home/Core 24H2"
+
+# end region
+
+Write-Host "Processing: $W11Home24H2 OSDBuild Task Creation..." -ForegroundColor Yellow
+New-OSDBuildTask -TaskName $W11Home24H2
+pause
+
+Write-Host "Processing: $W11Home24H2 .NET Framework 3.5 Enablement..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Home24H2 -EnableNetFX3 
+pause
+
+Write-Host "Processing: $W11Home24H2 Operating System Feature Enablement..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Home24H2 -EnableFeature
+pause
+
+Write-Host "Processing: $W11Home24H2 Operating System AppX Package Removal..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Home24H2 -RemoveAppx
+pause
+      
+Write-Host "Processing: $W11Home24H2 Operating System Capability Removal..." -ForegroundColor Yellow
+New-OSBuildTask -TaskName $W11Home24H2 -RemoveCapability
+pause
+
+$24H2 = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/refs/heads/main/OSDBuilder/Client/Windows11/24H2.ps1")
+Invoke-Expression $($24H2.Content)
