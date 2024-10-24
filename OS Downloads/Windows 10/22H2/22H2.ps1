16f9284ee6d5a77637255cb9d77d96addd734b91
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-24 13:31:25 -0400

    Update 22H2.ps1

--- OS Downloads/Windows 10/22H2/22H2.ps1
+++ OS Downloads/Windows 10/22H2/22H2.ps1
@@ -59,26 +59,17 @@ Remove-Item -Path "$ZIPDestination\*.zip" -Recurse -Force
 
 Write-Host "Excess Files and folders have been successfully deleted..." -ForegroundColor Green 
 
+Write-Host
+$ISODestination = "C:\ISOs\OS\Clients\Win10\22H2"
+
+Write-Host "Copying ISO to $ISODestination..." -ForegroundColor Cyan
+New-Item -Path $ISODestination -ItemType Directory
+$ISOFile = "$ZIPDestination\19045.4472.240510-1751.22H2_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.iso"
+
+
+Copy-Item -Path $ISOFile -Destination $ISODestination -Force
+
 Stop-Transcript
 
-$OSDBuilder = Read-Host -Prompt 'Do you want to use OSD Builder to further customize the ISO?'
-If(($OSDBuilder -eq "yes") -or ($OSDBuilder -eq "y") -or ($OSDBuilder -eq "YES") -or ($OSDBuilder -eq "Y")){
-$OSDBuilderCheck = Get-InstalledModule | Where-Object -Property Name -eq "OSDBuilder"
-If($OSDBuilderCheck -eq $true){
-$OSDBuilderPWD = Read-Host -Prompt 'Please specify a path for OSDBuilder working directory'
-Get-OSDBuilder -SetPath $OSDBuilderPWD
-Get-OSDBuilder -SetHome $OSDBuilderPWD
-Import-OSMedia -Update -ShowInfo
-}
-ElseIf($OSDBuilderCheck -eq $false){
-Install-Module -Name OSDBuilder -Force -AllowClobber -SkipPublisherCheck
-Import-Module -Name OSDBuilder -Force
-$OSDBuilderPWD = Read-Host -Prompt 'Please specify a path for OSDBuilder working directory'
-Get-OSDBuilder -SetPath $OSDBuilderPWD
-Get-OSDBuilder -SetHome $OSDBuilderPWD
-Import-OSMedia -Update -ShowInfo
-}
-ElseIf(($OSDBuilder -eq "no") -or ($OSDBuilder -eq "n") -or ($OSDBuilder -eq "NO") -or ($OSDBuilder -eq "N")){
-exit
-}
+
 
