3feeafec83776686801846a09f507ddd020bc508
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-17 14:16:31 -0500

    Update ClientRegistry.ps1

--- Registry Modifications/ClientRegistry.ps1
+++ Registry Modifications/ClientRegistry.ps1
@@ -58,7 +58,7 @@ Invoke-Expression $($3DMark.Content)
 $PCMark = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Software/Scripts/PCMark10.ps1")
 Invoke-Expression $($PCMark.Content) 
 
-$VMWare = Invoke-WebRequest("")
+$VMWare = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/refs/heads/main/Registry%20Modifications/Software/Scripts/VMWare.ps1")
 Invoke-Expression $($VMWare.Content)
 
 $Glary = Invoke-WEbRequest("")
