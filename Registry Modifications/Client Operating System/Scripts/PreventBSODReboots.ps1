add92fe2838bf76bb91d214d5fd352b8a317a093
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-27 06:09:13 -0500

    Update PreventBSODReboots.ps1

--- Registry Modifications/Client Operating System/Scripts/PreventBSODReboots.ps1
+++ Registry Modifications/Client Operating System/Scripts/PreventBSODReboots.ps1
@@ -1,2 +1,6 @@
+$PreventBSODReboots = "Prevent BSOD Reboots"
+
+Write-Verbose "Importing Client OS SYSTEM: $PreventBSODReboots to $env:computername..." -Verbose
+
 if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -Name 'AutoReboot' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
