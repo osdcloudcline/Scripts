1df633163ac7c8f2d7c88a1b9c674b49784b0c64
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-04 22:25:16 -0500

    Update ManagementPCSM.ps1

--- Start Menu Configurations/Management PC/ManagementPCSM.ps1
+++ Start Menu Configurations/Management PC/ManagementPCSM.ps1
@@ -6,3 +6,16 @@ $SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Management PC"
 Write-Host "This computer has been installed with the Honeypot Configuration of software" -ForgroundColor Green
 Write-Host
 Write-Host "The MANAGEMENT PC Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
+
+$StartMenudownload = "C:\downloads\StartMenu\Management PC"
+
+$MPCSMBINFileURL = "https://github.com/osdcloudcline/Scripts/raw/refs/heads/main/Start%20Menu%20Configurations/Honeypot/start2.bin"
+
+Write-Verbose "Acquiring MANAGEMENT PC start2.bin for Start Menu Configuration..." -Verbose
+Save-WebFile -SourceUrl $HoneypotSMBINFileURL -DestinationDirectory $StartMenudownload
+
+Write-Host "Applying changes to Start Menu on $env:computername..." -ForegroundColor Cyan 
+$StartMenuBINFile = "C:\downloads\StartMenu\Management PC\start2.bin"
+$SMDestination = "C:\Users\$env:username\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
+Write-Verbose "Copying Start Menu file to $SMDestination...." -Verbose
+Copy-Item -Path $StartMenuBINFile -Destination $SMDestination -Force 
