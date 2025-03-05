f43779d965c457ad0189155515133a86239452d3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-04 22:21:14 -0500

    Update CorporateSM.ps1

--- Start Menu Configurations/Corporate/CorporateSM.ps1
+++ Start Menu Configurations/Corporate/CorporateSM.ps1
@@ -1,8 +1,20 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
-$CORPLogFileFolder = (Test-Path -Path "C:\OSDCloud\DeploymentLogs\Corporate" -IsValid){
+
 $SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Corporate"
+
 Write-Host "This computer has been installed with the Corporate Configuration of software" -ForgroundColor Green
 Write-Host
 Write-Host "The Corporate Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
+
+$StartMenudownload = "C:\downloads\StartMenu\Corporate"
+
+Write-Verbose "Acquiring CORPORATE start2.bin for Start Menu Configuration..." -Verbose
+Save-WebFile -SourceUrl -DestinationDirectory $StartMenudownload
+
+Write-Host "Applying changes to Start Menu on $env:computername..." -ForegroundColor Cyan 
+$StartMenuBINFile = "C:\downloads\StartMenu\Corporate\start2.bin"
+$SMDestination = "C:\Users\$env:username\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
+Write-Verbose "Copying Start Menu file to $SMDestination...." -Verbose
+Copy-Item -Path $StartMenuBINFile -Destination $SMDestination -Force 
