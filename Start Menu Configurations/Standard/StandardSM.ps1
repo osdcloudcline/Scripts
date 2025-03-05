3666a4de666b70712d577c2a31125fe8f1f509a5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-05 00:22:17 -0500

    Delete Start Menu Configurations directory

--- Start Menu Configurations/Standard/StandardSM.ps1
+++
@@ -1,8 +0,0 @@
-Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
-Import-Module -Name OSD -Force
-
-$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Standard"
-
-Write-Host "This computer has been installed with the Standard Configuration of software" -ForgroundColor Green
-Write-Host
-Write-Host "The STANDARD Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
--- Start Menu Configurations/VDI/ClientOS-VDISM.ps1
+++
@@ -1,9 +0,0 @@
-
-Import-Module -Name OSD -Force
-
-
-$SMSource = "OSDCloud Cline\Scripts\Start Menu Configurations\Client OS\VDI"
-
-Write-Host "This computer has been installed with the VDI Remote Desktop Configuration of software" -ForgroundColor Green
-Write-Host
-Write-Host "The VDI Start Menu layout will now be downloaded from $SMSource to $env:userprofile and configured on $env:computername..." -ForegroundColor Cyan
