f32570c2038ee1a3ceed0d46453a034bd16f5805
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-16 00:34:34 -0400

    Pipe pwsh commands for module installation

--- PowerShell Modules/Version 7.x/Install.ps1
+++ PowerShell Modules/Version 7.x/Install.ps1
@@ -7,8 +7,8 @@ $PS7 = "pwsh.exe"
 Start-Process -FilePath $PS7
 
 Write-Verbose "Installing: $CloudModule1..." -Verbose
-pwsh Install-PSResource -Name Microsoft.PowerShell.PSResourceGet Repository PSGallery -TrustRepository -AcceptLicense
-pwsh Install-PSResource -Name PSCloudPC -Repository PSGallery -TrustRepository -AcceptLicense
+pwsh | Install-PSResource -Name Microsoft.PowerShell.PSResourceGet Repository PSGallery -TrustRepository -AcceptLicense
+pwsh | Install-PSResource -Name PSCloudPC -Repository PSGallery -TrustRepository -AcceptLicense
 Start-Sleep -Seconds 5
-pwsh Import-Module -Name PSCloudPC -Force
+pwsh | Import-Module -Name PSCloudPC -Force
 Get-Module -ListAvailable | Where-Object {$_.Name -eq "PSCloudPC"}
