f424396402b5378f77eaa152cf56480f347f4535
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-16 00:06:35 -0400

    Fix syntax error in Install.ps1 for PSCloudPC

--- PowerShell Modules/Version 7.x/Install.ps1
+++ PowerShell Modules/Version 7.x/Install.ps1
@@ -8,7 +8,7 @@ Start-Process -FilePath $PS7
 
 Write-Host  "Installing: $CloudModule1..." -ForegroundColor Cyan
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-Install-PSResource -Name PSCloudPC -Reinstall-
+Install-PSResource -Name PSCloudPC -Reinstall
 Start-Sleep -Seconds 3
 Write-Host
 
