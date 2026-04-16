577c0f633f92cc7d8c75ddbb3b3acfe0c037f21f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-15 23:40:55 -0400

    Update installation method for PSResourceGet module

--- PowerShell Modules/Version 7.x/Install.ps1
+++ PowerShell Modules/Version 7.x/Install.ps1
@@ -10,8 +10,8 @@ Write-Verbose "Installing: $CloudModule1..." -Verbose
 
 
 # 2. Ensure PSResourceGet is present
-Install-Module -Name Microsoft.PowerShell.PSResourceGet -AllowClobber -SkipPublisherCheck -Force
-Import-Module -Name Microsoft.PowerShell.PSResourceGet -Force
+Start-Process -FilePath $PS7 -PassThru | Install-PSResource -Name Microsoft.PowerShell.PSResourceGet -Version 1.2.0-preview1 -Prerelease -Reinstall
+
 
 # 3. Install the Cloud PC module directly
 # Using -TrustRepository and -AcceptLicense for silent automation
