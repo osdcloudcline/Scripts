481a65e2b81fa2b2163102d0d1c8159c18da066b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-15 23:58:13 -0400

    Update Install.ps1

--- PowerShell Modules/Version 7.x/Install.ps1
+++ PowerShell Modules/Version 7.x/Install.ps1
@@ -3,22 +3,12 @@ $CloudModule1 = "Windows 365 Cloud PC"
 $ModuleName = "PSCloudPC"
 
 
-$PS7 = "C:\Program Files\PowerShell\7\pwsh.exe" 
+$PS7 = "pwsh.exe"
+Start-Process -FilePath $PS7
 
-
-Write-Verbose "Installing: $CloudModule1..." -Verbose
-
-
-
-Install-Module Microsoft.PowerShell.PSResourceGet -AllowClobber -SkipPublisherCheck -Force
-Start-Process -FilePath $PS7 | Install-PSResource -Name PSCloudPC -Repository PSGallery -TrustRepository -AcceptLicense | Import-Module -Name PSCloudPC -Force
-
-# 4. Verification
-Write-Verbose "Verifying Module: $CloudModule1..." -Verbose
-$installedModule = Get-Module -ListAvailable | Where-Object {$_.Name -eq $ModuleName}
-
-if ($installedModule) {
-    Write-Host "Successfully installed $ModuleName" -ForegroundColor Green
-} else {
-    Write-Warning "Module $ModuleName was not found after installation."
-}
+Write-Host  "Installing: $CloudModule1..." -ForegroundColor Cyan
+Install-Module -Name PSCloudPC -Force -AllowClobber -SkipPublisherCheck
+Start-Sleep -Seconds 3
+Import-Module -Name PSCloudPC -Force
+Start-Sleep -Seconds 4
+Write-Host
